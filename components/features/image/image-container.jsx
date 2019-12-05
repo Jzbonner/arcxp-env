e358d351fead56e68140c98e2c2df5ad57f877a9
Antonio <garcia.juan1994@gmail.com>
2019-12-05 12:01:07 -0500

    removes unecessary chain and fixes lint error in demo image container

--- components/features/image/image-container.jsx
+++ components/features/image/image-container.jsx
@@ -4,17 +4,13 @@ import demo from '../../../resources/images/demo.jpg';
 import './default.scss';
 
 
-const ImageContainer = () => {
-  return (
-    <div>
+const ImageContainer = () => <div>
         <ImageComponent url={demo} alt={'I am a building'} location={'head'}/>
         This is the image size for the head
         <ImageComponent url={demo} alt={'I am a building'} location={'breaking'}/>
         This is the image size for breaking news
         <ImageComponent url={demo} alt={'I am a building'} location={'thumbnail'}/>
         This is the image size for thumbnails
-    </div>
-  );
-};
+    </div>;
 
 export default ImageContainer;
