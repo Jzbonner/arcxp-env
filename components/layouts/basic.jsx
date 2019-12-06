b53e23991c875331dc72797eafa2b05c11169400
lcopeland-AJC <lisa.copeland@ajc.com>
2019-12-06 10:59:48 -0500

    APD-2/article_page_headline-Lisa

---
+++ components/layouts/basic.jsx
@@ -0,0 +1,38 @@
+/*  /components/layouts/basic.jsx  */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StoryPageLayout = (props) => {
+  const [heading, leadImage, byline, content] = props.children;
+
+  return <>
+    <header>
+      <h1>
+        { heading }
+      </h1>
+      <div>
+        { leadImage }
+      </div>
+      <div>
+        { byline }
+      </div>
+    </header>
+    <article>
+      { content }
+    </article>
+  </>;
+};
+
+StoryPageLayout.sections = [
+  'heading',
+  'leadImage',
+  'byline',
+  'content',
+];
+
+StoryPageLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default StoryPageLayout;
---
+++ content/sources/content-api.js
@@ -0,0 +1,17 @@
+/*  /content/sources/content-api.js  */
+
+const schemaName = 'article';
+const params = {
+  path: 'text',
+  published: 'text',
+};
+const resolve = (query) => {
+  const { 'arc-site': arcSite = 'ajc', path, published } = query;
+  const requestUri = `/content/v4/stories/?website_url=${path}&website=${arcSite}`;
+  return published ? `${requestUri}&published=${published}` : requestUri;
+};
+export default {
+  resolve,
+  params,
+  schemaName,
+};
