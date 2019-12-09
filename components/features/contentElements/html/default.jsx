f4a9f3454f29f1be7796d557effd415aa23f0642
Ilkov <silkov@NATL2236LSILKOV.local>
2019-12-09 15:20:37 -0500

    reversed to dangerouslySetInnerHTML for the HTML element only, since it's better to actually display it as HTML, instead of text

--- components/features/contentElements/html/default.jsx
+++ components/features/contentElements/html/default.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const HTML = ({ src }) => (
     <div style={{ border: '1px solid #000', padding: '10px' }}>
       Content Element Type: <strong>HTML</strong>
-      <p>{src.content}</p>
+      <div dangerouslySetInnerHTML={{ __html: src.content }}></div>
     </div>
 );
 
