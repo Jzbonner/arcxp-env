99cedc2599bbdc886ac8f3ce076a65d5b8c6d3d6
lcopeland-AJC <lisa.copeland@ajc.com>
2019-12-12 12:32:57 -0500

    APD-24/article_page_subheadline-Lisa

--- components/features/article/subheadline/default.jsx
+++ components/features/article/subheadline/default.jsx
@@ -7,13 +7,10 @@ import './style.scss';
 const SubHeadline = () => {
   const appContext = useAppContext();
   const { globalContent } = appContext;
-  // console.log(globalContent.subheadlines);
 
   return (
     <div className="article-subheadline">
       <div className="article-subheadline-body text-align">
-        {/* <span>This is a subheadline. This will appear below the headline.</span> */}
-        {/* <span>Colin Kaepernick once led the San Francisco 49ers to back-to-back playoff appearances, including a Super Bowl</span> */}
         <span>{globalContent.subheadlines.basic}</span>
       </div>
     </div>
