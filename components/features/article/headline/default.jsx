dad38d94ac985addaa4b1786bd2679843e862b0d
Nicholas Engleman <nicholas.engleman@ajc.com>
2019-12-13 10:16:36 -0500

    ADP-4,64,5,6/StyleTweaksRevertedChangesToImageComponent-Nicholas

--- components/features/article/headline/default.jsx
+++ components/features/article/headline/default.jsx
@@ -14,20 +14,17 @@ const Headline = () => {
     promoData = globalContent.promo_items.basic;
   }
 
-  promoData.type = 'video';
+  // show to see how the headline component dispays with a video promo type. Used because I was getting errors when trying to add a video as a featured element.
+  // promoData.type = 'video';
 
   return (
     <div className="article-headline-container">
-      <div className={`article-headline ${promoData.type ? `with-${promoData.type}` : ''}`}>
+      <div className={`article-headline with-${promoData.type ? `${promoData.type}` : 'just-headline'}`}>
         <div className="article-headline-body">
           <h3>{globalContent.headlines.basic}</h3>
         </div>
       </div>
-      {promoData.type === 'image' && (
-        <div className="lead-image-container">
-          <Image imageSource={promoData.url} alt={promoData.caption} outerComponentClassName="head" />
-        </div>
-      )}
+      {promoData.type === 'image' && <Image imageSource={promoData.url} alt={promoData.caption} outerComponentClassName="head" />}
       {promoData.type === 'gallery' && <div className="gallery-container placeholder">Gallery Placeholder</div>}
       {promoData.type === 'video' && <div className="video-container placeholder">Video Placeholder</div>}
     </div>
