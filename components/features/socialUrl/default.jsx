6aa36731c010c0cbcbbeb5834a85d7e69314c446
Antonio <garcia.juan1994@gmail.com>
2019-12-17 10:58:13 -0500

    ADP-35/resolvedScriptLoadingAndAddedScriptJsDependency-Antonio

--- components/features/socialUrl/default.jsx
+++ components/features/socialUrl/default.jsx
@@ -2,12 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './default.scss';
 
-const Oembed = ({ html, className, type }) => (
+const Oembed = ({ html, className, type }) => {
+  let filteredHtml = html;
+  let SCRIPT_URL;
+  const scriptFilter = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
+
+  if (type === 'reddit' || type === 'facebook-post' || type === 'facebook-video' || type === 'twitter' || type === 'instagram') {
+    filteredHtml = html.replace(scriptFilter, '');
+    if (type === 'reddit') {
+      SCRIPT_URL = 'https://embed.redditmedia.com/widgets/platform.js';
+      /* eslint-disable-next-line global-require */
+      require('scriptjs')(SCRIPT_URL);
+    } else if (type === 'facebook-post' || type === 'facebook-video') {
+      SCRIPT_URL = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0';
+      /* eslint-disable-next-line global-require */
+      require('scriptjs')(SCRIPT_URL);
+    } else if (type === 'twitter') {
+      SCRIPT_URL = 'https://platform.twitter.com/widgets.js';
+      /* eslint-disable-next-line global-require */
+      require('scriptjs')(SCRIPT_URL);
+    } else if (type === 'instagram') {
+      SCRIPT_URL = '//www.instagram.com/embed.js';
+      /* eslint-disable-next-line global-require */
+      require('scriptjs')(SCRIPT_URL);
+    }
+  }
+
+  return (
     <div
     data-oembed-type={type}
     className={className}
-    dangerouslySetInnerHTML={{ __html: html }}/>
-);
+    dangerouslySetInnerHTML={{ __html: filteredHtml }}/>
+  );
+};
 
 Oembed.propTypes = {
   html: PropTypes.string,
