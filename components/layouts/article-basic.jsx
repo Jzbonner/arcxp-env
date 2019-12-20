1a4a5946bded79ed91ff5a953cae4a4b59f724c1
Antonio <garcia.juan1994@gmail.com>
2019-12-20 15:41:26 -0500

    APD-28/pullingChangesFromDevBeforePR-Antonio

--- components/layouts/article-basic.jsx
+++ components/layouts/article-basic.jsx
@@ -35,7 +35,6 @@ const StoryPageLayout = () => {
   return (
     <>
       <header>
-        <Headline headlines={headlines} basicItems={basicItems} />
         <div>
           <SubHeadline subheadlines={subheadlines} />
         </div>
