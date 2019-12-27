cc07bb92903acfdea604ec4c2cd3dde284d980b7
Nicholas Engleman <nicholas.engleman@ajc.com>
2019-12-27 13:27:42 -0500

    ADP-137/ArrangedComponentInResponsiveLayout-Nicholas

--- components/_helper_components/article/contentElements/default.jsx
+++ components/_helper_components/article/contentElements/default.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ParagraphCounter from '../../../layouts/_helper_functions/ParagraphCounter';
 import BlockQuote from './components/blockQuote/default';
 import Correction from './components/correction/default';
 import Gallery from './components/gallery/default';
@@ -14,44 +15,50 @@ import Video from './components/video/default';
 import Header from './components/header/default';
 
 const ContentElements = ({ contentElements }) => (
-    <div>
-      {contentElements.map((element) => {
-        // console.log('ELEMENT', element);
-        switch (element.type) {
-          case 'blockquote':
-          case 'quote':
-            return <BlockQuote src={element} />;
-          case 'correction':
-            return <Correction src={element} />;
-          case 'gallery':
-            return <Gallery src={element} />;
-          case 'raw_html':
-            return <HTML src={element} />;
-          case 'header':
-            return <Header src={element} />;
-          case 'image':
-            return <SecondaryImage src={element} />;
-          case 'text':
-            return <Paragraph src={element} />;
-          case 'interstitial_link':
-            return <InterstitialLink src={element} />;
-          case 'list':
-            return <List src={element} />;
-          case 'oembed_response':
-            return <Oembed src={element.raw_oembed} />;
-          case 'table':
-            return <Table src={element} />;
-          case 'video':
-            return <Video src={element} />;
-          default:
-            return (
-              <ul>
-                <li key={element.id}>{element.type}</li>
-              </ul>
-            );
-        }
-      })}
-    </div>
+  <div>
+    {contentElements.map((element, i) => {
+      let { type } = element.type;
+      if (i === 1 && ParagraphCounter(contentElements) > 1) {
+        type = 'Right Rail';
+      }
+      // console.log('ELEMENT', element);
+      switch (type) {
+        case 'Right Rail':
+          return <div className="rightrail">Right Rail</div>;
+        case 'blockquote':
+        case 'quote':
+          return <BlockQuote src={element} />;
+        case 'correction':
+          return <Correction src={element} />;
+        case 'gallery':
+          return <Gallery src={element} />;
+        case 'raw_html':
+          return <HTML src={element} />;
+        case 'header':
+          return <Header src={element} />;
+        case 'image':
+          return <SecondaryImage src={element} />;
+        case 'text':
+          return <Paragraph src={element} />;
+        case 'interstitial_link':
+          return <InterstitialLink src={element} />;
+        case 'list':
+          return <List src={element} />;
+        case 'oembed_response':
+          return <Oembed src={element.raw_oembed} />;
+        case 'table':
+          return <Table src={element} />;
+        case 'video':
+          return <Video src={element} />;
+        default:
+          return (
+            <ul>
+              <li key={element.id}>{element.type}</li>
+            </ul>
+          );
+      }
+    })}
+  </div>
 );
 
 ContentElements.propTypes = {
--- components/layouts/article-basic.jsx
+++ components/layouts/article-basic.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useAppContext } from 'fusion:context';
+import './article-basic.scss';
 import TimeStamp from '../_helper_components/article/timestamp/default';
 import Byline from '../_helper_components/article/byline/default';
 import ContentElements from '../_helper_components/article/contentElements/default';
@@ -36,19 +37,39 @@ const StoryPageLayout = () => {
   return (
     <>
       <header>
-        <Headline headlines={headlines} basicItems={basicItems} />
-        <div>
-          <SubHeadline subheadlines={subheadlines} />
+        <div className="breaking-news placeholder">
+          <div className="container">Breaking News</div>
         </div>
-        <div>
-          <SectionLabel label={label} taxonomy={taxonomy} />
-          <TimeStamp firstPublishDate={firstPublishDate} displayDate={displayDate} />
+        <div className="header">
+          <div className="container">
+            <div className="logo placeholder">Logo</div>
+            <div className="header-nav placeholder">
+              <nav className="menu placeholder">Menu</nav>
+              <div className="weather placeholder">Weather</div>
+              <div className="log-in placeholder">Log In</div>
+            </div>
+            <div className="subscribe placeholder">Support Local Journalism. Subscribe today for 99¢.</div>
+          </div>
         </div>
-        <Byline by={authorData} />
       </header>
-      <article>
-        <ContentElements contentElements={contentElements} />
-      </article>
+      <main className="main-container">
+        <Headline headlines={headlines} basicItems={basicItems} />
+        <article className="article">
+          <SubHeadline subheadlines={subheadlines} />
+          <div className="flex-row flex-center">
+            <SectionLabel label={label} taxonomy={taxonomy} />
+            <TimeStamp firstPublishDate={firstPublishDate} displayDate={displayDate} />
+          </div>
+          <div className="flex-row flex-center">
+            <Byline by={authorData} />
+          </div>
+          <ContentElements contentElements={contentElements} />
+          <div className="placeholder taboola">Taboola</div>
+        </article>
+      </main>
+      <footer className="footer placeholder">
+        <div className="container">Footer</div>
+      </footer>
     </>
   );
 };
