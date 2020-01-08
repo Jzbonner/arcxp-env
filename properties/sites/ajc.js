d4050db66e3beb1b4b275227b225d6ec587c0de8
Dave Young <dyoung@MATL2232LDYOUNG.hsd1.ga.comcast.net>
2020-01-08 15:36:37 -0500

    adding custom ajc properties

---
+++ properties/sites/ajc.js
@@ -0,0 +1,8 @@
+export default {
+  defaultSiteTitle: 'AJC',
+  dfpId: '21849707860/TEST_atlanta_np/ajc_web_default/',// staging id.  prod is '21849707860/Atlanta_NP/ajc_web_default/'
+  siteDomainURL: '//www.ajc.com',
+  twitterURL: '//twitter.com/ajc',
+  facebookURL: '//facebook.com/ajc',
+  instagramURL: '//instagram.com/ajcnews',
+};
