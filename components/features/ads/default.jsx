6a9b5ba2e2502d0310ce01c94a0082b512448513
Ilkov <silkov@NATL2236LSILKOV.local>
2020-01-08 10:40:21 -0500

    basic logic for the AD component

---
+++ components/features/ads/default.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ArcAdLib from './children/arcAdLib';
+
+const AdSetup = ({
+  customFields, breakpoints, dfpId, prerender, className, slotName, dimensions,
+}) => {
+  window.arcAdsPrerenderer = adDetails => new Promise((resolve) => {
+    if (prerender) {
+      prerender.adDetails();
+    }
+    resolve(adDetails);
+  });
+
+  const { id } = customFields;
+
+  const instance = ArcAdLib.getInstance();
+  if (instance) {
+    instance.registerAd({
+      id,
+      slotName,
+      dimensions,
+      sizemap: {
+        breakpoints,
+      },
+      dfpId,
+      prerender,
+    });
+  }
+
+  return (
+    <div className={className}>
+      <div id={id} className={`${slotName} arcad`} />
+    </div>
+  );
+};
+
+AdSetup.propTypes = {
+  dfpId: PropTypes.number,
+  breakpoints: PropTypes.array,
+  prerender: PropTypes.func,
+  className: PropTypes.string,
+  dimensions: PropTypes.array,
+  slotName: PropTypes.string,
+  customFields: PropTypes.shape({
+    id: PropTypes.oneOf(['HP01', 'RP09', 'PX01']).tag({
+      label: 'Slot ID',
+      description: 'Choose a Slot ID for your AD',
+    }).isRequired,
+  }),
+};
+
+AdSetup.defaultProps = {
+  dimensions: [0, 0],
+};
+
+export default AdSetup;
