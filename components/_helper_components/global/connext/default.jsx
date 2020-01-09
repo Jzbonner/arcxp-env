06f4989ec7d8e5c0221b3a98ff0d8122dcd6b426
Dave Young <dyoung@MATL2232LDYOUNG.hsd1.ga.comcast.net>
2020-01-09 14:28:23 -0500

    cleaned up formatting, still working on output

--- components/_helper_components/global/connext/default.jsx
+++ components/_helper_components/global/connext/default.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { connext } from 'fusion:environment';
 
 const ConnextInit = () => {
@@ -10,177 +11,182 @@ const ConnextInit = () => {
     tagManager,
     containerId,
   } = connext;
-  return null;
-    // return (
-    //   <script type="text/javascript">
-  let mg2Logout = function(e)  {
-    console.error('called logout');
-  	function delete_cookie(name) {
-  	  document.cookie = name + "=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  	  document.cookie = name + "=;path=/;domain="+location.host+";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  	  document.cookie = name + "=;path=/;domain=."+location.host+";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  	  let arr = location.host.split(".");
-  	  let rootdomain = arr[arr.length-2]+ "." + arr[arr.length-1];
-  	  document.cookie = name + "=;path=/;domain="+rootdomain+";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  	  document.cookie = name + "=;path=/;domain=."+rootdomain+";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  	}
-  	delete_cookie("igmAuth");
-  	delete_cookie("igmContent");
-  	delete_cookie("igmRegID");
-  	//Engage.Run();
-  	window.Connext.Logout();
+
+  return {
+    const doc = window.document;
+
+    const mg2Logout = () => {
+      console.error('called logout');
+      const deleteCookie = (name) => {
+        const { location: host } = window;
+        const arr = host.split('.');
+        const rootdomain = `${arr[arr.length - 2]}.${arr[arr.length - 1]}`;
+        doc.cookie = `${name}=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+        doc.cookie = `${name}=;path=/;domain=${host};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+        doc.cookie = `${name}=;path=/;domain=.${host};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+        doc.cookie = `${name}=;path=/;domain=${rootdomain};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+        doc.cookie = `${name}=;path=/;domain=.${rootdomain};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+      };
+      deleteCookie('igmAuth');
+      deleteCookie('igmContent');
+      deleteCookie('igmRegID');
+      window.Connext.Logout();
+    };
+
+    doc.addEventListener('connextLoaded', () => {
+      console.error('dave - connextLoaded in init');
+    });
+    doc.addEventListener('DOMContentLoaded', () => {
+      const connextLoaded = new Event('connextLoaded');
+      const connextLoggedIn = new Event('connextLoggedIn');
+      const connextLoggedOut = new Event('connextLoggedOut');
+      const connextIsSubscriber = new Event('connextIsSubscriber');
+
+      const loggedOutState = doc.querySelectorAll('.nav-auth__anon-user');
+      const loggedInState = doc.querySelectorAll('.nav-auth__authed-user');
+      const activationLinks = doc.querySelectorAll('.MG2activation');
+      const showThisEl = (el) => {
+        const elToShow = el;
+        elToShow.style.display = 'block';
+        elToShow.className = elToShow.className.replace('hidden', ' ');
+      };
+      const hideThisEl = (el) => {
+        const elToHide = el;
+        elToHide.style.display = 'none';
+        if (elToHide.className.indexOf('hidden') === -1) {
+          elToHide.className += ' hidden';
+        }
+      };
+      const toggleActivateLink = (showOrHide) => {
+        if (showOrHide === 'show') {
+          activationLinks.forEach((child) => {
+            showThisEl(child);
+          });
+        } else {
+          activationLinks.forEach((child) => {
+            hideThisEl(child);
+          });
+        }
+      };
+      const toggleLoggedInState = (showOrHide) => {
+        if (showOrHide === 'show') {
+          loggedInState.forEach((child) => {
+            showThisEl(child);
+          });
+        } else {
+          loggedInState.forEach((child) => {
+            hideThisEl(child);
+          });
+        }
+      };
+      const toggleLoggedOutState = (showOrHide) => {
+        if (showOrHide === 'show') {
+          loggedOutState.forEach((child) => {
+            showThisEl(child);
+          });
+        } else {
+          loggedOutState.forEach((child) => {
+            hideThisEl(child);
+          });
+        }
+      };
+
+      // console.error('>> domcontentloaded');
+      Promise.resolve(window.MG2Loader.init({
+        plugins: [
+          {
+            name: 'FP',
+            initOptions: {
+              version: clientCode || '1.0',
+              environment,
+            },
+          },
+          {
+            name: 'DL',
+            initOptions: {
+              environment,
+              version: clientCode,
+              collectors: ['connext'],
+              tagManager,
+              containerId,
+            },
+          },
+          {
+            name: 'NXT',
+            initOptions: {
+              clientCode,
+              environment,
+              siteCode,
+              configCode,
+              debug,
+              silentmode: false,
+              publicEventHandlers: {
+                onActionClosed: (e) => { console.error('>> onActionClosed', e); },
+                onActionShown: (e) => { console.error('>> onActionShown', e); },
+                onButtonClick: (e) => { console.error('>> onButtonClick', e); },
+                onCampaignFound: (e) => { console.error('>> onCampaignFound', e); },
+                onConversationDetermined: (e) => { console.error('>> onConversationDetermined', e); },
+                onCriticalError: (e) => { console.error('>> onCriticalError', e); },
+                onDebugNote: (e) => { console.error('>> onDebugNote', e); },
+                onInit: (e) => {
+                  console.error('>> onInit', e);
+                  doc.querySelectorAll('.js-auth-logout').forEach((el) => {
+                    el.addEventListener('click', mg2Logout);
+                  });
+                  doc.dispatchEvent(connextLoaded);
+                },
+                onLoggedIn: (e) => {
+                  toggleLoggedInState('show');
+                  toggleLoggedOutState('hide');
+                  console.error('>> onLoggedIn', e);
+                  doc.dispatchEvent(connextLoggedIn);
+                },
+                onNotAuthorized: (e) => {
+                  // this event fires on every Engage loading if user is logged out
+                  console.error('>> onNotAuthorized', e);
+
+                  toggleLoggedInState('hide');
+                  toggleLoggedOutState('show');
+                  doc.dispatchEvent(connextLoggedOut);
+                },
+                onMeterLevelSet: (e) => {
+                  console.error('>> onMeterLevelSet', e);
+                },
+                onAuthorized: (e) => {
+                  // this event fires on every Engage loading if user is logged in but doesn't have access to this product
+                  console.error('>> onAuthorized', e);
+                  toggleActivateLink('show');
+                  doc.dispatchEvent(connextLoggedIn);
+                },
+                onHasNoActiveSubscription: (e) => {
+                  // this event fires on every Engage loading if user is logged in but subscription is stopped or inactive
+                  console.error('>> onHasNoActiveSubscription', e);
+                  toggleActivateLink('show');
+                },
+                onHasAccessNotEntitled: (e) => {
+                  // this event fires on every Engage loading if user is logged in
+                  // and doesn't have access to this product but has access to another one
+                  console.error('>> onHasAccessNotEntitled', e);
+                },
+                onHasAccess: (e) => {
+                  // this event fires on every Engage loading if user is subscriber
+                  console.error('>> onHasAccess', e);
+                  toggleActivateLink('hide');
+                  doc.dispatchEvent(connextIsSubscriber);
+                },
+              },
+            },
+          },
+        ],
+      }));
+    });
   };
+  // return null;
+  // return (
+  // <script type='text/javascript'>
 
-  (function() {
-  	let loggedOutState = document.querySelectorAll(".nav-auth__anon-user"),
-  	    loggedInState = document.querySelectorAll(".nav-auth__authed-user"),
-  	    activationLinks = document.querySelectorAll(".MG2activation"),
-  	    myAccountLinks = document.querySelectorAll(".MG2MyAccount"),
-  	    showThisEl = function(el) {
-  		    el.style.display = 'block';
-              el.className = el.className.replace('hidden', ' ');
-    		},
-    		hideThisEl = function(el) {
-    			el.style.display = 'none';
-                if (el.className.indexOf('hidden') === -1) {
-                  el.className += ' hidden';
-                }
-    		},
-    		toggleActivateLink = function(showOrHide) {
-    			if (showOrHide === 'show') {
-                    activationLinks.forEach(function(child) {
-    	    		    showThisEl(child);
-    	    		});
-    			} else {
-    				activationLinks.forEach(function(child) {
-                        hideThisEl(child);
-                    });
-    			}
-  	    },
-  	    toggleLoggedInState = function(showOrHide) {
-  	    	if (showOrHide === 'show') {
-  	    		loggedInState.forEach(function(child) {
-  	    			showThisEl(child);
-  	            });
-  	    	} else {
-  	    		loggedInState.forEach(function(child) {
-                      hideThisEl(child);
-                  });
-  	    	}
-  	    },
-  	    toggleLoggedOutState = function(showOrHide) {
-  	    	if (showOrHide === 'show') {
-  	    		loggedOutState.forEach(function(child) {
-  	    			showThisEl(child);
-  	            });
-  	    	} else {
-  	    		loggedOutState.forEach(function(child) {
-  	    			hideThisEl(child);
-                  });
-  	    	}
-  	    };
-
-  	var connextLoaded = new Event('connextLoaded'),
-  	   connextLoggedIn = new Event('connextLoggedIn'),
-  	   connextLoggedOut = new Event('connextLoggedOut'),
-  	   connextIsSubscriber = new Event('connextIsSubscriber');
-
-  	document.addEventListener('conextLoaded', function() {
-  		console.error('dave - connextLoaded in init');
-  	});
-  	document.addEventListener('DOMContentLoaded', function() {
-  	// console.error('>> domcontentloaded');
-
-  	let loadConnext = Promise.resolve(MG2Loader.init({
-  	  plugins: [
-  	    {
-  	      name: 'FP',
-  	      initOptions: {
-  	        version: clientCode || '1.0',
-  	        environment: environment,
-  	      },
-  	    }, {
-  	      name: 'DL',
-  	      initOptions: {
-  	        environment: environment,
-  	        version: clientCode,
-  	        collectors: ['connext'],
-  	        tagManager: tagManager,
-  	        containerId: containerId,
-  	      },
-  	    },{
-  	        name: 'NXT',
-  	        initOptions: {
-  	          clientCode: clientCode,
-  	          environment: environment,
-  	          siteCode: siteCode,
-  	          configCode: configCode,
-  	          debug: debug,
-  	          silentmode: false,
-  	          publicEventHandlers: {
-  	            onActionClosed: function(e) { console.error('>> onActionClosed', e); },
-  	            onActionShown: function(e) { console.error('>> onActionShown', e); },
-  	            onButtonClick: function(e) { console.error('>> onButtonClick', e); },
-  	            onCampaignFound: function(e) { console.error('>> onCampaignFound', e); },
-  	            onConversationDetermined: function(e) { console.error('>> onConversationDetermined', e); },
-  	            onCriticalError: function(e) { console.error('>> onCriticalError', e); },
-  	            onDebugNote: function(e) { console.error('>> onDebugNote', e); },
-  	            onInit: function(e) {
-  	          	    console.error('>> onInit', e);
-  	          	    document.querySelectorAll('.js-auth-logout').forEach(function(el) {
-  	          	    	el.addEventListener('click', mg2Logout);
-  	          	    });
-  	          	    document.dispatchEvent(connextLoaded);
-  	            },
-  	            onLoggedIn: function(e) {
-                      toggleLoggedInState('show');
-                      toggleLoggedOutState('hide');
-                      console.error('>> onLoggedIn', e);
-                      document.dispatchEvent(connextLoggedIn);
-  	            },
-  	            onNotAuthorized: function(e) {
-  	                //this event fires on every Engage loading if user is logged out
-  	                console.error('>> onNotAuthorized', e);
-
-  	                toggleLoggedInState('hide');
-  	                toggleLoggedOutState('show');
-                      document.dispatchEvent(connextLoggedOut);
-  	            },
-  	            onMeterLevelSet: function(e) {
-  	            	console.error('>> onMeterLevelSet', e);
-  	            },
-  	            onAuthorized: function(e) {
-  	            	//this event fires on every Engage loading if user is logged in but doesn't have access to this product
-  	                console.error('>> onAuthorized', e);
-  	                toggleActivateLink('show');
-                      document.dispatchEvent(connextLoggedIn);
-  	            },
-  	            onHasNoActiveSubscription: function(e) {
-  	              //this event fires on every Engage loading if user is logged in but his subscription is stopped or inactive
-  	              console.error('>> onHasNoActiveSubscription', e);
-  	              toggleActivateLink('show');
-
-  	            },
-  	            onHasAccessNotEntitled: function(e) {
-  	              //this event fires on every Engage loading if user is logged in but doesn't have access to this product and has access to another one
-  	            	console.error('>> onHasAccessNotEntitled', e);
-  	            },
-  	            onHasAccess: function(eventData) {
-  	              //this event fires on every Engage loading if user is subscriber
-  	            	console.error('>> onHasAccess', eventData);
-  	            	toggleActivateLink('hide');
-                      document.dispatchEvent(connextIsSubscriber);
-  	            }
-  	          },
-  	        },
-  	      }
-  	    ]
-  	  })
-  	);
-  	});
-  })();
-    //     </script>
-    // );
+  //     </script>
+  // );
 };
 
 export default ConnextInit;
