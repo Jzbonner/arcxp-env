4e8d38acfa5b02a92f644b54de8436680f8df9b6
Dave Young <dave.young@ajc.com>
2020-01-15 14:47:23 -0500

    fixing the dfpid prop

--- properties/sites/ajc.js
+++ properties/sites/ajc.js
@@ -1,6 +1,6 @@
 export default {
   defaultSiteTitle: 'AJC',
-  dfpId: '21849707860/TEST_atlanta_np/ajc_web_default/', // staging id.  prod is '21849707860/Atlanta_NP/ajc_web_default/'
+  dfpId: '21849707860',
   siteDomainURL: '//www.ajc.com',
   twitterURL: '//twitter.com/ajc',
   facebookURL: '//facebook.com/ajc',
