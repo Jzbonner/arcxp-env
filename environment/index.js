62317235d83aef37eb6196313c86f06644f4b5d7
Dave Young <dave.young@ajc.com>
2020-01-15 12:42:43 -0500

    fixes to overzealous linter adjustments

--- environment/index.js
+++ environment/index.js
@@ -9,5 +9,5 @@ export default {
     containerId: 'GTM-W3VLHBK',
   },
   RESIZER_SECRET_KEY:
-    '%{AQICAHhcROb/QmeRxNIO1Wz8fwobT+WGv8awlNGG/lOIbIy/mwEez1nS5cgmYn+WwvfOtJ9mAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMX9nUHv48dsfY82ohAgEQgDvEqH0qqPQAuvA9ys76q7ZorQQJk89OUMORKLMiDnVG/mOgm+rxvg7zS/udLNZMsi+S66Gybe3UmRTWSQ==}',
+    '%{AQICAHhcROb/QmeRxNIO1Wz8fwobT+WGv8awlNGG/lOIbIy/mwEez1nS5cgmYn+WwvfOtJ9mAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMX9nUHv48dsfY82ohAgEQgDvEqH0qqPQAuvA9ys76q7ZorQQJk89OUMORKLMiDnVG/mOgm+rxvg7zS/udLNZMsi+S66Gybe3UmRTWSQ==}', // eslint-disable-line max-len
 };
