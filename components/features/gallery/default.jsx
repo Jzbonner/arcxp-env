6b8a4a04bb11926dd6a812e7173155c2d0eaa590
grincon0 <georgerincon2@gmail.com>
2020-01-17 16:53:13 -0500

    ADP-77/resolvedLintingErrors-George

--- components/features/gallery/default.jsx
+++ components/features/gallery/default.jsx
@@ -67,13 +67,9 @@ const Gallery = (props) => {
       const galleryFullWidth = galleryEl.current.offsetWidth;
       const focusElement = document.getElementById(`gallery-item-${currentIndex}`) || null;
       if (galleryEl && focusElement) {
-        console.log('full width of gallery', galleryFullWidth);
-
-        console.log('item offset', focusElement.offsetWidth);
         const translateAmount = parseInt(galleryFullWidth, 10)
           / 2 - parseInt(focusElement.offsetWidth, 10)
           / 2 - parseInt(focusElement.offsetLeft, 10);
-          console.log(translateX, translateAmount);
         if (translateX !== translateAmount) setTranslateX(translateAmount);
       }
     }
