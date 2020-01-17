901ffba69194943102996c04d4495d729e419c75
lcopeland-AJC <lisa.copeland@ajc.com>
2020-01-17 17:34:30 -0500

    APD-34/interstitialLinkStyling-Lisa

--- components/_helper_components/article/contentElements/components/interstitial_link/default.jsx
+++ components/_helper_components/article/contentElements/components/interstitial_link/default.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.scss';
 
-const InterstitialLink = ({ src }) => (
-    <div className="b-margin-bottom-d60-m40">
-      <p>Content Element Type: <strong>Interstitial Link</strong> Not Worked. Content: {src.content}</p>
+const InterstitialLink = ({ src }) => {
+  if (!(src)) return null;
+
+  return (
+    <div className="borders b-margin-bottom-d60-m40">
+      <div className="prefix-box">
+        <span className="prefix">Explore</span>
+      </div>
+      <div className="interstitial">
+        <a
+          className="link"
+          href={'https://www.google.com/'}
+          rel="noopener noreferrer"
+          target="_blank"
+          dangerouslySetInnerHTML={{ __html: src.content }}
+        />
+      </div>
     </div>
-);
+  );
+};
 
 InterstitialLink.propTypes = {
-  src: PropTypes.object,
+  src: PropTypes.string,
 };
 
 export default InterstitialLink;
