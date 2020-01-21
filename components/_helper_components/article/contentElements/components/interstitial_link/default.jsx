c9b3b3b291a907769c0f9d98ae7633c49fd87e73
lcopeland-AJC <lisa.copeland@ajc.com>
2020-01-21 10:13:21 -0500

    APD-34/updatedStyles

--- components/_helper_components/article/contentElements/components/interstitial_link/default.jsx
+++ components/_helper_components/article/contentElements/components/interstitial_link/default.jsx
@@ -3,22 +3,17 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const InterstitialLink = ({ src }) => {
-  if (!(src)) return null;
-
+  if(!(src)) return null;
   return (
     <div className="borders b-margin-bottom-d60-m40">
-      <div className="prefix-box">
-        <span className="prefix">Explore</span>
-      </div>
-      <div className="interstitial">
-        <a
-          className="link"
-          href={'https://www.google.com/'}
-          rel="noopener noreferrer"
-          target="_blank"
-          dangerouslySetInnerHTML={{ __html: src.content }}
-        />
-      </div>
+      <span className="prefix">Explore</span>
+      <a
+        className="link"
+        href={'https://www.google.com/'}
+        rel="noopener noreferrer"
+        target="_blank">
+        {src.content}
+      </a>
     </div>
   );
 };
