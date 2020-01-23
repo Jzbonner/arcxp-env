ff327d56ced7e41276ece35e4dfa609d4d8c40ac
Dave Young <dave.young@ajc.com>
2020-01-23 11:18:28 -0500

    [FEATURE] adding fb:pages metatag to the output (and also adding some protection around taboola, for undefined-content cases)

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -1,33 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import getProperties from 'fusion:properties';
+import { fbPagesId } from 'fusion:environment';
 import TaboolaFooter from '../features/taboolaFeed/taboolaFooter.jsx';
 import TaboolaHeader from '../features/taboolaFeed/taboolaHeader.jsx';
 
 const DefaultOutputType = (props) => {
   const {
-    arcSite = getProperties().sites[0], children, contextPath, deployment, CssLinks, Fusion, Libs, MetaTags, globalContent,
+    arcSite = getProperties().sites[0],
+    children,
+    contextPath,
+    deployment,
+    CssLinks,
+    Fusion,
+    Libs,
+    MetaTags,
+    globalContent,
   } = props;
-  const {
-    type,
-  } = globalContent;
+  if (globalContent) {
+    const {
+      type,
+    } = globalContent;
+  }
+  const contentType = typeof type !== 'undefined' ? type : null;
 
   return (
     <html>
       <head>
-        <title>Fusion Article</title>
+        <title>{arcSite}</title>
         <MetaTags />
         <Libs />
         <CssLinks />
-        <TaboolaHeader type={type}/>
+        {contentType &&
+          <TaboolaHeader type={contentType}/>
+        }
         <link rel="stylesheet" href={deployment(`${contextPath}/resources/dist/${arcSite}/css/style.css`)} />
         <link rel="icon" type="image/x-icon" href={deployment(`${contextPath}/resources/favicon.ico`)} />
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="fb:pages" content={fbPagesId} />
       </head>
       <body>
         <div id="fusion-app">{children}</div>
         <Fusion />
-        <TaboolaFooter type ={type}/>
+        {contentType &&
+          <TaboolaFooter type={contentType}/>
+        }
       </body>
     </html>
   );
