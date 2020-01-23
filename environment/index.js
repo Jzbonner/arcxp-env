e91bedbf0633b35a761f2151a9d83b206eecf771
Dave Young <dave.young@ajc.com>
2020-01-23 11:17:41 -0500

    [FEATURE] adding fbPagesId to environmental properties

--- environment/index.js
+++ environment/index.js
@@ -6,6 +6,7 @@ export default {
     moapPTD: `[1110596, 1099909, // ajc qa/prod
     ]`,
   },
+  fbPagesId: '13310147298', // ajc
   RESIZER_SECRET_KEY:
     '%{AQICAHhcROb/QmeRxNIO1Wz8fwobT+WGv8awlNGG/lOIbIy/mwEez1nS5cgmYn+WwvfOtJ9mAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMX9nUHv48dsfY82ohAgEQgDvEqH0qqPQAuvA9ys76q7ZorQQJk89OUMORKLMiDnVG/mOgm+rxvg7zS/udLNZMsi+S66Gybe3UmRTWSQ==}',
 };
