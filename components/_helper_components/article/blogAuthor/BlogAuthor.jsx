7d6bc599496d2fd8484e51ca6a24d935503c2b8a
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-01-31 13:53:39 -0500

    APD-22/BlogPostQAChanges-Nicholas

--- components/_helper_components/article/blogAuthor/BlogAuthor.jsx
+++ components/_helper_components/article/blogAuthor/BlogAuthor.jsx
@@ -4,14 +4,15 @@ import './styles.scss';
 
 const BlogAuthor = ({ subtype, authorData }) => {
   if (subtype === 'Blog' && authorData.length > 0) {
+    console.log(authorData);
     return (
       <div className="c-blogAuthor c-section b-margin-bottom-d60-m40">
         <p className="blogAuthor-title">About the Author{authorData.length > 1 ? 's' : ''}</p>
         <div className="blogAuthor-content">
           {authorData.map((val, index) => (
             <React.Fragment key={`blog-author-${index}`}>
-              <div className="b-flexRow">
-                {val.image.url && (
+              <div className="b-flexRow blog-author-content-heading">
+                {val.image && val.image.url && (
                   <div className="content-profileImage">
                     <img src={val.image.url} alt={`${val.name}'s Profile Picture`} />
                   </div>
@@ -25,8 +26,8 @@ const BlogAuthor = ({ subtype, authorData }) => {
               )}
               {authorData.length < 2 && (
                 <div className="b-flexRow b-flexCenter">
-                  {val.social_links[2].url && <a className="btn-facebook" href={val.social_links[2].url} />}
-                  {val.social_links[3].url && <a className="btn-twitter" href={val.social_links[3].url} />}
+                  {val.social_links[1] && val.social_links[1].url && <a className="btn-facebook" href={val.social_links[1].url} />}
+                  {val.social_links[2] && val.social_links[2].url && <a className="btn-twitter" href={val.social_links[2].url} />}
                 </div>
               )}
             </React.Fragment>
