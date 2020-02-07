f3066f63f63b8af159e0b3af4385363b86bf24bb
Ilkov <silkov@NATL2236LSILKOV.local>
2020-02-07 09:45:49 -0500

    APD-26/WidthCheckerAndNewScenario-Samuil

--- components/_helper_components/global/video/default.jsx
+++ components/_helper_components/global/video/default.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Caption from '../caption/default.jsx';
 import './default.scss';
@@ -9,11 +9,53 @@ const Video = ({ src, isLeadVideo, videoPlayerRules }) => {
   const { url: videoPlayer } = src.streams && src.streams[0] ? src.streams[0] : null;
   const { startPlaying, muteON } = videoPlayerRules || null;
 
+  const checkWindowSize = () => {
+    const isClient = typeof window === 'object';
+
+    const getSize = () => ({
+      width: isClient ? window.innerWidth : undefined,
+    });
+
+    const [windowSize, setWindowSize] = useState(getSize);
+
+    useEffect(() => {
+      if (!isClient) {
+        return false;
+      }
+      const handleResize = () => {
+        setWindowSize(getSize());
+      };
+
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    });
+
+    return windowSize;
+  };
+
+  const screenSize = checkWindowSize();
+  // console.log('SCREEN SIZE', screenSize);
+
   let mainCredit = {};
   if (credits) {
     mainCredit = credits.affiliation && credits.affiliation[0].name ? credits.affiliation[0].name : '';
   }
   const giveCredit = mainCredit.length > 1 ? `Credit: ${mainCredit}` : '';
+
+  const smartChecker = () => {
+    if (isLeadVideo && !giveCredit && !videoCaption) {
+      return null;
+    }
+    if (isLeadVideo && giveCredit && !videoCaption && screenSize.width < 1024) {
+      return <Caption src={src} isLeadVideo videoCaption={videoCaption} />;
+    }
+
+    if (isLeadVideo && giveCredit && !videoCaption && screenSize.width > 1024) {
+      return null;
+    }
+
+    return <Caption src={src} isLeadVideo videoCaption={videoCaption} />;
+  };
 
   return (
     <div className="c-video-component">
@@ -22,7 +64,7 @@ const Video = ({ src, isLeadVideo, videoPlayerRules }) => {
           <source src={videoPlayer} type="video/mp4" />
         </video>
       </div>
-      {isLeadVideo && !videoCaption && !giveCredit ? null : <Caption src={src} isLeadVideo videoCaption={videoCaption}/>}
+      {smartChecker()}
       <p className="video-credit-text">{giveCredit}</p>
     </div>
   );
