750c68220d03222022b75964b5caa75794b79b27
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-02-11 09:27:25 -0500

    APD-97/FinishedBuildOfFooterAndCopyright-Nicholas

--- components/_helper_components/footer/footer.jsx
+++ components/_helper_components/footer/footer.jsx
@@ -6,6 +6,8 @@ import ajcLogo from '../../../resources/images/ajc-logo.svg';
 import facebookIcon from '../../../resources/images/facebook-icon.svg';
 import twitterIcon from '../../../resources/images/twitter-icon.svg';
 import roundButton from '../../../resources/images/round-button.svg';
+import getLinkURL from '../../layouts/_helper_functions/getLinkUrl';
+import Copyright from '../copyright/copyright';
 
 const Footer = () => {
   const siteContent = useContent({
@@ -17,14 +19,6 @@ const Footer = () => {
 
   const { children } = siteContent || {};
   const [row1 = []] = children || [];
-
-  const getURL = (link) => {
-    if (link._id.includes('configsection') && link.site.site_url) {
-      // not sure about best default behavior if no site url is set for a configsection
-      return link.site.site_url;
-    }
-    return link._id;
-  };
 
   const [openMenu, setOpenMenu] = useState('');
 
@@ -38,35 +32,37 @@ const Footer = () => {
 
   return (
     <footer className="c-footer">
-      <div className="footer-row-1">
+      <div className="logo-row">
         <div className="col">
           <img className="logo" src={ajcLogo} alt="logo" />
         </div>
-        <a href="#" className="col newsletter-signup">
-          {row1.navigation.nav_title}
-          <img src={roundButton} alt="" />
-        </a>
+        <div className="col">
+          <a href="https://myaccount.ajc.com/ajc/preference" className="newsletter-signup">
+            <p>{row1.navigation && row1.navigation.nav_title}</p>
+            <img src={roundButton} alt="" />
+          </a>
+        </div>
       </div>
-      <div className="footer-row-2">
-        <ul className="menus">
-          {children.map((parent, i) => {
+      <ul className="menu-row">
+        {children
+          && children.map((parent, i) => {
             if (parent.children.length > 0) {
               return (
                 <li
                   key={`footerParentLink-${i}`}
-                  className={`col-header ${openMenu === parent.name ? 'is-visible-mobile' : ''}`}
+                  className={`menu ${openMenu === parent.name ? 'is-visible-mobile' : ''}`}
                   onClick={() => handleClick(parent.name)}
                 >
-                  <div className="header-text">
+                  <div className="menu-header">
                     {parent.name}
                     <img className="menu-arrow" src={menuArrow} alt="" />
                   </div>
 
-                  <ul className="childlist">
+                  <ul className="menu-body">
                     {parent.children
                       && parent.children.map((child, e) => (
-                        <li key={`footerChildLink-${e}`} className="col-links">
-                          <a href={getURL(child)}>{child.name}</a>
+                        <li key={`footerChildLink-${e}`} className="menu-body-links">
+                          <a href={getLinkURL(child)}>{child.name}</a>
                         </li>
                       ))}
                   </ul>
@@ -75,23 +71,23 @@ const Footer = () => {
             }
             return null;
           })}
-          <li className="col-header social-media">
-            <span className="header-text">Follow</span>
-            <ul className="social-media-icons">
-              <li>
-                <a href="https://www.facebook.com/ajc">
-                  <img src={facebookIcon} alt="" />
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/ajc">
-                  <img src={twitterIcon} alt="" />
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+        <li className="menu social-media">
+          <span className="header-text">Follow</span>
+          <ul className="social-media-icons">
+            <li>
+              <a href="https://www.facebook.com/ajc">
+                <img src={facebookIcon} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/ajc">
+                <img src={twitterIcon} alt="" />
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <Copyright />
     </footer>
   );
 };
