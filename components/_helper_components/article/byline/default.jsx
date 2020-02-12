a9880f2cd9a2930fabf8d1d6737723e5495a2b63
grincon0 <georgerincon2@gmail.com>
2020-02-12 15:14:27 -0500

    APD-58/cleanedUpCode-George

--- components/_helper_components/article/byline/default.jsx
+++ components/_helper_components/article/byline/default.jsx
@@ -9,24 +9,16 @@ const Byline = ({ by = [] }) => {
     // cleans up authors array items
 
     const authorsAndOrgs = authors.map((author) => {
-      if (author.affiliations) {
-        return {
-          name: author.name,
-          org: author.affiliations,
-        };
-      }
-      if (author.org && !author.affiliations) {
-        return {
-          name: author.name,
-          org: author.org,
-        };
-      }
-      if (!author.org && !author.affiliations) {
-        return {
-          name: author.name,
-          org: null,
-        };
-      }
+      if (!author.name) return null;
+
+      // staff
+      if (author.affiliations) return { name: author.name, org: author.affiliations };
+
+      // external
+      if (author.org && !author.affiliations) return { name: author.name, org: author.org };
+
+      // unknown org
+      if (!author.org && !author.affiliations) return { name: author.name, org: null };
 
       return null;
     });
@@ -41,7 +33,7 @@ const Byline = ({ by = [] }) => {
         if (author.name) {
           return <span key={author.name}>{i === 0 && 'By '}<a href="#">{author.name}</a>{author.org ? `  -  ${author.org}` : ''}</span>;
         }
-      // only one author
+        // only one author
       } else if (authors.length === 1) {
         if (author.name) {
           return <span key={author.name}>{i === 0 && 'By '}<a href="#">{author.name}</a>{author.org ? `,  ${author.org}` : ''}</span>;
@@ -59,23 +51,13 @@ const Byline = ({ by = [] }) => {
   };
 
   if (by.length > 0) {
-    let organization = null;
-    const authors = by.map((element) => {
-      if (!organization && (typeof element.org !== 'undefined' || element.org !== '')) {
-        organization = {
-          orgName: element.org,
-        };
-      }
-
-      return {
-        name: element.name,
-        org: element.org,
-        affiliations: element.additional_properties
-          && element.additional_properties.original
-          && element.additional_properties.original.affiliations,
-      };
-    });
-
+    const authors = by.map(element => ({
+      name: element.name,
+      org: element.org,
+      affiliations: element.additional_properties
+        && element.additional_properties.original
+        && element.additional_properties.original.affiliations,
+    }));
     finalizeByline(authors);
   }
 
