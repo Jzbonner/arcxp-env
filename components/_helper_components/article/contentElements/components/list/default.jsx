8ed0bb32cab08730f9d3676b53bba07090ce3e90
lcopeland-AJC <lisa.copeland@ajc.com>
2020-02-13 12:23:15 -0500

    APD-36/nestedListWorking-Lisa

--- components/_helper_components/article/contentElements/components/list/default.jsx
+++ components/_helper_components/article/contentElements/components/list/default.jsx
@@ -2,63 +2,47 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import SubList from '../list/default';
 import './styles.scss';
 
-const listItems = {
-  unordered: 'ul',
-  ordered: 'ol',
-  ul: 'ul',
-  ol: 'ol',
-};
-
-const isList = (item) => {
-  const { type } = item;
-  return type === 'list';
-};
-
-const renderListItem = (item, index, nextItem = {}) => {
-  const { content } = item;
-  const properListType = listItems[nextItem.list_type];
-  const nextItemIsAList = isList(nextItem);
-
-  return (
-      <li key={index}>
-        <span dangerouslySetInnerHTML={{ __html: content }} />
-        {nextItemIsAList ? (
-          <List
-            key={index}
-            items={nextItem.items}
-            listType={properListType}
-          />
-        ) : null}
-      </li>
-  );
-};
-
 const List = (props) => {
-  // console.log('list props', props);
   const { src = {} } = props;
   const { items = [], list_type: listType } = src;
-  // console.log(listType);
   if (!items.length) return null;
 
-  const ListType = listItems[listType] || listType;
+  if (props.src.list_type === 'unordered') {
+    return (
+      <div className="list">
+        <ul>
+          {props.src.items.map((e) => {
+            if (e.type === 'text') {
+              return <li>{e.content}</li>;
+            }
+            // eslint-disable-next-line react/jsx-key
+            return <SubList src={e} />;
+          })}
+        </ul>
+      </div>
 
-  return (
-    <div className="list">
-    <ListType>
-      {items.map((item, index) => {
-        if (isList(item) && items[index - 1] && !isList(items[index - 1])) return null;
-        if (isList(item)) {
-          return <List listType={listItems[item.list_type] || 'ul'} items={item.items} />;
-        }
-        const nextItem = items[index + 1];
-        return renderListItem(item, index, nextItem);
-      })}
-    </ListType>
-    </div>
-  );
+    );
+  }
+  if (listType === 'ordered') {
+    return (
+      <div className="list">
+        <ol>
+          {props.src.items.map((e) => {
+            if (e.type === 'text') {
+              return <li>{e.content}</li>;
+            }
+            // eslint-disable-next-line react/jsx-key
+            return <SubList src={e} />;
+          })}
+        </ol>
+      </div>
+
+    );
+  }
+  return null;
 };
 
 List.propTypes = {
