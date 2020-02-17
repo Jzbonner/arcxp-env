4b551ca7bc95f3238094fcb61da825c717cc8585
lcopeland-AJC <lisa.copeland@ajc.com>
2020-02-17 12:28:18 -0500

    APD-217/updatedGlobalMeta-Lisa

--- components/_helper_components/global/siteMeta/default.jsx
+++ components/_helper_components/global/siteMeta/default.jsx
@@ -1,50 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line arrow-body-style
 const SiteMeta = () => {
   return (
     <>
-        <link rel="apple-touch-icon" href="" />
-        <link rel="shortcut icon" href="" />
-        <link rel="canonical" href="" />
+        <link rel='apple-touch-icon' href='' />
+        <link rel='shortcut icon' href='' />
+        <link rel='canonical' href='' />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="" />
-        <meta name="twitter:site" content="@ajc" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:url" content="" />
-        <meta property="og:image" content="" />
-        <meta property="og:image:height" content="200" />
-        <meta property="og:image:width" content="200" />
-        <meta property="og:title" content="" />
-        <meta property="og:type" content="" />
-        <meta property="og:url" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:site_name" content="" />
-        <meta name="description" content="" />
-        <meta name="language" content="English" />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:description' content='' />
+        <meta name='twitter:image' content='' />
+        <meta name='twitter:site' content='@ajc' />
+        <meta name='twitter:title' content='' />
+        <meta name='twitter:url' content='' />
+        <meta property='og:image' content='' />
+        <meta property='og:image:height' content='200' />
+        <meta property='og:image:width' content='200' />
+        <meta property='og:title' content='' />
+        <meta property='og:type' content='' />
+        <meta property='og:url' content='' />
+        <meta property='og:description' content='' />
+        <meta property='og:site_name' content='' />
+        <meta name='description' content='' />
+        <meta name='language' content='English' />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="" />
-        <meta name="twitter:site" content="@ajc" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:url" content="" />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:description' content='' />
+        <meta name='twitter:image' content='' />
+        <meta name='twitter:site' content='@ajc' />
+        <meta name='twitter:title' content='' />
+        <meta name='twitter:url' content='' />
 
-        <meta property="og:image" content="" />
-        <meta property="og:image:height" content="" />
-        <meta property="og:image:width" content="" />
-        <meta property="og:title" content="" />
-        <meta property="og:type" content="article" />
-        <meta property="og:url" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:site_name" content="" />
+        <meta property='og:image' content='' />
+        <meta property='og:image:height' content='' />
+        <meta property='og:image:width' content='' />
+        <meta property='og:title' content='' />
+        <meta property='og:type' content='article' />
+        <meta property='og:url' content='' />
+        <meta property='og:description' content='' />
+        <meta property='og:site_name' content='' />
 
-        <meta name="thumbnail" content="" />
-        <meta name="language" content="English" />
+        <meta name='thumbnail' content='' />
+        <meta name='language' content='English' />
     </>
   );
 };
 
 export default SiteMeta;
+
+SiteMeta.propTypes = {
+  metaValue: PropTypes.func,
+};
