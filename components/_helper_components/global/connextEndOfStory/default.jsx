27c9a8024f7b8db50f9dba27145dea350b2cb8ac
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-02-18 13:01:32 -0500

    noTicket/updatedConnexDummyText-Nicholas

--- components/_helper_components/global/connextEndOfStory/default.jsx
+++ components/_helper_components/global/connextEndOfStory/default.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './default.scss';
 
-const ConnextEndOfStory = () => <p className="c-connext eos-msg__nolog-or-nosub b-margin-bottom-d40-m20">sdfsdfsdf</p>;
+const ConnextEndOfStory = () => <p className="c-connext eos-msg__nolog-or-nosub b-margin-bottom-d40-m20">APD-21</p>;
 
 export default ConnextEndOfStory;
