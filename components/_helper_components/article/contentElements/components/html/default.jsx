3b661eeb089f69bc1788d9efe6e46c6fdc37ff14
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-02-21 12:30:47 -0500

    APD-33/PlaceUsedEffectAtTheTopLevel-Nicholas

--- components/_helper_components/article/contentElements/components/html/default.jsx
+++ components/_helper_components/article/contentElements/components/html/default.jsx
@@ -4,41 +4,31 @@ import PropTypes from 'prop-types';
 const HTML = ({ src }) => {
   const { content } = src || {};
 
-  // powers external scripts and iframes
-  const externalScriptFilter = /(?<=src=")[^"]*/gi;
-  const externalScripts = content.match(externalScriptFilter);
+  useEffect(() => {
+    // powers external scripts and iframes
+    const externalScriptFilter = /(?<=src=")[^"]*/gi;
+    const externalScripts = content.match(externalScriptFilter);
 
-  externalScripts.forEach((scriptSrc) => {
-    const imgFilter = /(.svg|.png|.jpg|.jpeg|.gif)/;
-    if (!scriptSrc.search(imgFilter)) {
-      useEffect(() => {
+    externalScripts.forEach((scriptSrc) => {
+      const imgFilter = /(.svg|.png|.jpg|.jpeg|.gif)/;
+      if (!scriptSrc.search(imgFilter)) {
         const script = document.createElement('script');
         script.src = scriptSrc;
         script.async = true;
         document.body.appendChild(script);
+      }
+    });
 
-        return () => {
-          document.body.removeChild(script);
-        };
-      }, []);
-    }
-  });
+    // powers inline scripts
+    const internalScriptFilter = /(?<=<script>)([\s\S]*)(?=<\/script>)/gi;
+    const internalScripts = content.match(internalScriptFilter);
 
-  // powers inline scripts
-  const internalScriptFilter = /(?<=<script>)([\s\S]*)(?=<\/script>)/gi;
-  const internalScripts = content.match(internalScriptFilter);
-
-  internalScripts.forEach((scriptSrc) => {
-    useEffect(() => {
+    internalScripts.forEach((scriptSrc) => {
       const script = document.createElement('script');
       script.innerHTML = scriptSrc;
       document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
-      };
-    }, []);
-  });
+    });
+  }, []);
 
   if (!content) return null;
   return <div className="b-margin-bottom-d40-m20 html" dangerouslySetInnerHTML={{ __html: content }}></div>;
