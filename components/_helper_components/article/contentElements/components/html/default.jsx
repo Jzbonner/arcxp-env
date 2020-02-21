15d166056848956676c7d8927a606a74eb491a7b
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-02-21 12:19:09 -0500

    APD-33/AddedScriptSupportToHTMLElemenet-Nicholas

--- components/_helper_components/article/contentElements/components/html/default.jsx
+++ components/_helper_components/article/contentElements/components/html/default.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const HTML = ({ src }) => {
   const { content } = src || {};
+
+  // powers external scripts and iframes
+  const externalScriptFilter = /(?<=src=")[^"]*/gi;
+  const externalScripts = content.match(externalScriptFilter);
+
+  externalScripts.forEach((scriptSrc) => {
+    const imgFilter = /(.svg|.png|.jpg|.jpeg|.gif)/;
+    if (!scriptSrc.search(imgFilter)) {
+      useEffect(() => {
+        const script = document.createElement('script');
+        script.src = scriptSrc;
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+          document.body.removeChild(script);
+        };
+      }, []);
+    }
+  });
+
+  // powers inline scripts
+  const internalScriptFilter = /(?<=<script>)([\s\S]*)(?=<\/script>)/gi;
+  const internalScripts = content.match(internalScriptFilter);
+
+  internalScripts.forEach((scriptSrc) => {
+    useEffect(() => {
+      const script = document.createElement('script');
+      script.innerHTML = scriptSrc;
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }, []);
+  });
+
   if (!content) return null;
-  return <div className="b-margin-bottom-d40-m20" dangerouslySetInnerHTML={{ __html: content }}></div>;
+  return <div className="b-margin-bottom-d40-m20 html" dangerouslySetInnerHTML={{ __html: content }}></div>;
 };
 
 HTML.propTypes = {
