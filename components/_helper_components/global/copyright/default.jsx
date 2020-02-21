017d60242260727626b7ab3bf55c70d5a4d7f70c
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-02-21 13:23:12 -0500

    APD-97/ChangedLabelFromNameToNavTitle-Nicholas

--- components/_helper_components/global/copyright/default.jsx
+++ components/_helper_components/global/copyright/default.jsx
@@ -11,19 +11,23 @@ const Copyright = () => {
     },
   });
 
-
   const { children } = siteContent || {};
   const [ajc = [], visitor_agreement = [], privacy_policy = [], ad_choices = [], careers = []] = children || [];
+
+  const ajcName = ajc.navigation && ajc.navigation.nav_title;
+  const visitorAgreementName = visitor_agreement.navigation && visitor_agreement.navigation.nav_title;
+  const privacyPolicyName = privacy_policy.navigation && privacy_policy.navigation.nav_title;
+  const adChoicesName = ad_choices.navigation && ad_choices.navigation.nav_title;
+  const careersName = careers.navigation && careers.navigation.nav_title;
 
   const year = new Date().getFullYear();
 
   return (
     <div className="c-copyright">
-      &copy; {year} <a href={getLinkURL(ajc)}>{ajc.name}.</a> All Rights Reserved. By using this website, you accept the terms of our{' '}
-      <a href={getLinkURL(visitor_agreement)}>{visitor_agreement.name}</a> and{' '}
-      <a href={getLinkURL(privacy_policy)}>{privacy_policy.name}</a>, and understand your options regarding{' '}
-      <a href={getLinkURL(ad_choices)}>{ad_choices.name}</a>. Learn about <a href={getLinkURL(careers)}>{careers.name}</a> at Cox
-      Enterprises.
+      &copy; {year} <a href={getLinkURL(ajc)}>{ajcName}.</a> All Rights Reserved. By using this website, you accept the terms of our{' '}
+      <a href={getLinkURL(visitor_agreement)}>{visitorAgreementName}</a> and <a href={getLinkURL(privacy_policy)}>{privacyPolicyName}</a>,
+      and understand your options regarding <a href={getLinkURL(ad_choices)}>{adChoicesName}</a>. Learn about{' '}
+      <a href={getLinkURL(careers)}>{careersName}</a> at Cox Enterprises.
     </div>
   );
 };
