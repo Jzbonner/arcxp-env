a7a7e9ea239b679e699c85c02709b5d87147735d
Dave Young <dave.young@ajc.com>
2020-02-28 10:43:56 -0500

    [APD-217] correcting thumbnail logic for inlines, plus making width of og:image respect logo fallback

--- components/layouts/_helper_functions/getFeaturedImage.js
+++ components/layouts/_helper_functions/getFeaturedImage.js
@@ -15,9 +15,30 @@ const renderImage = () => {
   const { url: featuredImage } = promoItems ? promoItems.basic : {};
   const { url: videoThumbnail } = promoItems && promoItems.basic.promo_image ? promoItems.basic.promo_image : {};
   const { url: galleryThumbnail } = promoItems && promoItems.basic.promo_items ? promoItems.basic.promo_items.basic : {};
-  const { url: inlineImage } = contentElements && contentElements[0] ? contentElements[0] : {};
-  const { url: inlineVideoThumbnail } = contentElements
-  && contentElements[0] && contentElements[0].promo_image ? contentElements[0].promo_image : {};
+  let inlineImage = null;
+  let inlineVideoThumbnail = null;
+
+  // if there isn't any featured media, we parse through the contentElements to check for inline images and/or videos
+  if (!featuredImage && !videoThumbnail && !galleryThumbnail && contentElements) {
+    let foundFirstInline = false;
+    contentElements.forEach((el) => {
+      if (!foundFirstInline) {
+        const {
+          type,
+          url = null,
+          promo_items: inlinePromoItems = {},
+        } = el;
+        if (type === 'image') {
+          inlineImage = url;
+          foundFirstInline = true;
+        }
+        if (type === 'video' && inlinePromoItems.basic && inlinePromoItems.basic.url) {
+          inlineVideoThumbnail = inlinePromoItems.basic.url;
+          foundFirstInline = true;
+        }
+      }
+    });
+  }
 
   if (featuredImage) {
     return featuredImage;
