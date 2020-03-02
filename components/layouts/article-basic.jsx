a8c75f9d48cf1ccd55421f8b2cfab4c0c27a4c9d
lcopeland-AJC <lisa.copeland@ajc.com>
2020-03-02 10:09:12 -0500

    noTicket/updatedDividerPlacement-Lisa

--- components/layouts/article-basic.jsx
+++ components/layouts/article-basic.jsx
@@ -23,6 +23,7 @@ import '../../src/styles/container/_article-basic.scss';
 import '../../src/styles/base/_utility.scss';
 import filterContentElements from './_helper_functions/article/filterContentElements';
 import ConnextEndOfStory from '../_helper_components/global/connextEndOfStory/default';
+import Divider from '../../components/_helper_components/article/contentElements/components/divider/default';
 
 const RP01StoryDesktop = () => <ArcAd staticSlot={'RP01-Story-Desktop'} />;
 const RP01StoryTablet = () => <ArcAd staticSlot={'RP01-Story-Tablet'} />;
@@ -66,6 +67,7 @@ const StoryPageLayout = () => {
   const maxNumberOfParagraphs = paragraphCounter(filteredContentElements);
   const stop = maxNumberOfParagraphs === 4 ? 4 : 5;
 
+  const DividerInfo = () => <Divider />;
   const ConnextEndStory = () => <ConnextEndOfStory />;
   const BlogAuthorComponent = () => <BlogAuthor subtype={subtype} authorData={authorData} />;
 
@@ -133,7 +135,7 @@ const StoryPageLayout = () => {
             startIndex={stop}
             rightRail={{ insertBeforeParagraph: 8, ad: RP09StoryDesktop }}
             insertedAds={[{ insertAfterParagraph: 8, adArray: [RP09StoryTablet, MP03] }]}
-            insertAtSectionEnd={[BlogAuthorComponent, ConnextEndStory]}
+            insertAtSectionEnd={[BlogAuthorComponent, ConnextEndStory, DividerInfo]}
           />
 
           <Nativo elements={filteredContentElements} controllerClass="story-nativo_placeholder--boap" />
