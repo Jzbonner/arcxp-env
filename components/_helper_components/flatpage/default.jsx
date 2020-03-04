8b4ea93effb50a2571ab05d6e9ced556213e9d77
Carlos Morales <carlos.rey.morales@gmail.com>
2020-03-04 11:41:01 -0500

    APD-232/IdeaForRenderingFlatPagesV2-Carlos

--- components/_helper_components/flatpage/default.jsx
+++ components/_helper_components/flatpage/default.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FlatPage = () => <h1>this is a flatpage</h1>;
+import Footer from '../global/footer/default';
+import NavBar from '../global/navBar/default';
+import StickyNav from '../article/stickyNav/default';
+
+import '../../../src/styles/container/_article-basic.scss';
+import '../../../src/styles/base/_utility.scss';
+
+const FlatPage = ({ globalContent }) => {
+  if (!globalContent) return null;
+  const {
+    first_publish_date: firstPublishDate,
+    display_date: displayDate,
+    content_elements: contentElements,
+    promo_items: promoItems,
+    subtype,
+    headlines,
+    label,
+    comments,
+    taxonomy,
+    canonical_url: articleURL,
+    subheadlines,
+    credits,
+    type,
+  } = globalContent || {};
+
+  return (
+    <>
+      <header className="c-nav">
+        <NavBar/>
+        <StickyNav
+          articleURL={articleURL}
+          headlines={headlines}
+          comments={comments}
+        />
+      </header>
+      <main>
+        <h2>Hi! Im a flatpage!!!</h2>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+FlatPage.propTypes = {
+  globalContent: PropTypes.object,
+};
 
 export default FlatPage;
--- components/layouts/article-basic.jsx
+++ components/layouts/article-basic.jsx
@@ -57,7 +57,7 @@ const StoryPageLayout = () => {
     type,
   } = globalContent || {};
 
-  if (subtype === 'Flatpage') return <FlatPage />;
+  if (subtype === 'Flatpage') return <FlatPage globalContent />;
 
   const { by: authorData } = credits || {};
   const { basic: basicItems } = promoItems || {};
