e39fad902575a72828290e174a2eed630ef1f2f4
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-03-10 11:40:02 -0400

    APD-319/AddedConnextHyperLocalPromo-Nicholas

---
+++ components/_helper_components/global/ConnextHyperLocalSubscription/ConnextHyperLocalSubscription.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ConnextHyperLocalSubscription = () => (
+  <p className="c-connext hyperlocal_promo b-margin-bottom-d40-m20">APD-319 Hyperlocal Subscription Promo</p>
+);
+
+export default ConnextHyperLocalSubscription;
