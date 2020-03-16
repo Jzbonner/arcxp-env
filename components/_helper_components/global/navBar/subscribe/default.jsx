4a2a0da1abb8453b67265a740300326dc66ead4b
lcopeland-AJC <lisa.copeland@ajc.com>
2020-03-16 16:02:00 -0400

    APD-20/addedClassBack-Lisa

--- components/_helper_components/global/navBar/subscribe/default.jsx
+++ components/_helper_components/global/navBar/subscribe/default.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../default.scss';
 
 const Subscribe = () => (
-  <div className="b-margin-bottom-d40-m20"></div>
+  <div className="connext-sub-bar b-margin-bottom-d40-m20"></div>
 );
 
 export default Subscribe;
