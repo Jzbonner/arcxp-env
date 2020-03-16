8ba23f2d0bc9c65da80d19a2af733129f27b5ab1
lcopeland-AJC <lisa.copeland@ajc.com>
2020-03-16 15:39:25 -0400

    APD-20/removedExtraClasses-Lisa

--- components/_helper_components/global/navBar/subscribe/default.jsx
+++ components/_helper_components/global/navBar/subscribe/default.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../default.scss';
 
 const Subscribe = () => (
-  <div className="connext-sub-bar eos-msg__nolog-or-nosub b-margin-bottom-d40-m20"></div>
+  <div className="b-margin-bottom-d40-m20"></div>
 );
 
 export default Subscribe;
