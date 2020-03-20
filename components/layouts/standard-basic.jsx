3269951af5375f9843632855ed0694d2dc175b26
lcopeland-AJC <lisa.copeland@ajc.com>
2020-03-20 15:08:26 -0400

    APD-296/AddStandardLayout-Lisa

---
+++ components/layouts/standard-basic.jsx
@@ -0,0 +1,40 @@
+/*  /components/layouts/article-basic.jsx  */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import GlobalAdSlots from '../_helper_components/global/ads/default';
+import BreakingNews from '../_helper_components/global/breakingNews/default';
+import NavBar from '../_helper_components/global/navBar/default';
+import SectionHome from '../_helper_components/home/SectionHome/SectionHome';
+import Footer from '../_helper_components/global/footer/default';
+
+const StandardLayout = (props) => {
+  const [zone1, zone2, zone3, zone4, zone5] = props.children;
+
+  const Ad = () => <div>Placeholder Ad</div>;
+
+  return (
+    <>
+      <GlobalAdSlots />
+      <BreakingNews />
+      <NavBar />
+      <main className="c-standardContent">
+        <SectionHome feature={zone1} />
+        <SectionHome feature={zone2} rightRailAd={Ad} />
+        <SectionHome feature={zone3} />
+        <SectionHome feature={zone4} rightRailAd={Ad} />
+        <SectionHome feature={zone5} />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+StandardLayout.sections = ['Zone 1', 'Zone 2', 'Zone 3', 'Zone 4', 'Zone 5'];
+
+StandardLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default StandardLayout;
