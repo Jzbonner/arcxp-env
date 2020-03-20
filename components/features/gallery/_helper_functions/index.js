8d6294abe1d26b28143926c458c1984b45e4fb36
grincon0 <georgerincon2@gmail.com>
2020-03-20 14:43:09 -0400

    APD-110/removedUnusedCode-George

--- components/features/gallery/_helper_functions/index.js
+++ components/features/gallery/_helper_functions/index.js
@@ -2,4 +2,3 @@ export { default as debounce } from './debounce';
 export { default as createBaseGallery } from './createBaseGallery';
 export { default as reorganizeElements } from './reorganizeElements';
 export { default as handleImageFocus } from './handleImageFocus';
-export { default as insertGalleryAd } from './insertGalleryAd';
