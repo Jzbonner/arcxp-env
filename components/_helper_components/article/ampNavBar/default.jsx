b268bc99287e19e57e8adcdc6dd72ca0d0b0fb94
Carlos Morales <carlos.rey.morales@gmail.com>
2020-03-20 14:27:59 -0400

    APD-23/firstDraftOfAmpPage-Carlos

---
+++ components/_helper_components/article/ampNavBar/default.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './default.scss';
+
+const ampNavBar = () => <div className="amp-nav">Amp Nav Bar</div>;
+
+export default ampNavBar;
--- components/layouts/article-amp.jsx
+++ components/layouts/article-amp.jsx
@@ -1,6 +1,9 @@
 /*  /components/layouts/article-amp.jsx  */
 import React from 'react';
 import { useAppContext } from 'fusion:context';
+import AmpNavBar from '../_helper_components/article/ampNavBar/default';
+import Headline from '../_helper_components/article/headline/default.jsx';
+import TimeStamp from '../_helper_components/article/timestamp/default.jsx';
 
 const AmpPageLayout = () => {
   const appContext = useAppContext();
@@ -8,11 +11,32 @@ const AmpPageLayout = () => {
 
   if (!globalContent) return null;
   const {
-    canonical_url: articleURL,
+    promo_items: promoItems,
+    first_publish_date: firstPublishDate,
+    display_date: displayDate,
+    label,
+    headlines,
   } = globalContent || {};
 
+  const { basic: basicItems } = promoItems || {};
+  const { hide_timestamp: hideTimestamp } = label || {};
+  const { text: isHideTimestampTrue } = hideTimestamp || {};
+
   return (
-    <h1>{ articleURL }</h1>
+    <>
+      <AmpNavBar />
+      <main>
+        <header>
+          <Headline headlines={headlines} basicItems={basicItems} />
+          <div className="b-margin-bottom-d15-m10 c-label-wrapper b-pageContainer">
+            <TimeStamp firstPublishDate={firstPublishDate} displayDate={displayDate} isHideTimestampTrue={isHideTimestampTrue} />
+          </div>
+        </header>
+        <article>
+
+        </article>
+      </main>
+    </>
   );
 };
 
