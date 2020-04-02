c8abf60deba5e4bc8afc4a8a0d8d76621faca95e
Dave Young <dave.young@ajc.com>
2020-04-01 20:28:44 -0400

    adding styles for 50/50 cols (and enhancing classname logic)

--- components/_helper_components/home/SectionHome/SectionHome.jsx
+++ components/_helper_components/home/SectionHome/SectionHome.jsx
@@ -3,16 +3,19 @@ import PropTypes from 'prop-types';
 
 const SectionHomepage = ({ feature, rightColContent, rightRailContent }) => {
   if (feature || rightColContent || rightRailContent) {
-    let parentClass = 'c-section c-sectionHome b-margin-bottom-d40-m20';
-    parentClass += rightColContent ? 'halves' : '';
-    parentClass += rightRailContent ? 'with-rightRail' : 'full-width';
+    let parentClass = 'c-section c-sectionHome b-margin-bottom-d40-m20 ';
+    if (rightColContent) {
+      parentClass += ' halves';
+    } else {
+      parentClass += rightRailContent ? ' with-rightRail' : ' full-width';
+    }
     return (
       <div className={parentClass}>
         <div className="c-contentElements">
           {feature}
         </div>
-        {rightRailContent && <div className="c-rightRail">{rightRailContent}</div>}
         {rightColContent && <div className="c-contentElements">{rightColContent}</div>}
+        {rightRailContent && <div className="c-rightRail">{rightRailContent}</div>}
       </div>
     );
   }
