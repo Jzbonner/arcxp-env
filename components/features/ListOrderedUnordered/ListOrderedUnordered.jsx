d9c0e004537318127efd1d7012f5cff7b5a3dfd9
Ilkov <silkov@NATL2236LSILKOV.attlocal.net>
2020-04-07 00:04:42 -0400

    APD-281/OrderedAndUnorderedListFeature-Samuil

--- components/features/ListOrderedUnordered/ListOrderedUnordered.jsx
+++ components/features/ListOrderedUnordered/ListOrderedUnordered.jsx
@@ -1,8 +1,8 @@
-/* eslint-disable */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useContent } from 'fusion:content';
+import truncateHeadline from '../../layouts/_helper_functions/homepage/truncateHeadline';
+import './default.scss';
 
 const ListOrderedUnordered = (customFields = {}) => {
   const {
@@ -10,7 +10,8 @@ const ListOrderedUnordered = (customFields = {}) => {
       content: { contentService = 'collections-api', contentConfigValues = { id: '' } } = {},
       displayClass = '',
       startIndex = 1,
-      itemLimit = 100,
+      itemLimit = 1,
+      title = '',
     },
   } = customFields;
 
@@ -19,12 +20,41 @@ const ListOrderedUnordered = (customFields = {}) => {
     query: contentConfigValues,
   });
 
-  return <div>Ordered or Un-ordered List Feature</div>;
+  if (data) {
+    const { content_elements: innerData } = data || {};
+    const index = startIndex - 1; // starting the array from 1st child
+    const limitResult = index + itemLimit; // making sure the limit adds the index, so it doesn't reduce number of results
+    const filteredData = innerData.slice(index, limitResult); // filtering data based on startIndex & itemLimit (user input)
+
+    if (innerData) {
+      return (
+        <div className={`c-${displayClass}`}>
+          {title && <div className="title">{title}</div>}
+          <ul className="c-list-box">
+            {filteredData.map((el, i) => {
+              const { basic: headline } = el.headlines ? el.headlines : '';
+              const { canonical_url: itemURL } = el || '';
+              const countOrdered = i + 1 > 9 ? i + 1 : `0${i + 1}`;
+              return (
+                <li className='list-item' key={i}>
+                  <span className={`item-index-${displayClass}`}>{displayClass === 'Ordered List' ? countOrdered : ''}</span>
+                  <a href={itemURL} className="item-title">
+                    {truncateHeadline(headline)}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    }
+  }
+  return null;
 };
 
 ListOrderedUnordered.propTypes = {
   customFields: PropTypes.shape({
-    content: PropTypes.contentConfig('collections','query-feed').tag({
+    content: PropTypes.contentConfig('collections').tag({
       name: 'Content',
     }),
     startIndex: PropTypes.number.tag({
@@ -38,6 +68,9 @@ ListOrderedUnordered.propTypes = {
     displayClass: PropTypes.oneOf(['Ordered List', 'Un-ordered List']).tag({
       name: 'Display Class',
       defaultValue: 'Ordered List',
+    }),
+    title: PropTypes.string.tag({
+      name: 'Title',
     }),
   }),
 };
