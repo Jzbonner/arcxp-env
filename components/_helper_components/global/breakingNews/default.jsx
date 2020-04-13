6ed5a475c0189574725ec633f0be0056ba25c401
Ilkov <silkov@NATL2236LSILKOV.local>
2020-04-13 12:35:55 -0400

    APD-160/integratingCarlos'Suggestion-Samuil

--- components/_helper_components/global/breakingNews/default.jsx
+++ components/_helper_components/global/breakingNews/default.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import getProperties from 'fusion:properties';
 import { useContent } from 'fusion:content';
-import './default.scss';
+import LiveVideo from './liveVideo/liveVideo';
+import RenderBreakingNews from './renderBreakingNews/renderBreakingNews';
 
 const BreakingNews = () => {
-  const { breakingNewsID, breakingLiveVideoID } = getProperties();
-  const [isVisible, setVisibility] = useState(true);
+  const { breakingNewsID } = getProperties();
 
   const breakingNewsData = useContent({
     source: 'collections-api',
     query: { id: `${breakingNewsID}` },
   });
-
-  const hideBar = () => {
-    setVisibility(false);
-  };
 
   const breakingNewsItem = breakingNewsData && breakingNewsData.content_elements[0] ? breakingNewsData.content_elements : false;
   let breakingHeadline;
@@ -25,28 +21,12 @@ const BreakingNews = () => {
     breakingHeadline = breakingNewsData && breakingNewsItem[0] ? breakingNewsItem[0].headlines.basic : '';
     breakingURL = breakingNewsItem && breakingNewsItem[0] ? breakingNewsItem[0].canonical_url : '';
     mainTitle = 'Breaking News';
-  } else {
-    const liveVideoData = useContent({
-      source: 'collections-api',
-      query: { id: `${breakingLiveVideoID}` },
-    });
-    const liveVideoItem = liveVideoData && liveVideoData.content_elements[0] ? liveVideoData.content_elements : [];
-    breakingHeadline = liveVideoData && liveVideoItem[0] ? liveVideoItem[0].headlines.basic : '';
-    breakingURL = liveVideoItem && liveVideoItem[0] ? liveVideoItem[0].canonical_url : '';
-    mainTitle = 'Live Video';
   }
 
   if (breakingHeadline) {
-    return (
-      <div className={`c-breakingNews ${!isVisible ? 'is-hidden' : ''}`}>
-        <a href={breakingURL} className="breakingURL"></a>
-        <span className="c-breakingNews-hide" onClick={hideBar}></span>
-        <h2 className="c-breakingNews-heading">{mainTitle}</h2>
-        <h2 className="c-breakingNews-content">{breakingHeadline}</h2>
-      </div>
-    );
+    return <RenderBreakingNews breakingHeadline={breakingHeadline} breakingURL={breakingURL} mainTitle={mainTitle} />;
   }
-  return null;
+  return <LiveVideo />;
 };
 
 export default BreakingNews;
