17824948a12a83b5398541967085701ca6d88410
Ilkov <silkov@NATL2236LSILKOV.local>
2020-04-13 14:39:40 -0400

    noTicket/addingArcImageResizerToHero-Samuil

--- components/features/Hero/default.jsx
+++ components/features/Hero/default.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useContent } from 'fusion:content';
 import { useAppContext } from 'fusion:context';
 import truncateHeadline from '../../layouts/_helper_functions/homepage/truncateHeadline';
+import imageResizer from '../../layouts/_helper_functions/Thumbor';
 import './default.scss';
 
 const Hero = (customFields = {}) => {
   const {
     customFields: { content: { contentService = 'collections-api', contentConfigValues = { id: '' } } = {}, startIndex = 1 },
   } = customFields;
+  const [imageSrc, setImageSrc] = useState('');
 
   const appContext = useAppContext();
   const { contextPath } = appContext;
@@ -17,26 +19,27 @@ const Hero = (customFields = {}) => {
     source: contentService,
     query: contentConfigValues,
   });
+  const { content_elements: innerData } = data || {};
+  const singleItem = innerData[startIndex] ? innerData[startIndex - 1] : null; // adding "-1" so the array index always starts from 0
+  const { url: heroBackground } = singleItem && singleItem.promo_items ? singleItem.promo_items.basic : '';
+  const { basic: headline } = singleItem && singleItem.headlines ? singleItem.headlines : '';
+  const { canonical_url: heroURL } = singleItem || '';
 
-  if (data) {
-    const { content_elements: innerData } = data || {};
-    const singleItem = innerData[startIndex] ? innerData[startIndex - 1] : null; // adding "-1" so the array index always starts from 0
-    const { url: heroBackground } = singleItem && singleItem.promo_items ? singleItem.promo_items.basic : '';
-    const { basic: headline } = singleItem && singleItem.headlines ? singleItem.headlines : '';
-    const { canonical_url: heroURL } = singleItem || '';
+  useEffect(() => {
+    setImageSrc(imageResizer(heroBackground));
+  }, []);
 
-    if (innerData && heroBackground) {
-      return (
-        <div className="c-heroFeature">
-          <a href={`${contextPath}${heroURL}`} className="hero-url" />
-          <div className="hero-img" style={{ backgroundImage: `url(${heroBackground})` }}>
-            <div className="hero-headline">
-              <h2 className="headline-text">{truncateHeadline(headline)}</h2>
-            </div>
+  if (innerData && heroBackground) {
+    return (
+      <div className="c-heroFeature">
+        <a href={`${contextPath}${heroURL}`} className="hero-url" />
+        <div className="hero-img" style={{ backgroundImage: `url(${imageSrc})`, backgroundRepeat: 'no-repeat' }}>
+          <div className="hero-headline">
+            <h2 className="headline-text">{truncateHeadline(headline)}</h2>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
   return null;
 };
