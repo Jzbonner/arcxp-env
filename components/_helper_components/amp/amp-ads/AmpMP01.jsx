5a9898e579aeaa670646d19c308398ed30ac5dfc
Carlos Morales <carlos.rey.morales@gmail.com>
2020-04-16 15:45:00 -0400

    APD-292/addedFirstRoughDraftOfMP01-Carlos

---
+++ components/_helper_components/amp/amp-ads/AmpMP01.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const AmpMP01 = () => (
+  <amp-ad
+    width="320"
+    height="50"
+    layout="fixed"
+    type="doubleclick"
+    data-loading-strategy ="1.25"
+    data-slot="21849707860/Atlanta_NP/ajc_web_default/news/coronavirus"
+    json='{"targeting":{"ad_slot":["MP01"], "breakpoint": ["1px"], "topics":["breaking-news.ajc, ALERT, ajc top news, atlanta-news-metro.ajc, Georgia news, Intown Atlanta, Gwinnett County, DeKalb County, Cobb County, Fulton County, coronavirus, ajc-mobile-app-pinned"], "uuid":["ea9edd9e-7fed-11ea-8541-d0be205715eb"], "obj_type":["story_amp"]}, "useSameDomainRenderingUntilDeprecated": 1}'>
+  </amp-ad>
+);
+
+export default AmpMP01;
