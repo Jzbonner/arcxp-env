d7f18a6922cc41fdc0ddae7c1c07b8ea2b1f60bd
Dave Young <dave.young@ajc.com>
2020-04-17 16:38:07 -0400

    moving env vars into site props

--- properties/sites/ajc.js
+++ properties/sites/ajc.js
@@ -10,5 +10,6 @@ export default {
     siteMetro: 'ga: atlanta',
     siteFormat: 'newspaper',
     gtmContainerKey: 'GTM-WT4CBT7',
+    dtmLibraryURL: '//assets.adobedtm.com/20d5deb47c9e7fe47a9f969db01578a739d7179d/satelliteLib-3efdda86084064a3aef164882aed6ee114106d39',
   },
 };
