fe3bac4267360b4d395066091cbba7d94898815f
Dave Young <dave.young@ajc.com>
2020-04-17 14:59:14 -0400

    adding GTM implementation to output type

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -20,18 +20,28 @@ const DefaultOutputType = (props) => {
     Fusion,
     globalContent,
     hyperlocalTags = getProperties().hyperlocalTags,
+    metrics = getProperties().metrics,
     Libs,
     MetaTags,
   } = props;
-  const { isEnabled = false, clientCode, environment } = connext;
+  const { isEnabled = false, clientCode, environment: connextEnv } = connext;
   const { type, taxonomy } = globalContent || { type: null };
   const { tags = [] } = taxonomy || {};
   const noAds = checkTags(tags, 'no-ads');
   const isHyperlocalContent = checkTags(tags, hyperlocalTags);
+  const includeGtm = metrics && metrics.gtmContainerKey;
 
   return (
     <html>
       <head>
+        {includeGtm && (
+          /* Google Tag Manager */
+          <script type='text/javascript' dangerouslySetInnerHTML={{
+            /* eslint-disable-next-line max-len */
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${metrics.gtmContainerKey}');`,
+          }}></script>
+          /* End Google Tag Manager */
+        )}
         <MetaTags />
         <SiteMeta />
         <Libs />
@@ -45,17 +55,25 @@ const DefaultOutputType = (props) => {
         <SiteMetrics />
       </head>
       <body>
+        {includeGtm && (
+          /* Google Tag Manager (noscript) */
+          <noscript dangerouslySetInnerHTML={{
+            /* eslint-disable-next-line max-len */
+            __html: `<iframe src='https://www.googletagmanager.com/ns.html?id=${metrics.gtmContainerKey}' height='0' width='0' style='display:none;visibility:hidden'></iframe>`,
+          }}></noscript>
+          /* End Google Tag Manager (noscript) */
+        )}
         <div id="fusion-app">{children}</div>
         <Fusion />
         {!isHyperlocalContent && type && <TaboolaFooter type={type} />}
         {isEnabled && (
           <>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-            <script type="text/javascript" src={`https://loader-cdn.azureedge.net/${environment}/${clientCode}/loader.min.js`}></script>
+            <script type="text/javascript" src={`https://loader-cdn.azureedge.net/${connextEnv}/${clientCode}/loader.min.js`}></script>
             <ConnextInit />
           </>
         )}
-         <div id="fb-root"></div>
+        <div id="fb-root"></div>
         <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"></script>
       </body>
     </html>
@@ -71,6 +89,7 @@ DefaultOutputType.propTypes = {
   Fusion: PropTypes.func,
   globalContent: PropTypes.object,
   hyperlocalTags: PropTypes.object,
+  metrics: PropTypes.object,
   Libs: PropTypes.array,
   MetaTags: PropTypes.object,
 };
