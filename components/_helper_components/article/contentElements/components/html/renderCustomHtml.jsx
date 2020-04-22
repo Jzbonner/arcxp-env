994f5a1c10a1483667283be1602474fc358c92d2
Dave Young <dave.young@ajc.com>
2020-04-22 16:06:31 -0400

    [APD-380] further enhancements to js handling

--- components/_helper_components/article/contentElements/components/html/renderCustomHtml.jsx
+++ components/_helper_components/article/contentElements/components/html/renderCustomHtml.jsx
@@ -7,24 +7,28 @@ const renderCustomHtml = (content) => {
 
   useEffect(() => {
     // powers all inline & external scripts in embedded html
-    const scriptFilter = /[^<script.+>][\s\S][^<]*<\/script>/gi;
-    const scripts = html.match(scriptFilter);
-    setHtml(html.replace(scriptFilter, ''));
+    if (html.search(/<script/gi) !== -1) {
+      // only proceed if it actually contains a script (added because <style /> blocks were triggering)
+      const scriptFilter = /[<script+?>\s\S]*/gi;
+      let scripts = html.match(scriptFilter);
+      setHtml(html.replace(scriptFilter, ''));
 
-    if (Array.isArray(scripts)) {
-      scripts.forEach((script) => {
-        const newScript = document.createElement('script');
-        const externalFilter = /src=(['"].+['"])/gi;
-        if (script.search(externalFilter) !== -1) {
-          let scriptSrc = script.match(externalFilter);
-          scriptSrc = scriptSrc[0].replace(/src=|['"]/g, '');
-          newScript.src = scriptSrc;
-        } else {
-          const scriptVal = script.replace(/<script.+?>|<\/script>/g, '');
-          newScript.innerHTML = scriptVal;
-        }
-        document.body.appendChild(newScript);
-      });
+      if (Array.isArray(scripts)) {
+        scripts = scripts.filter(script => script !== '');
+        scripts.forEach((script) => {
+          const newScript = document.createElement('script');
+          const externalFilter = /.*?src=['"](.*?)['"]/gmi;
+          if (script.search(externalFilter) !== -1) {
+            let scriptSrc = script.match(externalFilter);
+            scriptSrc = scriptSrc[0].replace(/.*?src=|['"]/gmi, '');
+            newScript.src = scriptSrc;
+          } else {
+            const scriptVal = script.replace(/<script.*?>|<\/script>/g, '');
+            newScript.innerHTML = scriptVal;
+          }
+          document.body.appendChild(newScript);
+        });
+      }
     }
   }, []);
 
