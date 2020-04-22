ee9b58cf52c2e6716273d3175dd1de68c0e63aef
Ilkov <silkov@NATL2236LSILKOV.attlocal.net>
2020-04-22 16:28:48 -0400

    APD-437/workingQueryFeedVersion-Samuil

--- content/sources/query-feed.js
+++ content/sources/query-feed.js
@@ -1,4 +1,5 @@
 const schemaName = 'query-feed';
+const bodybuilder = require('bodybuilder');
 
 const params = {
   includeDistributor: 'text',
@@ -11,10 +12,10 @@ const params = {
   excludeContentTypes: 'text',
   excludeSections: 'text',
   excludeTags: 'text',
-  exludeSubtypes: 'text',
+  excludeSubtypes: 'text',
 };
 
-export const itemsToArray = (itemString = '') => (itemString.length ? itemString.split(',').map(item => item.replace(/"/g, '')) : '?*');
+export const itemsToArray = (itemString = '') => itemString.split(',').map(item => item.replace(/"/g, ''));
 
 const resolve = (query) => {
   const {
@@ -28,127 +29,82 @@ const resolve = (query) => {
     includeTags = '',
     excludeTags = '',
     includeSubtypes = '',
-    exludeSubtypes = '',
+    excludeSubtypes = '',
   } = query;
 
-  const contentTypesIncluded = itemsToArray(includeContentTypes);
-  const contentTypesExcluded = itemsToArray(excludeContentTypes);
+  const builder = bodybuilder();
+  if (includeDistributor) {
+    const distributors = itemsToArray(includeDistributor);
+    distributors.forEach((distributor) => {
+      builder.orQuery('term', 'distributor.reference_id', distributor);
+    });
+  }
+  if (includeContentTypes) {
+    const types = itemsToArray(includeContentTypes);
+    types.forEach((type) => {
+      builder.orQuery('term', 'type', type);
+    });
+  }
+  if (includeSubtypes) {
+    const subtypes = itemsToArray(includeSubtypes);
+    subtypes.forEach((subtype) => {
+      builder.orQuery('term', 'subtype', subtype);
+    });
+  }
+  if (includeSections) {
+    const sections = itemsToArray(includeSections);
+    sections.forEach((section) => {
+      builder.orQuery('term', 'taxonomy.sections._id', section);
+    });
+  }
+  if (includeTags) {
+    if (mustIncludeAllTags.toLowerCase() === 'yes') {
+      const tags = itemsToArray(includeTags);
+      tags.forEach((tag) => {
+        builder.andQuery('term', 'taxonomy.tags.text', tag);
+      });
+    } else {
+      const tags = itemsToArray(includeTags);
+      tags.forEach((tag) => {
+        builder.orQuery('term', 'taxonomy.tags.text', tag);
+      });
+    }
+  }
+  if (excludeDistributor) {
+    const distributors = itemsToArray(excludeDistributor);
+    distributors.forEach((distributor) => {
+      builder.notQuery('term', 'distributor.reference_id', distributor);
+    });
+  }
+  if (excludeContentTypes) {
+    const types = itemsToArray(excludeContentTypes);
+    types.forEach((type) => {
+      builder.notQuery('term', 'type', type);
+    });
+  }
+  if (excludeSubtypes) {
+    const subtypes = itemsToArray(excludeSubtypes);
+    subtypes.forEach((subtype) => {
+      builder.notQuery('term', 'subtype', subtype);
+    });
+  }
+  if (excludeSections) {
+    const sections = itemsToArray(excludeSections);
+    sections.forEach((section) => {
+      builder.notQuery('term', 'taxonomy.sections._id', section);
+    });
+  }
+  if (excludeTags) {
+    const tags = itemsToArray(includeTags);
+    tags.forEach((tag) => {
+      builder.notQuery('term', 'taxonomy.tags.text', tag);
+    });
+  }
+  const body = builder.build();
+  const newBody = JSON.stringify(body);
+  console.log('BODY', newBody);
 
-  const sectionsIncluded = itemsToArray(includeSections);
-  const sectionsExcluded = itemsToArray(excludeSections);
-
-  const tagsIncluded = mustIncludeAllTags === 'yes' ? includeTags : itemsToArray(includeTags);
-  const tagsExcluded = itemsToArray(excludeTags);
-
-  const subtypesIncluded = itemsToArray(includeSubtypes);
-  const subtypesExcluded = itemsToArray(exludeSubtypes);
-
-  const body = {
-    query: {
-      bool: {
-        must: [
-          {
-            terms: {
-              exists: {
-                'distributor.reference_id': includeDistributor,
-              },
-            },
-          },
-          {
-            terms: {
-              exists: {
-                type: contentTypesIncluded,
-              },
-            },
-          },
-          {
-            terms: {
-              exists: {
-                subtype: subtypesIncluded,
-              },
-            },
-          },
-          {
-            terms: {
-              exists: {
-                'taxonomy.tags.text': tagsIncluded,
-              },
-            },
-          },
-          {
-            nested: {
-              path: 'taxonomy.sections',
-              query: {
-                bool: {
-                  must: [
-                    {
-                      terms: {
-                        exists: {
-                          'taxonomy.sections._id': sectionsIncluded,
-                        },
-                      },
-                    },
-                    {
-                      term: {
-                        'taxonomy.sections._website': 'ajc',
-                      },
-                    },
-                  ],
-                },
-              },
-            },
-          },
-        ],
-        must_not: [
-          {
-            terms: {
-              'distributor.reference_id': excludeDistributor,
-            },
-          },
-          {
-            terms: {
-              type: contentTypesExcluded,
-            },
-          },
-          {
-            terms: {
-              subtype: subtypesExcluded,
-            },
-          },
-          {
-            terms: {
-              'taxonomy.tags.text': tagsExcluded,
-            },
-          },
-          {
-            nested: {
-              path: 'taxonomy.sections',
-              query: {
-                bool: {
-                  must: [
-                    {
-                      terms: {
-                        'taxonomy.sections._id': sectionsExcluded,
-                      },
-                    },
-                    {
-                      term: {
-                        'taxonomy.sections._website': 'ajc',
-                      },
-                    },
-                  ],
-                },
-              },
-            },
-          },
-        ],
-      },
-    },
-  };
-
-  const encodedBody = encodeURI(JSON.stringify(body));
-
-  return `/content/v4/search/published?body=${encodedBody}&website=ajc`;
+  return `/content/v4/search/published?body=${newBody}&website=ajc`;
 };
 
 export default {
