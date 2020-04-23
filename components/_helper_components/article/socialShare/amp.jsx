aad59d24cf6f58777bec63dd8086b911d509f506
Antonio <garcia.juan1994@gmail.com>
2020-04-23 15:48:21 -0400

    APD-372/addsConditional-Antonio

--- components/_helper_components/article/socialShare/amp.jsx
+++ components/_helper_components/article/socialShare/amp.jsx
@@ -6,7 +6,8 @@ const SocialShare = () => {
   const { facebookAppID } = getProperties();
   return (
     <div className="social-share-buttons b-margin-bottom-d40-m20">
-        <amp-social-share class="btn-facebook" type="facebook" width="35" height="35" data-param-app_id={facebookAppID}/>
+        { facebookAppID
+          && <amp-social-share class="btn-facebook" type="facebook" width="35" height="35" data-param-app_id={facebookAppID}/>}
         <amp-social-share class="btn-twitter" type="twitter" width="35" height="35"/>
         <amp-social-share class="btn-pinterest" type="pinterest" width="35" height="35"/>
         <amp-social-share class="btn-mail" type="email" width="35" height="35"/>
