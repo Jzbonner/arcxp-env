ed345aa088209d8e916ad2e7c360b7ecf3a36410
Dave Young <dave.young@ajc.com>
2020-04-22 22:55:49 -0400

    cleaned up nomenclature

--- components/_helper_components/article/contentElements/components/html/renderCustomHtml.jsx
+++ components/_helper_components/article/contentElements/components/html/renderCustomHtml.jsx
@@ -9,34 +9,33 @@ const renderCustomHtml = (content) => {
     // powers all inline & external scripts in embedded html
     if (html.search(/<script/gi) !== -1) {
       // only proceed if it actually contains a script (added because <style /> blocks were triggering)
-      const execScript = (script) => {
-        if (script.indexOf('<script') > -1) {
-          const newScript = document.createElement('script');
-          const externalFilter = /[script|type=".*"] src=['"](.*?)['"]/gmi;
-          if (script.search(externalFilter) !== -1) {
-            let scriptSrc = script.match(externalFilter);
-            scriptSrc = scriptSrc[0].replace(/.*?src=|['"]/gmi, '');
-            newScript.src = scriptSrc;
-          } else {
-            const scriptVal = script.replace(/<script.*?>|<\/script>/gi, '');
-            newScript.innerHTML = scriptVal;
-          }
-          document.body.appendChild(newScript);
-        }
-      };
       const scriptFilter = /(<script.*?>.*?<\/script>)/gi;
-      let scripts = html.split(scriptFilter);
+      let scriptBlocks = html.split(scriptFilter);
       setHtml(html.replace(scriptFilter, ''));
 
-      if (Array.isArray(scripts)) {
-        scripts = scripts.filter(script => script !== '');
-        scripts.forEach((script) => {
-          let secondaryScripts = script.split(/<\/script>/gi);
+      if (Array.isArray(scriptBlocks)) {
+        scriptBlocks = scriptBlocks.filter(script => script !== '');
+        scriptBlocks.forEach((scriptBlock) => {
+          let scriptTags = scriptBlock.split(/<\/script>/gi);
           // we do a second script check/split because in some cases it doesn't split ALL on the first go-round
           // mostly an issue in Third Party Tease features, where all of the code is in a single `html` block
-          secondaryScripts = secondaryScripts ? secondaryScripts.filter(newScript => newScript !== '') : [];
-          secondaryScripts.forEach((secondaryScript) => {
-            execScript(secondaryScript);
+          scriptTags = scriptTags ? scriptTags.filter(scriptTag => scriptTag !== '') : [];
+          scriptTags.forEach((script) => {
+            if (script.indexOf('<script') > -1) {
+              // we ensure it's actually a script, and not html or something else (as is sometimes the case)
+              // before we (re)create it as a new script tag
+              const newScript = document.createElement('script');
+              const externalFilter = /[script|type=".*"] src=['"](.*?)['"]/gmi;
+              if (script.search(externalFilter) !== -1) {
+                let scriptSrc = script.match(externalFilter);
+                scriptSrc = scriptSrc[0].replace(/.*?src=|['"]/gmi, '');
+                newScript.src = scriptSrc;
+              } else {
+                const scriptVal = script.replace(/<script.*?>|<\/script>/gi, '');
+                newScript.innerHTML = scriptVal;
+              }
+              document.body.appendChild(newScript);
+            }
           });
         });
       }
