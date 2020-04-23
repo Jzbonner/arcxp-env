aa83c494b9d8d2900f39edd0f1bd7129150c9283
Dave Young <dave.young@ajc.com>
2020-04-22 22:46:35 -0400

    another major rework, to handle multiple nested scripts in the same html block

--- components/_helper_components/article/contentElements/components/html/renderCustomHtml.jsx
+++ components/_helper_components/article/contentElements/components/html/renderCustomHtml.jsx
@@ -9,24 +9,35 @@ const renderCustomHtml = (content) => {
     // powers all inline & external scripts in embedded html
     if (html.search(/<script/gi) !== -1) {
       // only proceed if it actually contains a script (added because <style /> blocks were triggering)
-      const scriptFilter = /[<script+?>\s\S]*/gi;
-      let scripts = html.match(scriptFilter);
-      setHtml(html.replace(scriptFilter, ''));
-
-      if (Array.isArray(scripts)) {
-        scripts = scripts.filter(script => script !== '');
-        scripts.forEach((script) => {
+      const execScript = (script) => {
+        if (script.indexOf('<script') > -1) {
           const newScript = document.createElement('script');
-          const externalFilter = /.*?src=['"](.*?)['"]/gmi;
+          const externalFilter = /[script|type=".*"] src=['"](.*?)['"]/gmi;
           if (script.search(externalFilter) !== -1) {
             let scriptSrc = script.match(externalFilter);
             scriptSrc = scriptSrc[0].replace(/.*?src=|['"]/gmi, '');
             newScript.src = scriptSrc;
           } else {
-            const scriptVal = script.replace(/<script.*?>|<\/script>/g, '');
+            const scriptVal = script.replace(/<script.*?>|<\/script>/gi, '');
             newScript.innerHTML = scriptVal;
           }
           document.body.appendChild(newScript);
+        }
+      };
+      const scriptFilter = /(<script.*?>.*?<\/script>)/gi;
+      let scripts = html.split(scriptFilter);
+      setHtml(html.replace(scriptFilter, ''));
+
+      if (Array.isArray(scripts)) {
+        scripts = scripts.filter(script => script !== '');
+        scripts.forEach((script) => {
+          let secondaryScripts = script.split(/<\/script>/gi);
+          // we do a second script check/split because in some cases it doesn't split ALL on the first go-round
+          // mostly an issue in Third Party Tease features, where all of the code is in a single `html` block
+          secondaryScripts = secondaryScripts ? secondaryScripts.filter(newScript => newScript !== '') : [];
+          secondaryScripts.forEach((secondaryScript) => {
+            execScript(secondaryScript);
+          });
         });
       }
     }
