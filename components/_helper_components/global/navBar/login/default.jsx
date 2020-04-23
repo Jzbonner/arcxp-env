7b8c8bdb86118d4867c5773cbbbb957e79ad0258
Dave Young <dave.young@ajc.com>
2020-04-23 15:44:36 -0400

    getting submenu into place and 90% of styling

--- components/_helper_components/global/navBar/login/default.jsx
+++ components/_helper_components/global/navBar/login/default.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connext } from 'fusion:environment';
 import '../default.scss';
 import userIcon from '../../../../../resources/icons/login/user-icon.svg';
 import userIconWhite from '../../../../../resources/icons/login/user-icon-white.svg';
@@ -11,11 +13,43 @@ const Login = ({ isMobile, isFlyout, isSticky }) => {
   } else {
     source = userIconWhite;
   }
+  const {
+    isEnabled = false,
+    clientCode,
+  } = connext;
 
-  return (
-    <li className={`nav-login ${isSticky ? 'isSticky' : ''}`} data-mg2-action="register">
-      <img src={source}></img>
+  return isEnabled && (
+    <li className={`nav-login nav-items ${isSticky ? 'isSticky' : ''}`} data-mg2-action='register'>
+      <img src={source} />
       <div className='nav-itemText login-text'>Log in</div>
+      <div className={'section'} style={{ display: 'none' }}>
+        <div className={'section-item'}>
+          <a href={`//myaccount.${clientCode}.com/${clientCode}/myprofile`}>
+            <img src={source} />
+            <div className='nav-itemText login-text'>My Profile</div>
+          </a>
+        </div>
+        <div className={'subNav'}>
+          <ul className={'subNav-flyout'}>
+            <li className={'flyout-item'}>
+              <a href={`//myaccount.${clientCode}.com/${clientCode}/dashboard`} target='_blank'>My Account</a>
+            </li>
+            <li className={'flyout-item'}>
+              <a href={`//myaccount.${clientCode}.com/${clientCode}/preference`} target='_blank'>Newsletters</a>
+            </li>
+            <li className={'flyout-item'}>
+              <a href={`//events.${clientCode}.com`}>Events</a>
+            </li>
+            <li className={'flyout-item'}>
+              <a href='/our-products/'>Our Products</a>
+            </li>
+            <li className={'flyout-item MG2activation'}>
+              <a href='#' data-mg2-action='activation'>Activate My Account</a>
+            </li>
+          </ul>
+          <a href='#' className='btn-logout' onClick='mg2Logout' data-mg2-action='logout'>Logout</a>
+        </div>
+      </div>
     </li>
   );
 };
