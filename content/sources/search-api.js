9a8fc58584aa4bb2fb52ded18a2e098b509ae013
grincon0 <georgerincon2@gmail.com>
2020-04-27 11:02:47 -0400

    APD-286/workingSearchAPI-George

--- content/sources/search-api.js
+++ content/sources/search-api.js
@@ -1,9 +1,10 @@
 const schemaName = 'article';
 
 const params = {
-  path: 'text',
   published: 'text',
-  id: 'text',
+  section: 'text',
+  size: 'text',
+  sort: 'text',
 };
 
 // search api w/ the ability to get feed of stories from a specified section
@@ -13,8 +14,8 @@ const resolve = (query) => {
   } = query;
   let requestUri = `/content/v4/search/?website=${arcSite}`;
   requestUri += section ? `&q=type:story+AND+taxonomy.sites._id:%22/${section}%22` : null;
-  requestUri += sort ? '&sort=display_date:desc' : '';
   requestUri += size ? `&size=${size}` : '';
+  requestUri += sort ? '&sort=display_date:desc' : '';
   return published ? `${requestUri}&published=${published}` : requestUri;
 };
 
