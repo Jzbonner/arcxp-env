eb7884133c030e2a0363de0aa6f2840800d3679e
Antonio <garcia.juan1994@gmail.com>
2020-05-01 11:40:19 -0400

    APD-279/addsContentSource-Antonio

---
+++ content/sources/author-search.js
@@ -0,0 +1,20 @@
+const schemaName = 'author-search';
+
+const params = {
+  name: 'text',
+};
+
+const resolve = (query) => {
+  const {
+    'arc-site': arcSite = 'ajc', name,
+  } = query;
+  let requestUri = `/content/v4/search/published?website=${arcSite}`;
+  requestUri += name ? `&q=type:story+AND+credits.by_id:${name}&sort=display_date:desc&size=10` : '';
+  return requestUri;
+};
+
+export default {
+  resolve,
+  params,
+  schemaName,
+};
