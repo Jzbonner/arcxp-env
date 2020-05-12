e2a56002b383e6a1dbe42c27a1764cc5a83e0e2f
Dave Young <dave.young@ajc.com>
2020-05-12 13:59:41 -0400

    adding support for pb page specific ads

--- components/_helper_components/global/ads/default.jsx
+++ components/_helper_components/global/ads/default.jsx
@@ -4,25 +4,43 @@ import ArcAd from '../../../features/ads/default';
 import AmpAd from '../../amp/amp-ads/AmpAd';
 import '../../../../src/styles/base/_utility.scss';
 
-const GlobalAdSlots = ({ ampPage, uuid, taxonomy }) => (
-  ampPage ? <div className="b-hidden">
-    <AmpAd
-    adSlot='PX01'
-    uuid={uuid}
-    width={'300'}
-    height={'300'}
-    taxonomy={taxonomy}
-    componentName='ArcAd'
-    multiSize={'300x1'}
-    multiSizeValidation={'false'}/></div>
-    : <div className="b-hidden"><ArcAd staticSlot={'PX01'} /><ArcAd staticSlot={'HS01'} />
-    </div>
-);
+const GlobalAdSlots = ({
+  ampPage,
+  uuid,
+  taxonomy,
+  pbPage = false,
+}) => {
+  console.error('dave', pbPage);
+  if (ampPage) {
+    return <div className="b-hidden">
+            <AmpAd
+            adSlot='PX01'
+            uuid={uuid}
+            width={'300'}
+            height={'300'}
+            taxonomy={taxonomy}
+            componentName='ArcAd'
+            multiSize={'300x1'}
+            multiSizeValidation={'false'}/>
+        </div>;
+  }
+
+  return <div className="b-hidden">
+    <ArcAd staticSlot={'PX01'} />
+    <ArcAd staticSlot={'HS01'} />
+    {pbPage && <>
+        <ArcAd staticSlot={'HS02'} />
+        <ArcAd staticSlot={'NS01'} />
+        <ArcAd staticSlot={'NS02'} />
+    </>}
+  </div>;
+};
 
 GlobalAdSlots.propTypes = {
   ampPage: PropTypes.bool,
   uuid: PropTypes.string,
   taxonomy: PropTypes.object,
+  pbPage: PropTypes.bool,
 };
 
 export default GlobalAdSlots;
