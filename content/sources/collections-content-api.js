51c596dae648f17678ab7ee7708d51a4cb0d739b
Carlos Morales <carlos.rey.morales@gmail.com>
2020-05-21 10:50:46 -0400

    APD-317/anotherDraftOfContentSourceForRSSFeeds-Carlos

--- content/sources/collections-content-api.js
+++ content/sources/collections-content-api.js
@@ -1,12 +1,46 @@
-const resolve = (query) => {
-  const { id } = query || {};
+import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
+import axios from 'axios';
 
-  return `/websked/collections/v1/collections/contents/${id}/`;
+const schemaName = 'collections';
+const ttl = 120;
+
+const params = {
+  id: 'text',
+  site: 'text',
+};
+
+const fetch = (query) => {
+  const { site, id } = query || { site: 'ajc', id: '' };
+
+  if (id) {
+    const requestUri = `${CONTENT_BASE}websked/collections/v1/collections/contents/${id}`;
+
+    return axios.get(requestUri, {
+      headers: {
+        Authorization: `Bearer ${ARC_ACCESS_TOKEN}`,
+      },
+    }).then(({ data: collectionsData = [] }) => Promise.all(collectionsData.map((object) => {
+      // now we get the story data
+      const { _id: storyId } = object || { _id: '' };
+
+      if (storyId) {
+        const storyURL = `${CONTENT_BASE}/content/v4/?website=${site}&_id=${storyId}`;
+        return axios.get(storyURL, {
+          headers: {
+            Authorization: `Bearer ${ARC_ACCESS_TOKEN}`,
+          },
+        });
+      }
+      return new Promise(resolve => resolve({}));
+    })));
+  }
+
+  return [];
 };
 
 export default {
-  resolve,
-  params: {
-    id: 'text',
-  },
+  fetch,
+  schemaName,
+  params,
+  ttl,
 };
