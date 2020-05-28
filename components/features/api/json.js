ddfbda5578d61b00c3319592f94b089660012391
Carlos Morales <carlos.rey.morales@gmail.com>
2020-05-28 15:45:19 -0400

    APD-317/fixingJSONFeed-Carlos

--- components/features/api/json.js
+++ components/features/api/json.js
@@ -1,32 +1,16 @@
-import PropTypes from 'prop-types';
 import Consumer from 'fusion:consumer';
 
 @Consumer
 class Api {
   constructor(props) {
     this.props = props;
-    const {
-      customFields: {
-        content: { contentService = 'collections-content-api', contentConfigValues = { id: '' } } = {},
-      } = {},
-    } = props;
-
-    this.fetchContent({
-      data: {
-        source: contentService,
-        query: {
-          ...contentConfigValues,
-          site: 'ajc',
-        },
-      },
-    });
   }
 
   render() {
-    const { data } = this.state || {};
+    const { globalContent } = this.props || {};
 
-    if (data) {
-      const { data: arrayData } = data;
+    if (globalContent) {
+      const { data: arrayData } = globalContent || {};
 
       return {
         ...arrayData,
@@ -36,13 +20,5 @@ class Api {
     return ['no data'];
   }
 }
-
-Api.propTypes = {
-  customFields: PropTypes.shape({
-    content: PropTypes.contentConfig(['collections']).tag({
-      name: 'Content',
-    }),
-  }),
-};
 
 export default Api;
