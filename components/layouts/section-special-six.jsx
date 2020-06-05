a4e96c2c0094684df34098508726cb5109c5854c
Dave Young <dave.young@ajc.com>
2020-06-05 16:20:18 -0400

    adding a second col/col/rail zone (as #3 overall) and renaming layout to `section-special-six`

--- components/layouts/section-special-six.jsx
+++ components/layouts/section-special-six.jsx
@@ -1,4 +1,4 @@
-/*  /components/layouts/section-special-five.jsx  */
+/*  /components/layouts/section-special-six.jsx  */
 
 import React from 'react';
 import { useAppContext } from 'fusion:context';
@@ -11,19 +11,23 @@ const SectionLayout = (props) => {
     zone2Left,
     zone2Right,
     zone2RightRail,
-    zone3,
+    zone3Left,
+    zone3Right,
+    zone3RightRail,
     zone4,
-    zone4RightRail,
     zone5,
+    zone5RightRail,
+    zone6,
   ] = props.children;
   const appContext = useAppContext();
   const { layout } = appContext;
   const zonesCollection = [
     { content: zone1 },
     { content: zone2Left, rightHalfZone: zone2Right, rightRailZone: zone2RightRail },
-    { content: zone3 },
-    { content: zone4, rightRailZone: zone4RightRail },
-    { content: zone5 },
+    { content: zone3Left, rightHalfZone: zone3Right, rightRailZone: zone3RightRail },
+    { content: zone4 },
+    { content: zone5, rightRailZone: zone5RightRail },
+    { content: zone6 },
   ];
 
   return <SectionOutput zones={zonesCollection} layout={layout} />;
@@ -34,10 +38,13 @@ SectionLayout.sections = [
   'Left Col (zone 2)',
   'Right Col (zone 2)',
   'Right Rail (zone 2)',
-  'Zone 3',
+  'Left Col (zone 3)',
+  'Right Col (zone 3)',
+  'Right Rail (zone 3)',
   'Zone 4',
-  'Right Rail (zone 4)',
   'Zone 5',
+  'Right Rail (zone 5)',
+  'Zone 6',
 ];
 
 SectionLayout.propTypes = {
