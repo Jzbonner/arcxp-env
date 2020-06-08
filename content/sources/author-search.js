afabaeab44648f1579c15e78d4442f52a1f2c56d
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-06-08 09:23:33 -0400

    APD-529/UpdatedContentSourceToWorkWithNull-Nicholas

--- content/sources/author-search.js
+++ content/sources/author-search.js
@@ -6,7 +6,7 @@ const resolve = (query = {}) => {
   const { id = '' } = query;
 
   let requestUri = '/author/v1/author-service/';
-  requestUri += id !== '""' ? `?_id=${id}` : '';
+  requestUri += id !== '' ? `?_id=${id}` : '';
   return requestUri;
 };
 
