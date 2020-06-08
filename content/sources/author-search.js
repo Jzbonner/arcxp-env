0d5f6bea4cf63f1b6e805e4ef262a2ce50c33a9a
Nicholas Engleman <nicholas.engleman@ajc.com>
2020-06-08 09:20:46 -0400

    APD-529/UpdatedResolverToWorkWithStaffBioAndAllStaffPages-Nicholas

--- content/sources/author-search.js
+++ content/sources/author-search.js
@@ -2,10 +2,11 @@ const params = {
   id: 'text',
 };
 
-const resolve = (query) => {
+const resolve = (query = {}) => {
   const { id = '' } = query;
+
   let requestUri = '/author/v1/author-service/';
-  requestUri += id ? `?_id=${id}` : '';
+  requestUri += id !== '""' ? `?_id=${id}` : '';
   return requestUri;
 };
 
