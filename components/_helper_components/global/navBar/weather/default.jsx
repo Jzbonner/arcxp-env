5e7bde30768d9ea0ae713cbb9a684427e6f982bf
Dave Young <dave.young@ajc.com>
2020-06-10 13:34:59 -0400

    adding & mapping accuweather icon to our weather font

--- components/_helper_components/global/navBar/weather/default.jsx
+++ components/_helper_components/global/navBar/weather/default.jsx
@@ -3,12 +3,10 @@ import '../default.scss';
 import currentConditions from '../../utils/weather/currentConditions';
 
 const Weather = () => {
-  const { temp, iconPath, text } = currentConditions() || {};
+  const { temp = '', icon = '', text = '' } = currentConditions() || {};
   return (
     <>
-      <li className='nav-weather weather-icon'>
-        {iconPath && <img height='35px' src={iconPath} alt={text} />}
-      </li>
+      <li className={`nav-weather weather-icon weather-${icon}`} title={text}></li>
       <li className='nav-itemText nav-weather weather-text'>
         {temp && <a href='/weather/'>{temp}&deg;</a>}
       </li>
