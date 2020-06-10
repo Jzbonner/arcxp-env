1c6fb9cdf90635a03e14111d7e7f628816a95c68
Dave Young <dave.young@ajc.com>
2020-06-10 11:19:48 -0400

    various fixes for metrics, per Sarah

--- properties/sites/ajc.js
+++ properties/sites/ajc.js
@@ -3,14 +3,14 @@ import adstxt from './adstxt/ajc.com.json';
 export default {
   defaultSiteTitle: 'AJC',
   dfpId: '21849707860',
-  siteDomainURL: '//www.ajc.com',
+  siteDomainURL: 'https://www.ajc.com',
   twitterURL: '//twitter.com/ajc',
   facebookURL: '//facebook.com/ajc',
   instagramURL: '//instagram.com/ajcnews',
   metrics: {
     siteID: 'ajc',
     siteMetro: 'ga: atlanta',
-    siteFormat: 'newspaper',
+    siteFormat: 'news',
     gtmContainerKey: 'GTM-WT4CBT7',
     ampGtmID: 'GTM-WQBXD72',
   },
