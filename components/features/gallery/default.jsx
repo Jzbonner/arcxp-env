9acb8a0f4fd99e0ca1a5b7899a37d101ab190e75
grincon0 <georgerincon2@gmail.com>
2020-06-10 12:04:23 -0400

    APD-488/metricsAdjustmentForGallery-George

--- components/features/gallery/default.jsx
+++ components/features/gallery/default.jsx
@@ -158,8 +158,8 @@ const Gallery = (props) => {
   };
 
   const changeIndex = (action, maxNumber) => {
-    if (!hasOpened && currentIndex === 0) dispatchGalleryOpenEvent();
-    if (currentIndex !== 0) dispatchPhotoViewedEvent();
+    if (!hasOpened && (currentIndex === 1 || currentIndex === maxIndex)) dispatchGalleryOpenEvent();
+    if ((currentIndex === 0 || clickCount % 3 !== 0)) dispatchPhotoViewedEvent();
 
     const currentClickCount = clickCount;
     if (!isMobile) handleClickCount();
