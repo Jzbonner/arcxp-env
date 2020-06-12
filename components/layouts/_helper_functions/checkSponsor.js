e6b76e74784cf6bdcd8435f338f4f965b97fecc5
grincon0 <georgerincon2@gmail.com>
2020-06-12 16:08:31 -0400

    APD-473/workingActiveSponsorChecking-George

--- components/layouts/_helper_functions/checkSponsor.js
+++ components/layouts/_helper_functions/checkSponsor.js
@@ -2,8 +2,19 @@ const checkSponsor = (sections) => {
   if (!sections) {
     return null;
   }
+  let sponsorSectionID = null;
   const sponsorSection = sections.filter(section => section && section.path && section.path.includes('/sponsor/'));
-  const sponsorSectionID = sponsorSection && sponsorSection[0] && sponsorSection[0].path ? sponsorSection[0].path : null;
+
+  sponsorSection.forEach((el) => {
+    const inactive = el.additional_properties
+    && el.additional_properties.original
+    && el.additional_properties.original.inactive ? el.additional_properties.original.inactive : false;
+
+    if (!sponsorSectionID && !inactive) {
+      sponsorSectionID = el.path || null;
+    }
+  });
+
   return sponsorSectionID;
 };
 
