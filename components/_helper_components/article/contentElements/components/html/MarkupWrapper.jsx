c724b925035a0d139d174ff884f170186749df9d
Phil Pastorek <phil.pastorek@washpost.com>
2020-06-15 15:29:51 -0400

    adds 3rd party scripts to parse in iframe srcdoc

--- components/_helper_components/article/contentElements/components/html/MarkupWrapper.jsx
+++ components/_helper_components/article/contentElements/components/html/MarkupWrapper.jsx
@@ -1,25 +1,16 @@
-/* eslint-disable */
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import './styles.scss';
 
 const MarkupWrapper = ({ html }) => {
-
-  console.log("MarkupWrapper", html.match(/\<script/))
-
   if (html.match(/<script/)) {
-    console.log("MarkupWrapper => script")
     return <iframe className="scriptIframe" frameBorder="0" scrolling="no" width="100%" height="400" srcDoc={html} />;
   }
-  else {
-    console.log("MarkupWrapper => non script")
-    return <div dangerouslySetInnerHTML={{ __html: html }} />
-  }
-}
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+};
 
 MarkupWrapper.propTypes = {
   html: PropTypes.string,
-}
+};
 
 export default MarkupWrapper;
-/* eslint-enable */
