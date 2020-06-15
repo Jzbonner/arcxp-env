da20ba340b93edd3c729834521624391616482c0
Nicholas Engleman <nicholas@englemaninvest.com>
2020-06-15 19:27:29 -0400

    noTicket/ChangedStoriesPerPageTo10-Nicholas

--- components/_helper_components/listpage/default.jsx
+++ components/_helper_components/listpage/default.jsx
@@ -37,7 +37,7 @@ const ListPage = ({ globalContent, globalContentConfig }) => {
   });
 
   const { data } = collectionMetaData || {};
-  const storiesPerPage = 5;
+  const storiesPerPage = 10;
 
   return (
     <main className="c-listPage">
