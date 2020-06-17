776d92081564f76e28db51d9784128e14e975a94
Carlos Morales <carlos.rey.morales@gmail.com>
2020-06-17 11:14:40 -0400

    APD-477/addedSupportForVideoPages-Carlos

--- components/features/api/naviga.js
+++ components/features/api/naviga.js
@@ -28,13 +28,13 @@ class Api {
           credits,
         } = item || {};
 
+        const title = headlines && headlines.basic ? `<![CDATA[${headlines.basic}]]>` : '';
+        const author = credits && credits.by && credits.by[0] && credits.by[0].name ? `<![CDATA[${credits.by[0].name}]]>` : '';
+        const formattedDescription = description && description.basic ? `<![CDATA[${description.basic}]]>` : '';
+
+        const formattedDate = formatApiTime(firstPubDate, displayDate);
+
         if (type === 'story') {
-          const title = headlines && headlines.basic ? `<![CDATA[${headlines.basic}]]>` : '';
-          const author = credits && credits.by && credits.by[0] && credits.by[0].name ? `<![CDATA[${credits.by[0].name}]]>` : '';
-          const formattedDescription = description && description.basic ? `<![CDATA[${description.basic}]]>` : '';
-
-          const formattedDate = formatApiTime(firstPubDate, displayDate);
-
           const formatContentElements = formatNavigaContent(contentElements);
 
           const mediaArray = getMediaContent(contentElements);
@@ -67,6 +67,72 @@ class Api {
           };
 
           return xmlObject;
+        }
+
+        if (type === 'video') {
+          const {
+            streams,
+            promo_image: promoImage,
+          } = item || {};
+
+          const mp4Url = streams && streams[0] && streams[0].url;
+          const { caption, url: promoImageUrl } = promoImage || {};
+
+          const vidoeXmlObject = {
+            item: [
+              {
+                guid: `urn:uuid:${guid}`,
+              },
+              {
+                link: `${canonicalUrl}`,
+              },
+              {
+                description: formattedDescription,
+              },
+              {
+                pubDate: formattedDate,
+              },
+              {
+                title,
+              },
+              {
+                author,
+              },
+              [
+                {
+                  _name: 'media:content',
+                  _attrs: {
+                    type: 'video',
+                    medium: 'video/mp4',
+                    url: mp4Url,
+                  },
+                  _content: [
+                    {
+                      'media:title': title,
+                    },
+                    {
+                      'media:description': `<![CDATA[${caption}]]>`,
+                    },
+                    {
+                      _name: 'media:credit',
+                      _attrs: {
+                        role: 'author',
+                      },
+                      _content: author,
+                    },
+                    {
+                      _name: 'media:thumbnail',
+                      _attrs: {
+                        url: promoImageUrl,
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          };
+
+          return vidoeXmlObject;
         }
         return {};
       });
