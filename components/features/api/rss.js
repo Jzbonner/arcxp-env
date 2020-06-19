73b8640ac51fb45726fd5a1858731309728252f3
Carlos Morales <carlos.rey.morales@gmail.com>
2020-06-19 15:38:14 -0400

    APD-477-4/addedRssSupportForListPages-Carlos

--- components/features/api/rss.js
+++ components/features/api/rss.js
@@ -13,9 +13,7 @@ class Api {
     const { globalContent } = this.props || {};
 
     if (globalContent) {
-      const { data: arrayData } = globalContent || {};
-
-      return arrayData.map((item) => {
+      return globalContent.map((item) => {
         const {
           content_elements: contentElements = [],
           first_publish_date: firstPubDate,
