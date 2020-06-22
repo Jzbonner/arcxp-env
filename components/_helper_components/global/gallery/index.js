e5f3bb89d1769d85f8bea40316c43a249e5af209
Nicholas Engleman <nicholas@englemaninvest.com>
2020-06-22 11:09:28 -0400

    noTicket/ChangedCapitlization-Nicholas

--- components/_helper_components/global/gallery/index.js
+++ components/_helper_components/global/gallery/index.js
@@ -3,4 +3,4 @@ export { default as DesktopCaption } from './desktopCaption.jsx';
 export { default as GalleryItem } from './galleryItem.jsx';
 export { default as OverlayMosiac } from './overlayMosiac.jsx';
 export { default as MobileGallery } from './mobileGallery.jsx';
-export { default as ImageModal } from './ImageModal.jsx';
+export { default as ImageModal } from './imageModal.jsx';
