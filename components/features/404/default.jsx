9fba39fa2f650864945c1815af8eb6f7c2c38a58
Carlos Morales <carlos.rey.morales@gmail.com>
2020-06-23 11:41:16 -0400

    APD-557/commitingCodeToMoveBackToRss-Carlos

--- components/features/404/default.jsx
+++ components/features/404/default.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import './default.scss';
 
-export const FourZeroFour = () => <div className="404-feature">404</div>;
+export const FourZeroFour = () => {
+  return (
+    <div className="page-not-found-feature">
+      <div className="page-not-found-background-image">
+        404
+      </div>
+      <div className="page-not-found-text">
+        {`Sorry, we couldn't find that page.
+    Try Searching or go to our`} <a href="https://www.ajc.com">Frontpage.</a>
+      </div>
+    </div>
+  );
+};
 
 export default FourZeroFour;
