e9d821ca71a24dec387e2871cb8d53490e137223
Nicholas Engleman <nicholas@englemaninvest.com>
2020-06-24 16:08:56 -0400

    noTicket/AddSupportForRelativeLinks-Nicholas

--- components/_helper_components/home/featureTitle/featureTitle.jsx
+++ components/_helper_components/home/featureTitle/featureTitle.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useAppContext } from 'fusion:context';
 import './default.scss';
 
 const FeatureTitle = ({ title, moreURL }) => {
+  const appContext = useAppContext();
+  const { contextPath } = appContext;
+
+  const getLink = () => {
+    if (!moreURL.includes('https://www')) {
+      if (window.location.hostname !== 'ajc.com') {
+        return `${contextPath}${moreURL}/?_website=ajc`;
+      }
+      return `${contextPath}${moreURL}`;
+    }
+    return moreURL;
+  };
+
   if (title) {
     return (
       <div className="c-sectionTitle">
         {moreURL ? (
-          <a href={moreURL} className="titleURL">
+          <a href={getLink()} className="titleURL">
             {title}
             <span className="btn-arrow-right btn-readmore"></span>
           </a>
