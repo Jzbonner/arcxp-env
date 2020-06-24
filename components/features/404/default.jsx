db7bb3438ca769a562f891f7d175f1057dc0da75
Carlos Morales <carlos.rey.morales@gmail.com>
2020-06-24 07:44:45 -0400

    APD-557/mostlyDoneNeedImageFromUX-Carlos

--- components/features/404/default.jsx
+++ components/features/404/default.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './default.scss';
+import keyboardImage from '../../../resources/images/keyboard-404.png';
 
 export const FourZeroFour = () => {
   return (
     <div className="page-not-found-feature">
-      <div className="page-not-found-background-image">
-        404
+      <div className="background-image-text b-margin-bottom-d40-m20">
+        <img className="background-image" src={keyboardImage} />
+        <h1 className="number-text">404</h1>
       </div>
-      <div className="page-not-found-text">
-        {`Sorry, we couldn't find that page.
-    Try Searching or go to our`} <a href="https://www.ajc.com">Frontpage.</a>
+      <div className="text">
+        {'Sorry, we couldn\'t find that page.'}
+      </div>
+      <div className="text">
+        Try Searching or go to our <a href="https://www.ajc.com">Frontpage.</a>
       </div>
     </div>
   );
