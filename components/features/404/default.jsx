1a95903af70a77819fcf3b8d93754127386974aa
Carlos Morales <carlos.rey.morales@gmail.com>
2020-06-24 15:58:16 -0400

    APD-557/404PageCompleted-Carlos

--- components/features/404/default.jsx
+++ components/features/404/default.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import './default.scss';
-import keyboardImage from '../../../resources/images/keyboard-404.png';
+import keyboardImage from '../../../resources/images/404.png';
 
-export const FourZeroFour = () => {
-  return (
-    <div className="page-not-found-feature">
-      <div className="background-image-text b-margin-bottom-d40-m20">
-        <img className="background-image" src={keyboardImage} />
-        <h1 className="number-text">404</h1>
-      </div>
-      <div className="text">
-        {'Sorry, we couldn\'t find that page.'}
-      </div>
-      <div className="text">
-        Try Searching or go to our <a href="https://www.ajc.com">Frontpage.</a>
-      </div>
+export const FourZeroFour = () => (
+  <div className="page-not-found-feature">
+    <div className="background-image-text b-margin-bottom-d40-m20">
+      <img className="background-image" src={keyboardImage} />
     </div>
-  );
-};
+    <div className="text">
+      {'Sorry, we couldn\'t find that page.'}
+    </div>
+    <div className="text">
+      Try Searching or go to our <a href="https://www.ajc.com">Frontpage.</a>
+    </div>
+  </div>
+);
 
 export default FourZeroFour;
