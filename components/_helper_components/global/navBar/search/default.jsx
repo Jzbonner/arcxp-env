67b6581aa892eaa66bea7c72a7998303f26ef9e4
grincon0 <georgerincon2@gmail.com>
2020-06-29 17:08:12 -0400

    noTicket/pressingEnterExecutesSearch-George

--- components/_helper_components/global/navBar/search/default.jsx
+++ components/_helper_components/global/navBar/search/default.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, {
+  useState, useEffect, useLayoutEffect, useRef,
+} from 'react';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isEditing, setEditingState] = useState(false);
+
+  const inputRef = useRef(null);
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
@@ -12,14 +17,35 @@ const Search = () => {
     window.location.href = `/search/?q=${searchTerm}`;
   };
 
+  const handleEnterPress = (e) => {
+    if (e.keyCode === 13) handleSubmit();
+  };
+
+  const toggleEditing = () => {
+    setEditingState(!isEditing);
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleEnterPress, true);
+    return () => {
+      window.removeEventListener('keydown', handleEnterPress, true);
+    };
+  }, [searchTerm]);
+
+  useLayoutEffect(() => {
+    if (isEditing) inputRef.current.focus();
+  }, [isEditing]);
+
   return (
     <li className='nav-search nav-itemBottomBorder'>
       <form onSubmit={handleSubmit} className='search-form'>
         <input
+          onClick={toggleEditing}
           onChange={handleChange}
           type="text"
           name="value"
           value={searchTerm}
+          ref={inputRef}
           className='search-input'></input>
         <button type="submit" className="c-search-icon">
           <img className='search-icon'
