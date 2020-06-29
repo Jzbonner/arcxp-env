3614ee86115a91e2fc1f7f6da2ebfbf3c3892a7c
Antonio <garcia.juan1994@gmail.com>
2020-06-29 19:10:36 -0400

    APD-336/addsBackCSection-Antonio

--- components/layouts/article-basic.jsx
+++ components/layouts/article-basic.jsx
@@ -116,7 +116,7 @@ const StoryPageLayout = () => {
         );
       }
       return (
-        <div className='teads-ad'>
+        <div className='c-section teads-ad'>
           <AmpAd adSlot='PX01' uuid={uuid} width={'300'} height={'1'} taxonomy={taxonomy} componentName='ArcAd'
           multiSizeValidation={'false'} />
         </div>
