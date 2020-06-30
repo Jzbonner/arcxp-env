975cd9b7d8cd8525f202140e55eaac89f832284c
Antonio <garcia.juan1994@gmail.com>
2020-06-29 21:17:04 -0400

    APD-336/indentsDiv-Antonio

--- components/layouts/article-basic.jsx
+++ components/layouts/article-basic.jsx
@@ -108,10 +108,10 @@ const StoryPageLayout = () => {
       if (isHyperlocalContent) {
         return (
           <div className='c-section'>
-          <amp-fx-flying-carpet height="300px" class="ampAd">
-          <AmpAd adSlot='PX01' uuid={uuid} width={'300'} height={'500'} taxonomy={taxonomy} componentName='ArcAd'
-          multiSizeValidation={'false'}/>
-        </amp-fx-flying-carpet>
+            <amp-fx-flying-carpet height="300px" class="ampAd">
+            <AmpAd adSlot='PX01' uuid={uuid} width={'300'} height={'500'} taxonomy={taxonomy} componentName='ArcAd'
+            multiSizeValidation={'false'}/>
+          </amp-fx-flying-carpet>
         </div>
         );
       }
