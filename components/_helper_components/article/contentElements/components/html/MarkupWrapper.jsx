0ff72b4b0a90af19e966b4893d5b7c79245ae4a2
Phil Pastorek <phil.pastorek@washpost.com>
2020-07-07 13:20:37 -0400

    replaced iframe loader with htmlparser2 script creation

--- components/_helper_components/article/contentElements/components/html/MarkupWrapper.jsx
+++ components/_helper_components/article/contentElements/components/html/MarkupWrapper.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Parser } from 'htmlparser2';
 import './styles.scss';
 
-const MarkupWrapper = ({ html }) => {
-  if (html.match(/<script/)) {
-    return <iframe className="scriptIframe" frameBorder="0" scrolling="no" width="100%" height="400" srcDoc={html} />;
+class MarkupWrapper extends PureComponent {
+  constructor(props) {
+    super(props);
+    if (!props.src) {
+      // don't have a defined endpoint so we need to parse the html
+      let inScript = false;
+      let src = '';
+      let script = '';
+      const parser = new Parser({
+        onopentag: (tag, attribs) => {
+          if (tag !== 'script') return;
+          ({ src } = attribs);
+          if (!src) inScript = true;
+        },
+        ontext: (text) => {
+          if (!inScript) return;
+          script = text;
+        },
+        onclosetag: (tag) => {
+          if (tag !== 'script') return;
+          inScript = false;
+        },
+      });
+      parser.write(this.props.html);
+      parser.end();
+      if (src) this.scriptSrc = src;
+      if (script) this.scriptText = script;
+    }
   }
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-};
+
+  componentDidMount() {
+    const src = this.props.src || this.scriptSrc;
+    if (src) {
+      // if the endpoint is more than 1024 char, something is wrong
+      const type = `src-${src}`.substring(0, 1024);
+      if (window[type]) return;
+      window[type] = true;
+    }
+    const script = document.createElement('script');
+    script.async = true;
+    if (src) script.src = src;
+    if (this.scriptText) script.text = this.scriptText;
+    document.getElementsByTagName('body')[0].appendChild(script);
+  }
+
+  render() {
+    if (this.props.render) return this.props.render();
+    return <div dangerouslySetInnerHTML={{ __html: this.props.html }} />;
+  }
+}
+
 
 MarkupWrapper.propTypes = {
   html: PropTypes.string,
+  src: PropTypes.string,
+  render: PropTypes.func,
 };
 
 export default MarkupWrapper;
