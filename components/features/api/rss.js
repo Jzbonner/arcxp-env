1b21f9f2ed971a4da2716c1f5a5a48a0a222e644
Carlos Morales <carlos.rey.morales@gmail.com>
2020-07-10 11:05:47 -0400

    APD-646/sortingByDisplayDateOnRssFeeds-Carlos

--- components/features/api/rss.js
+++ components/features/api/rss.js
@@ -15,158 +15,159 @@ class Api {
     const { siteID = 'ajc' } = metrics || {};
 
     if (globalContent) {
-      return globalContent.map((item) => {
-        const {
-          content_elements: contentElements = [],
-          first_publish_date: firstPubDate,
-          display_date: displayDate,
-          canonical_url: canonicalUrl,
-          _id: guid,
-          type = '',
-          headlines,
-          description,
-          credits,
-          promo_items: promoItems,
-        } = item || {};
-
-        const title = headlines && headlines.basic ? `<![CDATA[${headlines.basic}]]>` : '';
-        const author = credits && credits.by && credits.by[0] && credits.by[0].name ? `<![CDATA[${credits.by[0].name}]]>` : '';
-        const formattedDescription = description && description.basic ? `<![CDATA[${description.basic}]]>` : '';
-
-        const formattedDate = formatApiTime(firstPubDate, displayDate);
-
-        if (type === 'story') {
-          const formatContentElements = formatNavigaContent(siteID, contentElements);
-
-          const mediaArray = getMediaContent(type, siteID, contentElements, promoItems);
-
-          const xmlObject = {
-            item: [
-              {
-                guid: `urn:uuid:${guid}`,
-              },
-              {
-                link: `${websiteURL}${canonicalUrl}`,
-              },
-              {
-                description: formattedDescription,
-              },
-              {
-                pubDate: formattedDate,
-              },
-              {
-                'content:encoded': `<![CDATA[${formatContentElements.join('')}]]>`,
-              },
-              {
-                title,
-              },
-              {
-                author,
-              },
-              mediaArray,
-            ],
-          };
-
-          return xmlObject;
-        }
-
-        if (type === 'video') {
+      return globalContent.sort((a, b) => a && a.display_date && b && b.display_date && a.display_date - b.display_date)
+        .map((item) => {
           const {
-            streams,
-            promo_image: promoImage,
+            content_elements: contentElements = [],
+            first_publish_date: firstPubDate,
+            display_date: displayDate,
+            canonical_url: canonicalUrl,
+            _id: guid,
+            type = '',
+            headlines,
+            description,
+            credits,
+            promo_items: promoItems,
           } = item || {};
 
-          const mp4Url = streams && streams[0] && streams[0].url;
-          const { caption, url: promoImageUrl } = promoImage || {};
+          const title = headlines && headlines.basic ? `<![CDATA[${headlines.basic}]]>` : '';
+          const author = credits && credits.by && credits.by[0] && credits.by[0].name ? `<![CDATA[${credits.by[0].name}]]>` : '';
+          const formattedDescription = description && description.basic ? `<![CDATA[${description.basic}]]>` : '';
 
-          const vidoeXmlObject = {
-            item: [
-              {
-                guid: `urn:uuid:${guid}`,
-              },
-              {
-                link: `${websiteURL}${canonicalUrl}`,
-              },
-              {
-                description: formattedDescription,
-              },
-              {
-                pubDate: formattedDate,
-              },
-              {
-                title,
-              },
-              {
-                author,
-              },
-              [
+          const formattedDate = formatApiTime(firstPubDate, displayDate);
+
+          if (type === 'story') {
+            const formatContentElements = formatNavigaContent(siteID, contentElements);
+
+            const mediaArray = getMediaContent(type, siteID, contentElements, promoItems);
+
+            const xmlObject = {
+              item: [
                 {
-                  _name: 'media:content',
-                  _attrs: {
-                    type: 'video',
-                    medium: 'video/mp4',
-                    url: mp4Url,
-                  },
-                  _content: [
-                    {
-                      'media:title': title,
-                    },
-                    {
-                      'media:description': `<![CDATA[${caption}]]>`,
-                    },
-                    {
-                      _name: 'media:credit',
-                      _attrs: {
-                        role: 'author',
-                      },
-                      _content: author,
-                    },
-                    {
-                      _name: 'media:thumbnail',
-                      _attrs: {
-                        url: promoImageUrl,
-                      },
-                    },
-                  ],
+                  guid: `urn:uuid:${guid}`,
                 },
+                {
+                  link: `${websiteURL}${canonicalUrl}`,
+                },
+                {
+                  description: formattedDescription,
+                },
+                {
+                  pubDate: formattedDate,
+                },
+                {
+                  'content:encoded': `<![CDATA[${formatContentElements.join('')}]]>`,
+                },
+                {
+                  title,
+                },
+                {
+                  author,
+                },
+                mediaArray,
               ],
-            ],
-          };
+            };
 
-          return vidoeXmlObject;
-        }
+            return xmlObject;
+          }
 
-        if (type === 'gallery') {
-          const galleryMediaArray = getMediaContent(type, siteID, contentElements);
+          if (type === 'video') {
+            const {
+              streams,
+              promo_image: promoImage,
+            } = item || {};
 
-          const galleryXmlObject = {
-            item: [
-              {
-                guid: `urn:uuid:${guid}`,
-              },
-              {
-                link: `${websiteURL}${canonicalUrl}`,
-              },
-              {
-                description: formattedDescription,
-              },
-              {
-                pubDate: formattedDate,
-              },
-              {
-                title,
-              },
-              {
-                author,
-              },
-              galleryMediaArray,
-            ],
-          };
+            const mp4Url = streams && streams[0] && streams[0].url;
+            const { caption, url: promoImageUrl } = promoImage || {};
 
-          return galleryXmlObject;
-        }
-        return {};
-      });
+            const vidoeXmlObject = {
+              item: [
+                {
+                  guid: `urn:uuid:${guid}`,
+                },
+                {
+                  link: `${websiteURL}${canonicalUrl}`,
+                },
+                {
+                  description: formattedDescription,
+                },
+                {
+                  pubDate: formattedDate,
+                },
+                {
+                  title,
+                },
+                {
+                  author,
+                },
+                [
+                  {
+                    _name: 'media:content',
+                    _attrs: {
+                      type: 'video',
+                      medium: 'video/mp4',
+                      url: mp4Url,
+                    },
+                    _content: [
+                      {
+                        'media:title': title,
+                      },
+                      {
+                        'media:description': `<![CDATA[${caption}]]>`,
+                      },
+                      {
+                        _name: 'media:credit',
+                        _attrs: {
+                          role: 'author',
+                        },
+                        _content: author,
+                      },
+                      {
+                        _name: 'media:thumbnail',
+                        _attrs: {
+                          url: promoImageUrl,
+                        },
+                      },
+                    ],
+                  },
+                ],
+              ],
+            };
+
+            return vidoeXmlObject;
+          }
+
+          if (type === 'gallery') {
+            const galleryMediaArray = getMediaContent(type, siteID, contentElements);
+
+            const galleryXmlObject = {
+              item: [
+                {
+                  guid: `urn:uuid:${guid}`,
+                },
+                {
+                  link: `${websiteURL}${canonicalUrl}`,
+                },
+                {
+                  description: formattedDescription,
+                },
+                {
+                  pubDate: formattedDate,
+                },
+                {
+                  title,
+                },
+                {
+                  author,
+                },
+                galleryMediaArray,
+              ],
+            };
+
+            return galleryXmlObject;
+          }
+          return {};
+        });
     }
 
     return [];
