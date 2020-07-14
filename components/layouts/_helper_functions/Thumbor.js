f01ab1ae8599cc51e81e3dfbce1827f2e609b0f1
Dave Young <dave.young@ajc.com>
2020-07-13 20:04:41 -0400

    updated resizer to use prod when applicable

--- components/layouts/_helper_functions/Thumbor.js
+++ components/layouts/_helper_functions/Thumbor.js
@@ -1,13 +1,19 @@
 import getProperties from 'fusion:properties';
+import fetchEnv from '../../_helper_components/global/utils/environment.js';
 
 const Thumbor = require('thumbor-lite');
 const { RESIZER_SECRET_KEY } = require('../../../environment/index');
+
+const currentEnv = fetchEnv();
 
 export default function (url, arcSite = 'ajc', width = 1000, height = 600) {
   if (url) {
     const { cdnOrg, cdnSite } = getProperties(arcSite);
 
-    const thumbor = new Thumbor(RESIZER_SECRET_KEY, `https://${cdnOrg}-${cdnSite}-sandbox.cdn.arcpublishing.com/resizer`);
+    const thumbor = new Thumbor(
+      RESIZER_SECRET_KEY,
+      `https://${cdnOrg}-${cdnSite}-${currentEnv !== 'prod' ? 'sandbox' : 'prod'}.cdn.arcpublishing.com/resizer`,
+    );
 
     const imageUrl = url.substring(url.indexOf('//') + 2);
     return thumbor
