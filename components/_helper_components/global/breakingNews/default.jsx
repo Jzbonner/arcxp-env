c1e5cb4c28ff70929c9f550a70ca1046908fe127
Phil Pastorek <phil.pastorek@washpost.com>
2020-07-15 18:15:14 -0400

    breaks up the breaking news and video

--- components/_helper_components/global/breakingNews/default.jsx
+++ components/_helper_components/global/breakingNews/default.jsx
@@ -1,48 +1,12 @@
-/* eslint-disable no-console */
-import React, { useState } from 'react';
-import getProperties from 'fusion:properties';
-import { useContent } from 'fusion:content';
-import { useFusionContext } from 'fusion:context';
-import fetchEnv from '../utils/environment.js';
-import './default.scss';
+import React from 'react';
+import BreakingNewsStory from './breaking-news-story';
+import BreakingNewsVideo from './breaking-news-video';
 
-const BreakingNews = () => {
-  const [isVisible, setVisibility] = useState(true);
-  const hideBar = () => {
-    setVisibility(false);
-  };
-  const fusionContext = useFusionContext();
-  const { arcSite } = fusionContext;
-  const currentEnv = fetchEnv();
-  const {
-    breakingNewsID_sandbox: breakingNewsSandbox,
-    breakingLiveVideoID_sandbox: liveVideoSandbox,
-    breakingNewsID,
-    breakingLiveVideoID,
-  } = getProperties(arcSite);
-
-  const newsData = useContent({
-    source: 'breaking-news-video-alert',
-    query: {
-      breakingNewsID: currentEnv === 'prod' ? breakingNewsID : breakingNewsSandbox,
-      breakingLiveVideoID: currentEnv === 'prod' ? breakingLiveVideoID : liveVideoSandbox,
-      arcSite,
-    },
-  });
-
-  const { url, headline, typeOfHeadline } = newsData || {};
-
-  if (url && headline && typeOfHeadline) {
-    return (
-      <div className={`c-breakingNews ${!isVisible ? 'is-hidden' : ''}`}>
-        <a href={url} className="breakingURL" />
-        <span className="c-breakingNews-hide" onClick={hideBar} />
-        <h2 className="c-breakingNews-heading">{headline}</h2>
-        <h2 className="c-breakingNews-content">{typeOfHeadline}</h2>
-      </div>
-    );
-  }
-  return null;
-};
+const BreakingNews = () => (
+  <>
+    <BreakingNewsStory />
+    {!BreakingNewsStory && <BreakingNewsVideo />}
+  </>
+);
 
 export default BreakingNews;
