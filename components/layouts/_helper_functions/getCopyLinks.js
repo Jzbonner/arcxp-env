499f5806c6981cd2da9279e63dfc51ae4bdfb681
Ilkov <silkov@NATL2236LSILKOV.attlocal.net>
2020-07-16 13:45:01 -0400

    noTicket/anotherOne-Samuil

--- components/layouts/_helper_functions/getCopyLinks.js
+++ components/layouts/_helper_functions/getCopyLinks.js
@@ -2,7 +2,7 @@ export default (link) => {
   if (link && link.site && link.site.site_url) {
     return link.site.site_url;
   }
-  if (link && !link.site && !link.site.site_url && link._id) {
+  if (link && link._id) {
     return link._id;
   }
   return '#';
