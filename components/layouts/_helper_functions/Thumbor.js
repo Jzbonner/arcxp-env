d59ab1e31525679c6b55ccc0f1f6842840fd2b77
Dave Young <dave.young@ajc.com>
2020-07-15 22:55:51 -0400

    updating resizer urls for prod, to use the main site domain

--- components/layouts/_helper_functions/Thumbor.js
+++ components/layouts/_helper_functions/Thumbor.js
@@ -9,10 +9,14 @@ const currentEnv = fetchEnv();
 export default function (url, arcSite = 'ajc', width = 1000, height = 600) {
   if (url) {
     const { cdnOrg, cdnSite } = getProperties(arcSite);
+    let siteDomain = `${cdnOrg}-${cdnSite}-sandbox.cdn.arcpublishing.com`;
+    if (currentEnv === 'prod') {
+      siteDomain = `www.${cdnSite}.com`;
+    }
 
     const thumbor = new Thumbor(
       RESIZER_SECRET_KEY,
-      `https://${cdnOrg}-${cdnSite}-${currentEnv !== 'prod' ? 'sandbox' : 'prod'}.cdn.arcpublishing.com/resizer`,
+      `https://${siteDomain}/resizer`,
     );
 
     const imageUrl = url.substring(url.indexOf('//') + 2);
