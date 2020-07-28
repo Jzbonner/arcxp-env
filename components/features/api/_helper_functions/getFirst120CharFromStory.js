5c9e3c93f11c77cafe7b0f0d9c2aa6e5181c2d65
Carlos Morales <carlos.morales@ajc.com>
2020-07-28 14:53:27 -0400

    APD-724/makingRequestedPRChange-Carlos

--- components/features/api/_helper_functions/getFirst120CharFromStory.js
+++ components/features/api/_helper_functions/getFirst120CharFromStory.js
@@ -2,7 +2,7 @@ export const getFirst120CharsFromStory = (contentElements = []) => {
   const firstParagraph = contentElements.find((el = { type: '', content: '' }) => el.type === 'text' && el.content !== '<br/>');
   const { content = '' } = firstParagraph || {};
   const first120Chars = typeof content === 'string' && content.substring(0, 120);
-  return first120Chars ? `<![CDATA[${first120Chars}]]>` : '';
+  return first120Chars ? `<![CDATA[${first120Chars}...]]>` : '';
 };
 
 export default getFirst120CharsFromStory;
