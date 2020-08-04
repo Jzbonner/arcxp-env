469c820908d8c1420a15e7e77cb9c3deda71a5a3
Dave Young <dave.young@ajc.com>
2020-08-04 07:18:29 -0400

    removing the subsequent API call for each story, since `content_elements` is included from the initial fetch (of either collections-api or query results)

--- content/sources/helper_functions/AddFirstInlineImage.js
+++ content/sources/helper_functions/AddFirstInlineImage.js
@@ -1,48 +1,16 @@
-/* eslint-disable no-console */
-import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
-import axios from 'axios';
-
 export default (data, arcSite, currentDisplayClass = '', requiredClasses = []) => {
   const collectionElements = data;
-  const promiseArray = [];
 
   if (requiredClasses.some(requiredClass => requiredClass === currentDisplayClass)) {
     collectionElements.forEach((el, e) => {
-      if (el.type === 'story' && !el.promo_items) {
-        const storyURL = `${CONTENT_BASE}/content/v4/?website=${arcSite}&_id=${el._id}`;
-
-        const promise = axios
-          .get(storyURL, {
-            headers: {
-              Authorization: `Bearer ${ARC_ACCESS_TOKEN}`,
-            },
-          })
-          .then(({ data: storyData }) => storyData)
-          .then((storyData) => {
-            if (storyData.content_elements) {
-              for (let i = 0; i < storyData.content_elements.length; i += 1) {
-                if (storyData.content_elements[i].type === 'image') {
-                  return storyData.content_elements[i];
-                }
-              }
-            }
-            return null;
-          })
-          .then((firstInlineImage) => {
-            if (firstInlineImage) {
-              collectionElements[e].firstInlineImage = firstInlineImage;
-              return collectionElements;
-            }
-            return null;
-          })
-          .catch((error) => {
-            console.log('AXIOS CATCH - addfirstinlineimage => ', error);
-          });
-
-        promiseArray.push(promise);
+      if (el.type === 'story' && !el.promo_items && el.content_elements) {
+        for (let i = 0; i < el.content_elements.length; i += 1) {
+          if (el.content_elements[i].type === 'image') {
+            collectionElements[e].firstInlineImage = el.content_elements[i];
+          }
+        }
       }
     });
-    return Promise.all(promiseArray).then(() => collectionElements);
   }
-  return data;
+  return collectionElements;
 };
