f0091c1e1e4804ca41c3dcf2032369a983e4ed17
Ilkov <silkov@NATL2236LSILKOV.attlocal.net>
2020-08-06 13:29:22 -0400

    noTicket/detectingSiteNameToReplaceMobileFormURL-Samuil

--- components/features/TransparenseeWidget/default.jsx
+++ components/features/TransparenseeWidget/default.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import InnerHTML from 'dangerously-set-html-content';
 import getProperties from 'fusion:properties';
 import { useContent } from 'fusion:content';
+import { useFusionContext } from 'fusion:context';
 import checkWindowSize from '../../_helper_components/global/utils/check_window_size/default';
 import searchIcon from '../../../resources/icons/search.svg';
 import './default.scss';
@@ -11,6 +12,14 @@ import './default.scss';
 const TransparenseeWidget = (props) => {
   const { customFields } = props;
   const { widgetURL } = customFields;
+  const fusionContext = useFusionContext();
+  const { arcSite } = fusionContext;
+  let formSrcUrl;
+
+  if (arcSite === 'ajc') formSrcUrl = 'https://events.ajc.com/events';
+  if (arcSite === 'dayton') formSrcUrl = 'https://thingstodo.dayton.com/events';
+  if (arcSite === 'journal-news') formSrcUrl = 'https://thingstodo.journal-news.com/events';
+  if (arcSite === 'springfield-news-sun') formSrcUrl = 'https://thingstodo.springfieldnewssun.com/events';
 
   const callback = useContent({
     source: 'widget',
@@ -31,7 +40,7 @@ const TransparenseeWidget = (props) => {
           <InnerHTML html={payload} id="ttd-search-widget" className="hidden--small" />
         ) : (
           <div className="hidden--medium hidden--large">
-            <form className="ttd-search" action="//events.ajc.com/events" method="get">
+            <form className="ttd-search" action={formSrcUrl} method="get">
               <input className="ttd-search__input" type="text" name="search" placeholder="Search for an event or movie" />
               <input className="ttd-search__submit" alt="submit" name="submit" type="image" src={searchIcon} />
             </form>
