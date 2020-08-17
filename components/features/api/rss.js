7ad64f9ab66e00c978311d9c641926a7c529c9f1
Ilkov <silkov@NATL2236LSILKOV.attlocal.net>
2020-08-17 12:03:48 -0400

    noTicket/636EditSuggestion-Samuil

--- components/features/api/rss.js
+++ components/features/api/rss.js
@@ -29,7 +29,7 @@ class Api {
     const noHeaderAndFooter = queryTypePresent && queryParams.type === 'app';
     const rssFeedDetectAppWebview = () => {
       if (noHeaderAndFooter) {
-        return '&#x26;nowrap=y'; // had to use &#x26; instead of simple "&" as per the W3 RSS validator
+        return '?nowrap=y'; // had to use &#x26; instead of simple "&" as per the W3 RSS validator
       } return '';
     };
     let maxItems = feedStart + size;
