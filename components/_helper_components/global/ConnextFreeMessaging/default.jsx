2c7783d69db8dd526e43dccec11bc3530d06d642
Antonio <garcia.juan1994@gmail.com>
2020-08-24 13:26:50 -0400

    APD-822/addsFreeAnchorComponentAndConsition-Antonio

---
+++ components/_helper_components/global/ConnextFreeMessaging/default.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConnextFreeMessaging = () => (
+  <div className="free-story-messaging b-margin-bottom-d30-m20"></div>
+);
+
+ConnextFreeMessaging.defaultProps = {
+  componentName: 'ConnextInlinePromoSubscription',
+};
+
+export default ConnextFreeMessaging;
