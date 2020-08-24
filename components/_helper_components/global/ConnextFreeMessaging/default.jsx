f04d7b02823b698d892e0bffa79469f9ff3dba6b
Antonio <garcia.juan1994@gmail.com>
2020-08-24 14:06:28 -0400

    APD-822/fixesLintErrors-Antonio

--- components/_helper_components/global/ConnextFreeMessaging/default.jsx
+++ components/_helper_components/global/ConnextFreeMessaging/default.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './default.scss';
 
 const ConnextFreeMessaging = () => (
-  <div className="free-story-messaging b-margin-bottom-d30-m20"></div>
+  <div className="free-story-messaging connext-free-messaging b-margin-bottom-d30-m20"></div>
 );
 
 ConnextFreeMessaging.defaultProps = {
