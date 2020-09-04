9e0af03cfcd4928a1bbad303264718ce701a214b
Nicholas Engleman <you@example.com>
2020-09-04 16:23:34 -0400

    APD-862/AddedMoreStyleChanges-Nicholas

--- components/features/TopPhotoNoPhoto/default.jsx
+++ components/features/TopPhotoNoPhoto/default.jsx
@@ -4,17 +4,32 @@ import Lead from '../Lead/default';
 import './default.scss';
 
 const TopPhotoNoPhoto = (customFields = {}) => {
+  const limit = customFields
+    && customFields.customFields
+    && customFields.customFields.content
+    && customFields.customFields.content.contentConfigValues
+    && customFields.customFields.content.contentConfigValues.size
+    ? customFields.customFields.content.contentConfigValues.size
+    : 2;
+
   const newCustomFields = {
     ...customFields,
     customFields: {
       ...customFields.customFields,
       displayClass: '5-Item Feature - No Photo',
+      content: {
+        ...customFields.customFields.content,
+        contentConfigValues: {
+          ...customFields.customFields.content.contentConfigValues,
+          size: limit,
+        },
+      },
     },
   };
 
   return (
     <div className='topPhotoNoPhoto'>
-      <Lead {...newCustomFields} />
+      <Lead {...newCustomFields} limitOverride={limit} />
     </div>
   );
 };
