43237d79a42efdb1c29cce72834d7ace45a2e1c7
Phil Pastorek <phil.pastorek@washpost.com>
2020-09-30 14:35:21 -0400

    env file rearrangement

---
+++ environment/coxohio-sandbox.js
@@ -0,0 +1,4 @@
+export default {
+  RESIZER_SECRET_KEY: '%{AQICAHgepo+1ZBMbkOR/rGy+iDRMCgUcyeboZY90NRaGClIlNwHPGt8qypLqxIou0F1VA1wbAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMwHg7aMLrK2Ro9dnFAgEQgDswQ6cG6VnVcBxO8UDMlkbH3OvG3C2tZP2HYYgIn+T+uJRWBc8MtIrMmcgVeoPyTml06fRw5nMAv6/Adw==}',
+  RESIZER_SECRET_KEY_OLD: 'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7', // only galleryItem is using this right now temporarily until that component can be upgraded.
+};
