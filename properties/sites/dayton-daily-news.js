6b7050f9116439833489de8ea5fa7a94064c5167
Dave Young <dave.young@ajc.com>
2020-11-18 11:14:30 -0500

    [APD-809] setting `adsA9Id` for Dayton sites

--- properties/sites/dayton-daily-news.js
+++ properties/sites/dayton-daily-news.js
@@ -87,7 +87,7 @@ export default {
     sandbox: {
       adsBidding,
       adsA9Enabled: true,
-      adsA9Id: '',
+      adsA9Id: '3970',
       adsPrebidEnabled: true,
       adsPrebidTimeout: 1500,
       devconActive: false,
@@ -96,7 +96,7 @@ export default {
     prod: {
       adsBidding,
       adsA9Enabled: false,
-      adsA9Id: '',
+      adsA9Id: '3970',
       adsPrebidEnabled: false,
       adsPrebidTimeout: 1500,
       devconActive: false,
