57e4169daea1f59b493ed6591c973bb33f458b8c
Antonio <garcia.juan1994@gmail.com>
2021-01-27 15:31:16 -0500

    APD-859/fixesLintErrors-Antonio

--- environment/coxohio-sandbox.js
+++ environment/coxohio-sandbox.js
@@ -1,4 +1,5 @@
 export default {
   RESIZER_SECRET_KEY: '%{AQICAHgepo+1ZBMbkOR/rGy+iDRMCgUcyeboZY90NRaGClIlNwHPGt8qypLqxIou0F1VA1wbAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMwHg7aMLrK2Ro9dnFAgEQgDswQ6cG6VnVcBxO8UDMlkbH3OvG3C2tZP2HYYgIn+T+uJRWBc8MtIrMmcgVeoPyTml06fRw5nMAv6/Adw==}',
   RESIZER_SECRET_KEY_OLD: 'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7', // only galleryItem is using this right now temporarily until that component can be upgraded.
+  CHARTBEAT_KEY: '7b12d4da2a1f427c5123cec839c41196',
 };
