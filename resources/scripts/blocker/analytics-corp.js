62d5c1d06b282076f96ddd1102131e29a1ec8670
grincon0 <georgerincon2@gmail.com>
2021-02-15 13:32:50 -0500

    APD-1045/finalizingScriptBlockerBait-George

--- resources/scripts/blocker/analytics-corp.js
+++ resources/scripts/blocker/analytics-corp.js
@@ -1,3 +1,3 @@
 /* For Apd-1045 - setting up bait variable for tracking blocker detection */
 // eslint-disable-next-line no-unused-vars
-var canAnalyze = true;
+const canAnalyze = true;
--- resources/scripts/blocker/prebid-ads-bt.js
+++ resources/scripts/blocker/prebid-ads-bt.js
@@ -1,4 +1,3 @@
 /* For Apd-1045 - setting up bait variable for ad blocker detection */
 // eslint-disable-next-line no-unused-vars
-var canRunAds = window.canRunAds || [];
-console.log('running ads', canRunAds);
+const canRunAds = true;
