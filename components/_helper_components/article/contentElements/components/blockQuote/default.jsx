d32bdf141a2b598744eb549d49c3f5dbeedf66c4
Dave Young <dave.young@ajc.com>
2021-02-26 15:33:54 -0500

    [noticket] updating blockquote to support html

--- components/_helper_components/article/contentElements/components/blockQuote/default.jsx
+++ components/_helper_components/article/contentElements/components/blockQuote/default.jsx
@@ -7,11 +7,18 @@ import './styles.scss';
 const Blockquote = ({ contentElements, citation }) => (
   <div className="blockquote b-margin-bottom-d40-m20">
     {contentElements.map((el, index) => {
+      const { content: contentText } = el;
+      let convertedText;
       switch (el.type) {
         case 'list':
           return <List key={`blockquote-${index}`} src={el} />;
         case 'text':
-          return <Paragraph key={`blockquote-${index}`} src={el} />;
+          // we run some conversions to replace stringified code with actual tags, and fancy quotes with standard
+          convertedText = contentText.replace(/&lt;/g, '<');
+          convertedText = convertedText.replace(/&gt;/g, '>');
+          convertedText = convertedText.replace(/”/g, '"');
+          convertedText = convertedText.replace(/’/g, "'");
+          return <Paragraph key={`blockquote-${index}`} src={{ content: convertedText }} />;
         default:
           return null;
       }
