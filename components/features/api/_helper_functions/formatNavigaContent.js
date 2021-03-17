c6e6e3ddcade5904cdc272f7ee675d922be42a8e
Dave Young <dave.young@ajc.com>
2021-03-17 11:18:28 -0400

    [noticket] using `forEach` instead of `map`

--- components/features/api/_helper_functions/formatNavigaContent.js
+++ components/features/api/_helper_functions/formatNavigaContent.js
@@ -62,7 +62,7 @@ export const formatNavigaContent = (siteID, contentElements) => contentElements.
     const { content_elements: quoteElements, citation } = el || {};
     const { content: citationContent } = citation || {};
     return `<div class="blockquote">
-      ${quoteElements.map(qEl => qEl.content && `<p>${qEl.content}</p>`)}
+      ${quoteElements.forEach(qEl => qEl.content && `<p>${qEl.content}</p>`)}
       ${citationContent ? `<blockquote>${citationContent}</blockquote>` : ''}
     </div>`;
   }
