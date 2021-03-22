5419d9332c1f876186d77ffeb9f954cc7e73a42e
Dave Young <dave.young@ajc.com>
2021-03-22 13:07:19 -0400

    [noticket] ensuring `first120Chars` is always a defined value (even if just an empty string)

--- components/features/api/_helper_functions/getFirst120CharFromStory.js
+++ components/features/api/_helper_functions/getFirst120CharFromStory.js
@@ -1,10 +1,10 @@
 export const getFirst120CharsFromStory = (contentElements = []) => {
   const firstParagraph = contentElements.find((el = { type: '', content: '' }) => el.type === 'text' && el.content !== '<br/>');
   const { content = '' } = firstParagraph || {};
-  let first120Chars = typeof content === 'string' && content;
+  let first120Chars = typeof content === 'string' ? content : '';
   first120Chars = first120Chars.replace(/<[^>]*>/gi, '');
   first120Chars = first120Chars.substring(0, 120);
-  return first120Chars ? `<![CDATA[${first120Chars}...]]>` : '';
+  return first120Chars.length ? `<![CDATA[${first120Chars}...]]>` : '';
 };
 
 export default getFirst120CharsFromStory;
