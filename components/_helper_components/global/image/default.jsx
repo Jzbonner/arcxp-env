91152790ed2fb3e645a425ef90b44a6e34fccaed
dave young <secondgen7@gmail.com>
2021-04-21 15:49:20 -0400

    [APD-1284] removing the empty array default values for focalMin & focalMax (to allow for proper cascading to rootFocalCoords)

--- components/_helper_components/global/image/default.jsx
+++ components/_helper_components/global/image/default.jsx
@@ -37,7 +37,7 @@ const Image = ({
     img = resizedObject;
   } else {
     const { focal_point: focalPoint } = additionalProperties || {};
-    const { min: focalMin = [], max: focalMax = [] } = focalPoint || {};
+    const { min: focalMin, max: focalMax } = focalPoint || {};
     /*
       we have to (re)create the root focalCoords array because the format of root-level focal_point arrays differs from those focal_point arrays contained within `additionalProperties`.
       The former have x, y coordinates, whereas the latter is an array of arrays, with `min` & `max` children that have keyless arrays of x & y coords.
