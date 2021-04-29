911ed79e0648e51a35c79c05830b34e87dd37dbb
Ilkov <silkov@NATL2236LSILKOV.lan>
2021-04-29 17:35:48 +0300

    APD-1151/addedSwipingIssueWithGalleryAdStateBreakingTheSwipe-Samuil

--- components/features/ads/children/adtypes.jsx
+++ components/features/ads/children/adtypes.jsx
@@ -513,9 +513,14 @@ export const adSlots = {
         [600, 400],
         [300, 250],
       ], // desktop
+      [
+        [600, 400],
+        [300, 250],
+      ], // tablet
     ],
     breakpoints: [
       [1024, 0], // desktop
+      [768, 0], // tablet
     ],
   },
   PG02: {
