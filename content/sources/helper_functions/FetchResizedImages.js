9737c978d49734096e93888c4088aed16e33d927
dave young <secondgen7@gmail.com>
2021-05-04 17:31:43 -0400

    [APD-1296] adding support for array requests to resizer (to limit total number of queries)

--- content/sources/helper_functions/FetchResizedImages.js
+++ content/sources/helper_functions/FetchResizedImages.js
@@ -2,63 +2,85 @@ import resizer from '../resizer';
 import setFocalCoords from './setFocalCoords';
 
 export default (arcSite, apiData, width, height, useSrcSet, srcSetSizes, squareImageSize, useSquareImageAfter, isGalleryFetch = false) => {
-  console.error('dave isGalleryFetch', isGalleryFetch);
-  const addResizedData = (el, i = -1, isGallery = false) => {
-    const {
-      url, height: originalHeight, width: originalWidth, additional_properties: additionalProperties, focal_point: rootFocalPoint,
-    } = el || {};
-    const focalCoords = setFocalCoords(additionalProperties, rootFocalPoint);
-    const newEl = el;
-    let finalWidth = width;
-    let finalHeight = height;
-    if (squareImageSize && i >= useSquareImageAfter) {
-      finalWidth = squareImageSize;
-      finalHeight = squareImageSize;
-    }
+  let isGallery = isGalleryFetch;
+  const addResizedData = (elsToFetch) => {
+    const imageQueries = [];
+    const originalEls = elsToFetch;
+    elsToFetch.forEach((el, i) => {
+      const {
+        url, height: originalHeight, width: originalWidth, additional_properties: additionalProperties, focal_point: rootFocalPoint,
+      } = el || {};
+      const focalCoords = setFocalCoords(additionalProperties, rootFocalPoint);
+      let finalWidth = width;
+      let finalHeight = height;
+      if (squareImageSize && i >= useSquareImageAfter) {
+        finalWidth = squareImageSize;
+        finalHeight = squareImageSize;
+      }
 
-    if (!url) return newEl;
+      imageQueries.push({
+        src: url,
+        height: finalHeight,
+        width: finalWidth,
+        srcSetSizes,
+        originalHeight,
+        originalWidth,
+        focalCoords,
+      });
+    });
 
-    const img = resizer.fetch({
-      src: url,
-      height: finalHeight,
-      width: finalWidth,
-      srcSetSizes,
-      originalHeight,
-      originalWidth,
-      focalCoords,
+    const imageResponses = resizer.fetch({
+      srcArray: imageQueries,
       arcSite,
       isGallery,
     });
 
-    newEl.useSrcSet = useSrcSet;
-    newEl.resized_obj = img || null;
-    return newEl;
+    if (imageResponses.length) {
+      imageResponses.forEach((img, i) => {
+        originalEls[i].useSrcSet = useSrcSet;
+        originalEls[i].resized_obj = img || null;
+      });
+    }
+
+    return originalEls;
   };
 
-  if (apiData && apiData.length) {
-    const newArrData = apiData;
-    let imageElIndex = 0;
-    newArrData.forEach((el, e) => {
-      console.error('dave has teaseImageObject', el, el.teaseImageObject);
-      if (el.teaseImageObject) {
-        newArrData[e].teaseImageObject = addResizedData(el.teaseImageObject, imageElIndex, isGalleryFetch);
-        imageElIndex += 1;
-      } else {
-        newArrData[e].teaseImageObject = addResizedData(el, imageElIndex, isGalleryFetch);
-        imageElIndex += 1;
-      }
-    });
-    return newArrData;
-  }
+  const imagesToFetch = [];
 
   if (apiData && apiData.type === 'gallery') {
     const newGallData = apiData;
     const { content_elements: newContentElements = [] } = newGallData;
-    console.error('dave it is a gallery');
-    newContentElements.forEach((el, e) => {
-      newGallData.content_elements[e] = addResizedData(el, e, true);
+    isGallery = true;
+    newContentElements.forEach((el) => {
+      imagesToFetch.push(el);
+    });
+
+    const fetchedImages = addResizedData(imagesToFetch);
+
+    fetchedImages.forEach((fetchedImageObj, e) => {
+      if (fetchedImageObj) {
+        newGallData.content_elements[e] = fetchedImageObj;
+      }
     });
     return newGallData;
+  }
+
+  if (apiData && apiData.length) {
+    const newArrData = apiData;
+
+    newArrData.forEach((el) => {
+      const imageEl = el.teaseImageObject || el;
+      imagesToFetch.push(imageEl);
+    });
+
+    const fetchedImages = addResizedData(imagesToFetch);
+
+    fetchedImages.forEach((fetchedImageObj, e) => {
+      if (fetchedImageObj) {
+        newArrData[e].teaseImageObject = fetchedImageObj;
+      }
+    });
+    return newArrData;
   }
 
   return apiData;
