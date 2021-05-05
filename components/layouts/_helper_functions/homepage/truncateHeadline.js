ebe72062c3ecaa10099b40b70599c4afdeb28439
dave young <secondgen7@gmail.com>
2021-05-05 14:18:33 -0400

    [APD-959][APD-1041] updating truncation logic to ensure that words do not get skipped (in lieu of later, shorter words) and some other enhancements

--- components/layouts/_helper_functions/homepage/truncateHeadline.js
+++ components/layouts/_helper_functions/homepage/truncateHeadline.js
@@ -1,17 +1,29 @@
-export default function (headline, applyExtension = false) {
+export default (headline, applyExtension = false) => {
   const charLimit = applyExtension ? 90 : 72;
 
   if (!headline) {
     return null;
   }
-  if (headline.length > charLimit) {
+  if (headline.length >= charLimit) {
     let newHeadline = '';
-    headline.split(' ').forEach((word) => {
-      if (newHeadline.length + word.length + 1 < charLimit) {
-        newHeadline = newHeadline.concat(word, ' ');
+    let lastWordIndex = -1;
+    headline.split(' ').forEach((word, i) => {
+      if (newHeadline.length + word.length + 1 <= charLimit && lastWordIndex === -1) {
+        newHeadline = `${newHeadline} ${word}`;
+      } else if (lastWordIndex === -1) {
+        // the headline limit has been exceeded, so set the lastWordIndex
+        lastWordIndex = i;
+        // then check the new headline for ending punctuation and remove, if present
+        if (['.', ',', '?', ':', ';', '!', ' '].includes(newHeadline.slice(-1))) {
+          newHeadline = newHeadline.substring(0, -1);
+        }
+        if (newHeadline.length < charLimit) {
+          // and if the new headline length is less than the limit, append an ellipsis
+          newHeadline = `${newHeadline}...`;
+        }
       }
     });
-    return newHeadline.slice(0, -1).concat('...');
+    return newHeadline;
   }
   return headline;
-}
+};
