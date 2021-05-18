e4f5ef10bba540f0d2f9bdc34b864fc59fb4d2d7
dave young <secondgen7@gmail.com>
2021-05-18 11:47:34 -0400

    [noticket] correcting query feed issues (encoding filter)

--- content/sources/author-stories-list.js
+++ content/sources/author-stories-list.js
@@ -10,10 +10,11 @@ const resolve = (query) => {
   const {
     'arc-site': arcSite = 'ajc', id, from,
   } = query;
+  const encodedFilter = encodeURIComponent(filter.replace(/\s+/g, ''));
   let requestUri = `/content/v4/search/published?website=${arcSite}`;
   requestUri += id ? `&q=type:story+AND+credits.by._id:${id}&sort=display_date:desc&size=10` : '';
   requestUri += from ? `&from=${from}` : '';
-  requestUri += `&_sourceInclude=${filter}`;
+  requestUri += `&_sourceInclude=${encodedFilter}`;
 
   return requestUri;
 };
