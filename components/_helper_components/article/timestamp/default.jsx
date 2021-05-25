04125aae2be3ac03736f40ff63312cf8fcdf7187
grincon0 <georgerincon2@gmail.com>
2021-05-24 22:48:51 -0400

    APD-1213/leadObjectAreaRedesignForAMP-George

--- components/_helper_components/article/timestamp/default.jsx
+++ components/_helper_components/article/timestamp/default.jsx
@@ -17,7 +17,7 @@ const TimeStamp = ({
   if (timeStamp === null) return null;
 
   return (
-    <span className={isTease ? 'isTease article-timestamp' : 'article-timestamp'}>
+    <span className={`${isTease ? 'isTease article-timestamp' : 'article-timestamp'} ${ampPage && 'is-amp'}`}>
       {timeStamp}
     </span>
   );
