c09a3e38d41bfbff49ad4b0db5fd629959db79be
Carlos Morales <carlos.rey.morales@gmail.com>
2021-06-09 14:20:27 -0400

    APD-1210/imagesAreSquare-Carlos

--- components/_helper_components/home/ListItem/ListItem.jsx
+++ components/_helper_components/home/ListItem/ListItem.jsx
@@ -57,7 +57,7 @@ const ListItem = ({
   const isListPage = listPage ? 'listPage' : '';
   let defaultPromoWidth = 500;
   let defaultPromoHeight = 282;
-  if (isTTDFeature) {
+  if (isTTDFeature || isListPage) {
     defaultPromoWidth = 110;
     defaultPromoHeight = 110;
   }
@@ -130,7 +130,7 @@ const ListItem = ({
       </div>
       {!hidePromo && getPromoItem() && !isDontMissFeature && (
         <a href={relativeURL} className="homeList-image">
-          <Image src={getPromoItem()} width={promoWidth} height={promoHeight} imageType="isHomepageImage" teaseContentType={contentType === 'video' || contentType === 'gallery' ? contentType : null} />
+          <Image src={getPromoItem()} width={promoWidth} height={promoHeight} imageType="isHomepageImage" teaseContentType={contentType === 'video' || contentType === 'gallery' ? contentType : null} squareImage={isListPage === 'listPage'}/>
           {sponsorName && <div className="c-sponsorOverlay">{sponsorName}</div>}
         </a>
       )}
