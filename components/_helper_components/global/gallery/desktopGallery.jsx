37a977cd3f3b01e106dd537371213bdc3b5b39f3
dave young <secondgen7@gmail.com>
2021-06-20 23:20:51 -0400

    [APD-1352] correcting the translateX calculation for initial gallery/page load scenario

--- components/_helper_components/global/gallery/desktopGallery.jsx
+++ components/_helper_components/global/gallery/desktopGallery.jsx
@@ -6,14 +6,29 @@ const DesktopGallery = (props) => {
 
   useEffect(() => {
     if (typeof translateX !== 'number') {
+      // fallback for initial load when the passed-in translateX hasn't yet been calculated
       const galleryContainers = document.querySelectorAll('.gallery-container');
       galleryContainers.forEach((gallery) => {
         if (gallery) {
           const focusElement = gallery.querySelector('#gallery-item-0');
           if (focusElement) {
-            const translateXValue = parseInt(gallery.offsetWidth, 10) / 2 - parseInt(focusElement.offsetWidth, 10) / 2 - parseInt(focusElement.offsetLeft, 10);
+            let focusElementOffset = 0;
+            let stopCounting = false;
+            gallery.querySelectorAll('.gallery-image').forEach((image) => {
+              // we loop through the images to add up the total left offset based on preceding images's widths & margins
+              if (image.getAttribute('data-index') === '0') {
+                stopCounting = true;
+                const style = image.currentStyle || window.getComputedStyle(image);
+                // and we also include the focus element's left margin as well
+                focusElementOffset += parseFloat(style.marginLeft);
+              } else if (!stopCounting) {
+                const style = image.currentStyle || window.getComputedStyle(image);
+                const margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+                focusElementOffset += image.offsetWidth + margin;
+              }
+            });
             // eslint-disable-next-line no-param-reassign
-            gallery.style.transform = `translateX(${translateXValue}px)`;
+            gallery.style.transform = `translateX(-${focusElementOffset - parseInt(focusElement.offsetWidth, 10) / 2}px)`;
           }
         }
       });
