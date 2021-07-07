15cd8c4f7fe1b721b31e019b52d1127a9ee00ec2
Mohanraj-lister <mohanraj.srinivasan@listertechnologies.com>
2021-07-07 15:27:54 +0530

    APD-1129/searchPageContentAPIOhio-Mohan

--- content/sources/search-page.js
+++ content/sources/search-page.js
@@ -1,4 +1,4 @@
-import { SEARCH_PAGE_KEY } from 'fusion:environment';
+import { SEARCH_PAGE_KEY_AJC, SEARCH_PAGE_KEY_OHIO } from 'fusion:environment';
 
 const params = {
   q: 'text',
@@ -6,8 +6,9 @@ const params = {
 };
 
 const resolve = (query) => {
-  const { arcSite = 'ajc', q, page } = query;
-  const requestUri = `https://search.arcpublishing.com/search?website_id=${arcSite}&q=${q}&page=${page}&key=${SEARCH_PAGE_KEY}`;
+  const { 'arc-site': arcSite = 'ajc', q, page } = query;
+  const searchPageQuery = (arcSite === 'ajc' ? SEARCH_PAGE_KEY_AJC : SEARCH_PAGE_KEY_OHIO);
+  const requestUri = `https://search.arcpublishing.com/search?website_id=${arcSite}&q=${q}&page=${page}&key=${searchPageQuery}`;
   return requestUri;
 };
 
