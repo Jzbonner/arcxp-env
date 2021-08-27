06b66ee8b4a243b90d1d965d536cec5d1faea8b9
grincon0 <georgerincon2@gmail.com>
2021-08-27 13:28:26 -0400

    APD-1470/rmPaddingTopOnSpecialSectionOnePageTitle-George

--- components/features/pageTitle/default.jsx
+++ components/features/pageTitle/default.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useEditableContent } from 'fusion:content';
-import { useAppContext, useFusionContext, useComponentContext } from 'fusion:context';
+import { useEditableContent, useComponentContext } from 'fusion:content';
 import './default.scss';
 
 const PageTitle = () => {
   const componentContext = useComponentContext();
   const { pageTitle } = componentContext && componentContext.customFields;
   const { editableField } = useEditableContent();
-  const appContext = useAppContext();
-  const fusionContext = useFusionContext();
 
-  const { layout = '' } = fusionContext;
-  let noPadding = false;
-
-  if (layout === 'special-section-one' && pageTitle.length > 0) {
-    noPadding = true;
-  }
-
-  console.log('pageTitle', pageTitle);
-  console.log('appContext', appContext);
-  console.log('fusionContext', fusionContext);
   return (
-    <div className={`c-page-title ${noPadding ? 'no-padding' : ''}`}>
+    <div className="c-page-title">
       <h1 className="c-title-content" {...editableField('Page Title')} suppressContentEditableWarning value="Add Page Title">
         {pageTitle}
       </h1>
