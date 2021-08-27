ba1838566793d7139318e074ddbe1edd5be0a077
grincon0 <georgerincon2@gmail.com>
2021-08-27 09:46:33 -0400

    APD-1470/testingQaFix-George

--- components/features/pageTitle/default.jsx
+++ components/features/pageTitle/default.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useEditableContent } from 'fusion:content';
-import { useComponentContext } from 'fusion:context';
+import { useAppContext, useFusionContext, useComponentContext } from 'fusion:context';
 import './default.scss';
 
 const PageTitle = () => {
   const componentContext = useComponentContext();
   const { pageTitle } = componentContext && componentContext.customFields;
   const { editableField } = useEditableContent();
+  const appContext = useAppContext();
+  const fusionContext = useFusionContext();
 
+  const { layout = '' } = fusionContext;
+  let noPadding = false;
+
+  if (layout === 'special-section-one' && pageTitle.length > 0) {
+    noPadding = true;
+  }
+
+  console.log('pageTitle', pageTitle);
+  console.log('appContext', appContext);
+  console.log('fusionContext', fusionContext);
   return (
-    <div className="c-page-title">
+    <div className={`c-page-title ${noPadding ? 'no-padding' : ''}`}>
       <h1 className="c-title-content" {...editableField('Page Title')} suppressContentEditableWarning value="Add Page Title">
         {pageTitle}
       </h1>
