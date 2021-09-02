1fefd491118385e36427329b8971079c62f28d7a
dave young <secondgen7@gmail.com>
2021-09-02 10:14:13 -0400

    [APD-1496] code cleanup

--- components/_helper_components/home/ListItemPreview/ListItemPreview.jsx
+++ components/_helper_components/home/ListItemPreview/ListItemPreview.jsx
@@ -13,26 +13,18 @@ const ListItemPreview = ({ id }) => {
     },
   });
 
-  if (storyData && storyData.headlines && storyData.headlines.web) {
+  if (storyData && storyData?.headlines?.web) {
     previewText = storyData.headlines.web;
-  } else if (
-    storyData
-    && storyData.content_elements
-    && storyData.content_elements[0]
-    && storyData.content_elements[0].type === 'text'
-  ) {
+  } else if (storyData?.content_elements[0]?.type === 'text') {
     let previewData = storyData.content_elements[0].content;
-    if (storyData.content_elements[0].content.length < 90
-    && storyData.content_elements[1]) {
-      previewData = `${storyData.content_elements[0].content} ${storyData.content_elements[1].content}`;
+    if (previewData.length < 90 && storyData.content_elements[1]) {
+      previewData = `${previewData} ${storyData.content_elements[1].content}`;
     }
     const textContent = safeHtml(previewData, { whiteList: {} });
     previewText = textContent;
   }
 
-  return <div className='c-listItemPreview'>
-    <p>{previewText}</p>
-  </div>;
+  return <div className='c-listItemPreview'>{previewText}</div>;
 };
 
 ListItemPreview.propTypes = {
