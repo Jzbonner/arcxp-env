b5d345459ee5524c84bfc700d5c89aa7845820d4
Mohanraj-lister <mohanraj.srinivasan@listertechnologies.com>
2021-09-09 23:15:26 +0530

    APD-1354/assigning host based on environment-Mohan

--- content/sources/most-read.js
+++ content/sources/most-read.js
@@ -3,6 +3,7 @@ import getProperties from 'fusion:properties';
 import { CHARTBEAT_KEY, CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
 import filterMostRead from './helper_functions/filterMostRead';
 import titleCheck from './helper_functions/titleCheck';
+import fetchEnv from '../../components/_helper_components/global/utils/environment';
 
 const ttl = 900;
 
@@ -13,17 +14,19 @@ const params = {
   includeStoryContent: 'checkbox',
 };
 
+const env = fetchEnv();
+
 
 const fetch = (query = {}) => {
   const {
     host = 'ajc.com', limit = '10', arcSite = 'ajc', includeStoryContent = false,
   } = query;
-  const { siteDomainURL } = getProperties(arcSite);
   let { section = '' } = query;
   const { chartbeat } = getProperties(arcSite);
   const { blacklist } = chartbeat;
+  // If env is not prod then host will be sandbox.ajc.com
+  const hostBasedonEnv = (env === 'prod') ? `${host}` : 'sandbox.ajc.com';
   let requestUri = `https://api.chartbeat.com/live/toppages/v3/?apikey=${CHARTBEAT_KEY}&types=1&host=${host}&limit=${limit}`;
-
   let newUri = requestUri;
   requestUri += section ? `&section=${section}` : '';
 
@@ -34,7 +37,7 @@ const fetch = (query = {}) => {
     .then(({ data }) => {
       if (!section || (data && data.pages && titleCheck(data, data.pages.length))) {
         // keep initial existing data that matches the user's input
-        const newArray = filterMostRead(data, host, blacklist);
+        const newArray = filterMostRead(data, hostBasedonEnv, blacklist);
         // grab the primary section if any
         const primarySection = `/${section.split('/')[1]}`;
         // check if the user's section input has secondary section
@@ -43,7 +46,7 @@ const fetch = (query = {}) => {
           newUri += section ? `&section=${section}` : '';
           return axios.get(newUri).then(({ data: siteData }) => {
             // update the array with the seconday data from the primary section if any
-            newArray.push(...filterMostRead(siteData, host, blacklist));
+            newArray.push(...filterMostRead(siteData, hostBasedonEnv, blacklist));
             return newArray;
           });
         } if (primarySection && primarySection === section && section !== '' && titleCheck(newArray, newArray.length, true)) {
@@ -51,20 +54,20 @@ const fetch = (query = {}) => {
           newUri += section ? `&section=${section}` : '';
           return axios.get(newUri).then(({ data: siteData }) => {
             // update the array with general data if the primary section has less than 5 results
-            newArray.push(...filterMostRead(siteData, host, blacklist));
+            newArray.push(...filterMostRead(siteData, hostBasedonEnv, blacklist));
             return newArray;
           });
         }
         return newArray;
       }
-      return filterMostRead(data, host, blacklist);
+      return filterMostRead(data, hostBasedonEnv, blacklist);
     })
     .then((data) => {
       mostReadContent = [...data];
       if (Array.isArray(data) && includeStoryContent === '') {
         const urls = [];
         data.map((element) => {
-          const path = element && element.path && element.path.replace(host, `${siteDomainURL}`);
+          const path = element && element.path && `https://${element.path}`;
           return urls.push(path);
         });
         const contentRequestUri = `${CONTENT_BASE}/content/v4/urls?website=${arcSite}`;
@@ -81,7 +84,7 @@ const fetch = (query = {}) => {
       /* eslint-disable camelcase */
         const { content_elements } = data;
         const mostReadWithArcData = mostReadContent.map((element, i) => {
-          const path = element && element.path && `${element.path.split(host)[1]}`;
+          const path = element && element.path && `${element.path.split(hostBasedonEnv)[1]}`;
           // checking the id's and merging the two array
           if (path && content_elements[i] && content_elements[i].website_url !== undefined && path === content_elements[i].website_url) {
             return Object.assign({}, element, content_elements[i]);
