185df485c097aaa580d47714555ab2115d476d50
kcorbin <kcorbin@AJC-C02Y207AJHCC.attlocal.net>
2021-10-21 11:43:42 -0400

    [APD-1554] Added conditional check to manually add description tag to non-page content

--- components/_helper_components/global/siteMeta/default.jsx
+++ components/_helper_components/global/siteMeta/default.jsx
@@ -45,13 +45,17 @@ const SiteMeta = () => {
   if (!seoTitle) pageTitle = title;
 
   const parsedDescription = safeHtml(description, { whiteList: {} });
-  // Page Builder seems to render Page Metadata automatically, no need to manually render 'description' anymore unless its for a custom tag like Twitter
+  // cap meta description at 150 characters.Google has no limit but recommends <= 150 chars, 160 chars max is what is allowed in video center.
+  const metaDescParsed = parsedDescription.substring(0, 150);
+  // Page Builder seems to render Page Metadata automatically for pages, here is a conditional check to manually add them for non-pages
+  const hasMetaDescription = !!metaValue('description');
 
   return (
     <>
       <link rel="apple-touch-icon" href={appleIconPath} />
       <link rel="icon" type="image/png" href={faviconPath} />
       <title>{pageTitle}</title>
+      {!hasMetaDescription && metaDescParsed ? <meta name="description" content={metaDescParsed} /> : null}
       {!isNativoLandingPage && <link rel="canonical" href={updatedURL} />}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:description" content={parsedDescription} />
