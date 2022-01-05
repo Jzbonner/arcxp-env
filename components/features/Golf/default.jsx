53a4c5b18332d3ab44e68e5ce601f4e0e07e9fde
Carlos Morales <carlos.rey.morales@gmail.com>
2022-01-05 15:36:42 -0500

    APD-1656/manualIdForLeaderBoardAdded-Carlos

--- components/features/Golf/default.jsx
+++ components/features/Golf/default.jsx
@@ -5,18 +5,18 @@ import Schedule from '../Schedule/default';
 import Leaderboard from '../Leaderboard/default';
 
 const Golf = ({ customFields = {} }) => {
-  const currentYear = new Date().getFullYear();
   const tour = 'pga';
   const {
     widgetName,
-    year = currentYear,
+    manualTournamentId,
+    year,
   } = customFields;
 
   switch (widgetName) {
     case 'Schedule':
       return <Schedule year={year} tour={tour} />;
     case 'Leaderboard':
-      return <Leaderboard year={year} tour={tour} />;
+      return <Leaderboard year={year} tour={tour} manualTournamentId={manualTournamentId} />;
     default:
       return <h1>{widgetName} is Under Construction</h1>;
   }
@@ -26,12 +26,17 @@ Golf.propTypes = {
   customFields: PropTypes.shape({
     widgetName: PropTypes.oneOf(['Schedule', 'Leaderboard', 'Tee Times', 'Statistics', 'Earnings']).tag({
       name: 'Widget Name',
+      defaultValue: 'Schedule',
     }),
     year: PropTypes.number.tag({
       name: 'Year',
+      defaultValue: new Date().getFullYear(),
     }),
     round: PropTypes.string.tag({
       name: 'Round',
+    }),
+    manualTournamentId: PropTypes.string.tag({
+      name: 'Manual Tournament Id',
     }),
   }),
 };
