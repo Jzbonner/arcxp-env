b02428ac8588513f42a8e6650605ca885de870a3
dave young <secondgen7@gmail.com>
2022-01-11 10:52:58 -0500

    [BRAN-4] sorting the cards (to ensure date order) and initial work for map

--- components/features/FlipCards/default.jsx
+++ components/features/FlipCards/default.jsx
@@ -21,78 +21,89 @@ const FlipCards = ({ customFields = {} }) => {
     'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december',
   ];
 
-  const renderCards = data => data && data?.items.map((cardData, i) => {
-    if (!cardData) return null;
+  const renderCards = (data) => {
+    if (!data.items) return null;
 
-    const {
-      'First name': fName = '',
-      'Last name': lName = '',
-      Age: age,
-      Gender: gender,
-      photo_url: imageUri,
-      Date: date,
-      Latitude: lat,
-      Longitude: long,
-      Details: description,
-      Link: storyUri,
-    } = cardData || {};
+    const sortedData = data.items;
+    sortedData.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+    return sortedData.map((cardData, i) => {
+      if (!cardData) return null;
 
-    const dateArr = date.split('-');
-    const {
-      0: year,
-      1: month,
-      2: day,
-    } = dateArr;
-    const monthFullName = month ? months[month - 1] : '';
-    const monthShortName = monthFullName.substr(0, 3);
+      const {
+        'First name': fName = '',
+        'Last name': lName = '',
+        Age: age,
+        Gender: gender,
+        photo_url: imageUri,
+        Date: date,
+        Latitude: lat,
+        Longitude: long,
+        Details: description,
+        Link: storyUri,
+      } = cardData || {};
 
-    // when attempting to populate className values with template literals, I was running into an eslint error:
-    // TypeError: Cannot read property 'range' of null
-    // happened for all three of the example immediately following this (name) and the className compilation
-    // const name = `${fName} ${lName}`;
+      const dateArr = date.split('-');
+      const {
+        0: year,
+        1: month,
+        2: day,
+      } = dateArr;
+      const monthFullName = month ? months[month - 1] : '';
+      const monthShortName = monthFullName.substr(0, 3);
 
-    let cardClass = 'card ';
-    cardClass += isFlipped === i ? 'is-flipped' : '';
-    let descriptionClass = 'description ';
-    descriptionClass += storyUri ? 'condensed' : '';
-    let cardBackContentClass = 'back content';
-    cardBackContentClass += imageUri ? ' with-image' : '';
-    let fullName = fName;
-    fullName += ' ';
-    fullName += lName;
+      // when attempting to populate className values with template literals, I was running into an eslint error:
+      // TypeError: Cannot read property 'range' of null
+      // happened for all three of the example immediately following this (name) and the className compilation
+      // const name = `${fName} ${lName}`;
 
-    return <div className={cardClass} key={i} onClick={ () => flipCard(i) }>
-      <div className='front content'>
-        <h2>{fName}<br />{lName}</h2>
-        <h3>Age: {age}</h3>
-        <div className='month'>
-          <span>{monthShortName.substr(0, 1)}</span>
-          <span>{monthShortName.substr(1, 1)}</span>
-          <span>{monthShortName.substr(2, 1)}</span></div>
-        {date && <h3 className='date'>Died:<br />{monthFullName} {day}, {year}</h3>}
-      </div>
-      <div className={cardBackContentClass}>
-        <div className='details'>
-          {imageUri && <img src={imageUri} alt={fullName} />}
-          <h2>{fullName}</h2>
-          {age && <h3>Age: {age}</h3>}
-          {gender && <h3>Gender: {gender}</h3>}
+      let cardClass = 'card ';
+      cardClass += isFlipped === i ? 'is-flipped' : '';
+      let descriptionClass = 'description ';
+      descriptionClass += storyUri ? 'condensed' : '';
+      let cardBackContentClass = 'back content';
+      cardBackContentClass += imageUri ? ' with-image' : '';
+      let fullName = fName;
+      fullName += ' ';
+      fullName += lName;
+
+      return <div className={cardClass} key={i} onClick={ () => flipCard(i) }>
+        <div className='front content'>
+          <h2>{fName}<br />{lName}</h2>
+          <h3>Age: {age}</h3>
+          <div className='month'>
+            <span>{monthShortName.substr(0, 1)}</span>
+            <span>{monthShortName.substr(1, 1)}</span>
+            <span>{monthShortName.substr(2, 1)}</span></div>
+          {date && <h3 className='date'>Died:<br />{monthFullName} {day}, {year}</h3>}
         </div>
-        {lat && long && <p className='location'>
-          <a href='#' data-lat={lat} data-long={long}>Location of death</a>
-        </p>}
-        {description && <p className={descriptionClass}>{description}</p>}
-        {storyUri && <a className='more-link' href={storyUri}>Read the Article</a>}
-      </div>
-    </div>;
-  });
+        <div className={cardBackContentClass}>
+          <div className='details'>
+            {imageUri && <img src={imageUri} alt={fullName} />}
+            <h2>{fullName}</h2>
+            {age && <h3>Age: {age}</h3>}
+            {gender && <h3>Gender: {gender}</h3>}
+          </div>
+          {lat && long && <p className='location'>
+            <a href='#' data-lat={lat} data-long={long}>Location of death</a>
+          </p>}
+          {description && <p className={descriptionClass}>{description}</p>}
+          {storyUri && <a className='more-link' href={storyUri}>Read the Article</a>}
+        </div>
+      </div>;
+    });
+  };
 
   if (useLocalData) {
     const data = AtlantaHomicidesMasterList2021;
 
-    return <div className='c-cards'>
-      {renderCards(data)}
-    </div>;
+    return <>
+      <div id='map' className='c-map'></div>
+      <div className='c-cards'>
+        {renderCards(data)}
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossOrigin=""/>
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossOrigin=""></script>
+      </div>
+    </>;
   }
 
   return <h1>FlipCards are still under construction</h1>;
