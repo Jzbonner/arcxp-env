d856a92063f89d7d60926615375d0679494a9814
Carlos Morales <carlos.rey.morales@gmail.com>
2022-02-03 11:34:44 -0500

    APD-1680/fixedAdIssue-Carlos

--- components/features/bravesOpeningDayCountdown/default.jsx
+++ components/features/bravesOpeningDayCountdown/default.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Countdown from '../../_helper_components/global/countdown/default';
 import ArcAd from '../ads/default';
@@ -11,41 +11,62 @@ import './default.scss';
 
 export const BravesOpeningDayCountdown = ({ tags = [], storyPage = false }) => {
   const [display, setDisplay] = useState(true);
+  const [displayAd, setDisplayAd] = useState(false);
 
   // 4/7/2022 4:30 pm. the time was a guess.
   const countDownTime = 1649363400000;
   const now = new Date();
 
-  if (Math.round((countDownTime - now) / 1000) < 0) return null;
+  if (Math.round((countDownTime - now) / 1000) < 0) {
+    return null;
+  }
 
-  if (storyPage && !tags.some(tag => tag.slug === 'atlanta-braves')) return null;
+  if (storyPage && !tags.some(tag => tag.slug === 'atlanta-braves')) {
+    return null;
+  }
+
+  useEffect(() => {
+    const handleWCC01Flighted = () => {
+      setDisplayAd(true);
+    };
+
+    document.addEventListener('WCC01_Flighted', handleWCC01Flighted);
+
+    return () => {
+      document.removeEventListener('WCC01_Flighted', handleWCC01Flighted);
+    };
+  }, []);
 
   return (
-        <>
-            { display
-                && <div className="BraveOpeningDayCountdown">
-                    <div className="bravesLogo flex-center">
-                        <img src={BravesLogo} alt="braves logo" />
-                    </div>
-                    <div className="countdown flex-center">
-                        <img src={CountdownBackGround} alt="countdown background" />
-                        <Countdown endDateTime={countDownTime} spacer={true} />
-                        <div className='opening-day'>
-                            <span></span>
-                            <div>Opening Day</div>
-                        </div>
-                    </div>
-                    <div className="ad flex-center">
-                        <ArcAd staticSlot={'WCC01'} />
-                    </div>
-                    <img
-                        src={ClosingIcon}
-                        alt='closing-icon'
-                        className='closing-icon'
-                        onClick={() => setDisplay(false)}/>
-                </div>
-            }
-        </>
+    <>
+      {display && (
+        <div className="BraveOpeningDayCountdown">
+          <div className="bravesLogo flex-center">
+            <img src={BravesLogo} alt="braves logo" />
+          </div>
+          <div className="countdown flex-center">
+            <img src={CountdownBackGround} alt="countdown background" />
+            <Countdown endDateTime={countDownTime} spacer={true} />
+            <div className="opening-day">
+              <span></span>
+              <div>Opening Day</div>
+            </div>
+          </div>
+          <div
+            className="ad flex-center"
+            style={{ display: `${displayAd ? 'flex' : 'none'}` }}
+          >
+            <ArcAd staticSlot={'WCC01'} />
+          </div>
+          <img
+            src={ClosingIcon}
+            alt="closing-icon"
+            className="closing-icon"
+            onClick={() => setDisplay(false)}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
