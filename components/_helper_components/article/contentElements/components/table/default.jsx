498abe60066697b77bd48e26577cbaef05b99859
Antonio <garcia.juan1994@gmail.com>
2022-02-17 10:48:34 -0500

    AS1-1/addsTableStyling-Antonio

--- components/_helper_components/article/contentElements/components/table/default.jsx
+++ components/_helper_components/article/contentElements/components/table/default.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './default.scss';
 
 const Table = ({ src }) => {
   const { header = [], rows = [] } = src || {};
   return (
     <div className="b-margin-bottom-d30-m20">
-      <table>
+      <table className='c-table b-flexRow b-flexColumn'>
+          { header.length > 0 && <thead><tr>
+            { header.map((cells, i) => <th key={`table-header-${i}`}><span>{cells.content}</span></th>)}
+          </tr></thead>
+          }
         <tbody>
-          <tr>
-          { header.map((cells, i) => <th key={`table-header-${i}`}>{cells.content}</th>)}
-          </tr>
           { rows.map((row, i) => <tr key={`table-row-${i}`}>
-              {row[0] && <td key={`table-row-${i + 1}-cell-1`}>{row[0].content}</td>}
-              {row[1] && <td key={`table-row-${i + 1}-cell-2`}>{row[1].content}</td>}
-              {row[2] && <td key={`table-row-${i + 1}-cell-3`}>{row[2].content}</td>}
-              {row[3] && <td key={`table-row-${i + 1}-cell-4`}>{row[3].content}</td>}
+              {row[0] && <td key={`table-row-${i + 1}-cell-1`}><span>{row[0].content}</span></td>}
+              {row[1] && <td key={`table-row-${i + 1}-cell-2`}><span>{row[1].content}</span></td>}
+              {row[2] && <td key={`table-row-${i + 1}-cell-3`}><span>{row[2].content}</span></td>}
+              {row[3] && <td key={`table-row-${i + 1}-cell-4`}><span>{row[3].content}</span></td>}
           </tr>)}
         </tbody>
       </table>
