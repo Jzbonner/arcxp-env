b07480cf33002a31d09fd5a1ca8833e69c53622d
grincon0 <georgerincon2@gmail.com>
2022-03-07 16:39:33 -0500

    AS1-3/singleQuotesDayton-George

--- components/layouts/_helper_functions/staffpage/OHIO_SITES.js
+++ components/layouts/_helper_functions/staffpage/OHIO_SITES.js
@@ -1,6 +1,7 @@
 export default () => ({
   'dayton-daily-news': 11,
-  dayton: 12,
+  // eslint-disable-next-line quote-props
+  'dayton': 12,
   'springfield-news-sun': 13,
   'journal-news': 14,
 });
