087b9c7d8bc83a3b792aecdcc1d25d9044e4f773
Mohanraj-lister <mohanraj.srinivasan@listertechnologies.com>
2022-03-14 21:13:38 +0530

    AS3-48/codeRefactor-Mohan

--- resources/icons/global/hamburgerDark.jsx
+++ resources/icons/global/hamburgerDark.jsx
@@ -4,7 +4,7 @@
 import React from 'react';
 
 const hamburgerDark = () => (
-    <svg width="21" height="20" xmlns="http://www.w3.org/2000/svg">
+    <svg width="21" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 20">
         <g fill="#212126" fillRule="evenodd">
             <path d="M.274.414h19.912v2.254H.274zM7.34 9.053h12.845v2.254H7.34zM.274 17.692h19.912v2.254H.274z"/>
         </g>
