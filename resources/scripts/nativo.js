ce6514b8f51ff567bf6fc934f9efa8852f448803
Nicholas Engleman <nicholas@englemaninvest.com>
2022-04-06 17:24:15 -0400

    AS3-72/OnlyApplyNativoAttributeOnStoryAndLiveUpdatePages-Nicholas

--- resources/scripts/nativo.js
+++ resources/scripts/nativo.js
@@ -2,14 +2,17 @@ window.addEventListener('DOMContentLoaded', () => {
   const nativoScript = document.createElement('script');
   const meterMeta = document.querySelector('meta[name="story.meter"]');
   const isProd = document.location.host.indexOf('sandbox') === -1;
-  const { arcSite } = window.Fusion;
+  const { arcSite, globalContentConfig, layout } = window.Fusion;
   const connextProps = window.Fusion.getProperties(arcSite).connext || [];
   const connextEnvProps = isProd ? connextProps.prod : connextProps.sandbox;
   const allowMeter = connextEnvProps ? connextEnvProps.allowMeter : false;
   let nativoIsDeferred = false;
   nativoScript.src = 'https://s.ntv.io/serve/load.js';
   nativoScript.id = 'nativoScript';
-  if (meterMeta && arcSite !== 'dayton') {
+  const isLiveUpdatesPage = globalContentConfig?.query?.includeSubtypes === 'liveupdates';
+  const isStory = layout === 'article-basic' || layout === 'article-in-depth';
+
+  if ((isStory || isLiveUpdatesPage) && arcSite !== 'dayton') {
     // it's a story page (thus `meterMeta` exists) and NOT Dayton (because Dayton doesn't yet have Taboola)
     nativoScript.setAttribute('data-ntv-set-no-auto-start', true);
     if (meterMeta.getAttribute('content') === 'premium' && allowMeter) {
