9fe95f06ab9f6e3a8c42b888033b21017ad3cfb7
Nicholas Engleman <nicholas@englemaninvest.com>
2022-04-07 13:50:02 -0400

    AS3-74/AddedSubscriberOnlyToNativoScript-Nicholas

--- resources/scripts/nativo.js
+++ resources/scripts/nativo.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   if ((isStory || isLiveUpdatesPage) && arcSite !== 'dayton') {
     // it's a story page (thus `meterMeta` exists) and NOT Dayton (because Dayton doesn't yet have Taboola)
     nativoScript.setAttribute('data-ntv-set-no-auto-start', true);
-    if (meterMeta.getAttribute('content') === 'premium' && allowMeter) {
+    if ((meterMeta.getAttribute('content') === 'premium' || meterMeta.getAttribute('content') === 'subscriberonly') && allowMeter) {
       // we add the nativo script to the deferred list for premium stories so that it isn't loaded until we know the paywall/auth state
       window.deferUntilKnownAuthState = window.deferUntilKnownAuthState || [];
       window.deferUntilKnownAuthState.push({ script: nativoScript });
