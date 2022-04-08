5905db21c167506512a4863ffb5fff3b16f50cb0
Nicholas Engleman <nicholas@englemaninvest.com>
2022-04-08 10:48:16 -0400

    noTicket/RemoveSophiCatch-Nicholas

--- content/sources/sophi-paywall.js
+++ content/sources/sophi-paywall.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { SOPHI_PAYWALL_ENDPOINT } from 'fusion:environment';
 import GetSophiPaywallBearerToken from './helper_functions/getSophiPaywallBearerToken.js';
 
-let ttl = 21600; // 6 hour cache time
+const ttl = 21600; // 6 hour cache time
 const serveStaleCache = false; // prevent using cached (empty) response in the case of 404's
 
 const params = {
@@ -35,11 +35,6 @@ const fetch = async ({ ids }, { cachedCall }) => {
         throw new Error(`SOPHI ERROR - Sophi paywall status for ${idString} is unavailable or empty.  Endpoint: ${SOPHI_PAYWALL_ENDPOINT}.`);
       }
       return data.map(resp => resp?.paywallStatus);
-    })
-    .catch((error) => {
-      ttl = 0; // manually re-set the TTL to 0 so that we don't cache this errant response (for some reason the 21600 value was still being respected, even when we reject the promise)
-      console.error(`AXIOS CATCH - get Sophi paywall status for ${idString} =>`, error);
-      return null;
     });
 };
 
