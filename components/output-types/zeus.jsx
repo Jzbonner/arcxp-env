fbdf36d438e581b0479c9a6186cf7e2fe47a35f8
dave young <secondgen7@gmail.com>
2022-04-14 16:41:52 -0400

    [AS3-77] adding zeus library (and separate outputType... for now) and omitting apstag & other ads calls in the same case

---
+++ components/output-types/zeus.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RenderOutputType from './_helper_components/default';
+
+const DefaultOutputType = props => <RenderOutputType {...props} />;
+
+export default DefaultOutputType;
