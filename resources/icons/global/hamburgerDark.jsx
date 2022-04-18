ad4fae167f7e830433046b150286600cb54ee354
Antonio <garcia.juan1994@gmail.com>
2022-04-18 14:17:44 -0400

    AS1-32/adjustsSvgsAndStyling-Antonio

--- resources/icons/global/hamburgerDark.jsx
+++ resources/icons/global/hamburgerDark.jsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-const hamburgerDark = () => (
+const HamburgerDark = () => (
     <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
     <g fill="#FFF" fillRule="evenodd">
         <path d="M0 0h20v2.308H0zM7.097 8.846H20v2.308H7.097zM0 17.692h20V20H0z"/>
@@ -11,4 +11,4 @@ const hamburgerDark = () => (
 </svg>
 );
 
-export default hamburgerDark;
+export default HamburgerDark;
