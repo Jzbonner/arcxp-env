f06e4063440be720f8a3db62eeb4d968f327090f
dave young <secondgen7@gmail.com>
2022-04-20 14:31:26 -0400

    [AS3-23] initial output from zeus sub-component, and allowing for arcads and zeus to coexist

---
+++ components/features/ads/zeus/children/adMapping.jsx
@@ -0,0 +1,194 @@
+const adMapping = {
+  PX01: {
+    id: 'interscroller',
+  },
+  HS01: {
+    id: 'top-half-shade',
+  },
+  HS02: {
+    id: 'inline-peekthrough',
+  },
+  NS01: {
+    id: 'native',
+  },
+  HP00: {
+    id: 'leaderboard',
+  },
+  HP01: {
+    id: 'inline-top',
+  },
+  'HP01-LiveUpdates': {
+    slotName: 'HP01',
+    id: 'inline-top-LU',
+  },
+  HP02: {
+    id: 'inline-mid',
+  },
+  HP03: {
+    id: 'inline-*',
+  },
+  'HP03-LiveUpdates': {
+    slotName: 'HP03',
+    id: 'inline-LU-*',
+  },
+  HP04: {
+    id: 'inline-*',
+  },
+  'HP04-LiveUpdates': {
+    slotName: 'HP04',
+    id: 'inline-LU-*',
+  },
+  HP05: {
+    id: 'inline-bottom',
+  },
+  'HP05-listPage': {
+    slotName: 'HP05',
+    id: 'right-rail-adhesive-*',
+  },
+  'HP05-FlatPage': {
+    slotName: 'HP05',
+    id: 'tablet-*',
+  },
+  RP01: {
+    id: 'right-rail-top',
+  },
+  'RP01 300x250': {
+    slotName: 'RP01',
+    id: 'right-rail-top-cube',
+  },
+  'RP01 300x250 (desktop only)': {
+    slotName: 'RP01',
+    id: 'right-rail-top-cube',
+  },
+  'RP01 300x600': {
+    slotName: 'RP01',
+    id: 'right-rail-top-rectangle',
+  },
+  'RP01 300x600 (desktop only)': {
+    slotName: 'RP01',
+    id: 'right-rail-top-rectangle',
+  },
+  'RP01 sticky': {
+    slotName: 'RP01',
+    id: 'right-rail-top-adhesive',
+  },
+  'RP01 sticky (desktop only)': {
+    slotName: 'RP01',
+    id: 'right-rail-top-adhesive',
+  },
+  'RP01 desktop': {
+    slotName: 'RP01',
+    id: 'right-rail-top',
+  },
+  'RP01 tablet': {
+    slotName: 'RP01',
+    id: 'tablet-top',
+  },
+  'RP01-Story-Desktop': {
+    slotName: 'RP01',
+    id: 'right-rail-top-article',
+  },
+  'RP01-Story-FlatPage': {
+    slotName: 'RP01',
+    id: 'right-rail-top',
+  },
+  'RP01-Story-Tablet': {
+    slotName: 'RP01',
+    id: 'tablet-article-*',
+  },
+  'RP01-List-Page': {
+    slotName: 'RP01',
+    id: 'right-rail-adhesive-*',
+  },
+  'RP01-LiveUpdates': {
+    slotName: 'RP01',
+    id: 'inline-mid-LU',
+  },
+  RP02: {
+    id: 'kroger',
+  },
+  'RP03 sticky': {
+    slotName: 'RP03',
+    id: 'right-rail-adhesive-*',
+  },
+  'RP03 sticky (desktop only)': {
+    slotName: 'RP03',
+    id: 'right-rail-adhesive-*',
+  },
+  'RP03 tablet': {
+    slotName: 'RP03',
+    id: 'tablet-*',
+  },
+  'RP09-Story-Desktop': {
+    slotName: 'RP09',
+    id: 'right-rail-adhesive-article',
+  },
+  'RP09-Story-Tablet': {
+    slotName: 'RP09',
+    id: 'tablet-article-*',
+  },
+  'RP09 300x250': {
+    slotName: 'RP09',
+    id: 'right-rail-mid-cube',
+  },
+  'RP09 300x250 (desktop only)': {
+    slotName: 'RP09',
+    id: 'right-rail-mid-cube',
+  },
+  'RP09 sticky': {
+    slotName: 'RP09',
+    id: 'right-rail-adhesive-*',
+  },
+  'RP09 sticky (desktop only)': {
+    slotName: 'RP09',
+    id: 'right-rail-adhesive-*',
+  },
+  'RP09 sticky listPage': {
+    slotName: 'RP09',
+    id: 'right-rail-adhesive-*',
+  },
+  'RP09 tablet': {
+    slotName: 'RP09',
+    id: 'tablet-*',
+  },
+  MP01: {
+    id: 'mob-leaderboard-top',
+  },
+  'MP01-LiveUpdates': {
+    slotName: 'MP01',
+    id: 'inline-top-LU',
+  },
+  MP02: {
+    id: 'mob-cube-premium-*',
+  },
+  'MP02-LiveUpdates': {
+    slotName: 'MP02',
+    id: 'inline-mid-LU',
+  },
+  MP03: {
+    id: 'mob-cube-premium-*',
+  },
+  MP04: {
+    id: 'mob-cube-*',
+  },
+  MP05: {
+    id: 'right-rail-adhesive-*',
+  },
+  PG01: {
+    id: 'gallery-*',
+  },
+  PG02: {
+    id: 'sponsorship',
+  },
+  MPG01: {
+    id: 'gallery-*',
+  },
+  SP01: {
+    id: 'sponsorship',
+  },
+  WCC01: {
+    id: 'sponsorship-countdown-clock',
+  },
+};
+
+export { adMapping as default };
